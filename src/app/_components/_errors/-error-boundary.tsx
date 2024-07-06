15197d91333bccbe7249051b40009e48a7d801e8
Quang Hiep <quanghiep03198@gmail.com>
2024-07-06 08:55:21 +0700

    style: scrollable error boundary call stack message

--- src/app/_components/_errors/-error-boundary.tsx
+++ src/app/_components/_errors/-error-boundary.tsx
@@ -6,6 +6,8 @@ import {
 	CollapsibleTrigger,
 	Div,
 	Icon,
+	ScrollArea,
+	ScrollBar,
 	Typography,
 	buttonVariants
 } from '@/components/ui'
@@ -22,38 +24,37 @@ const ErrorBoundary: React.FC<React.PropsWithChildren> = ({ children }) => {
 
 const ErrorFallback: React.FC<{ error: Error }> = ({ error }) => {
 	const { resetBoundary } = useErrorBoundary()
-	const [open, setOpen] = useState<boolean>(false)
+	const [open, setOpen] = useState<boolean>(true)
 
 	return (
-		<Div role='alert' className='mx-auto flex h-full w-full max-w-full flex-grow flex-col items-center space-y-4'>
-			<Div className='flex w-full items-start gap-x-6'>
-				<Div className='inline-flex items-center justify-center rounded-full bg-destructive/20 p-4'>
-					<Icon name='Bug' stroke='hsl(var(--destructive))' strokeWidth={1.5} size={32} aria-hidden='true' />
-				</Div>
-				<Div>
-					<Typography variant='h6' color='destructive' className='mb-2'>
-						Something went wrong
-					</Typography>
-					<Typography variant='p' className='mb-6'>
-						"{error.message}"
-					</Typography>
-
-					<Div className='mb-6'>
-						<Typography
-							variant='p'
-							className={cn('inline-block', open ? 'line-clamp-none' : 'line-clamp-3')}
-							color='muted'>
-							{error.stack}
-						</Typography>
-						<Button variant='link' className='px-0 text-foreground' onClick={() => setOpen(!open)}>
-							{open ? 'Show less' : 'Show more'}
-						</Button>
-					</Div>
-
-					<Button variant='destructive' onClick={resetBoundary}>
-						Try again
-					</Button>
-				</Div>
+		<Div role='alert' className='flex w-full max-w-full items-start gap-x-6 overflow-hidden'>
+			<Div className='inline-flex items-center justify-center rounded-full bg-destructive/20 p-4'>
+				<Icon name='Bug' stroke='hsl(var(--destructive))' strokeWidth={1.5} size={28} aria-hidden='true' />
+			</Div>
+			<Div>
+				<Typography variant='h6' color='destructive' className='mb-2'>
+					Something went wrong
+				</Typography>
+				<Typography variant='p' className='mb-2'>
+					"{error.message}"
+				</Typography>
+				<Collapsible open={open} onOpenChange={setOpen} className='mb-6'>
+					<CollapsibleTrigger
+						className={cn(buttonVariants({ variant: 'link', className: 'px-0 text-foreground' }))}>
+						{open ? 'Show less' : 'Show more'}
+					</CollapsibleTrigger>
+					<CollapsibleContent>
+						<ScrollArea className='h-64 w-full'>
+							<Typography variant='p' color='muted'>
+								{error.stack}
+							</Typography>
+							<ScrollBar orientation='horizontal' />
+						</ScrollArea>
+					</CollapsibleContent>
+				</Collapsible>
+				<Button variant='destructive' onClick={resetBoundary}>
+					Try again
+				</Button>
 			</Div>
 		</Div>
 	)
