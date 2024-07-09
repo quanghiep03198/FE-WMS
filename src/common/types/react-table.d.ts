86d8e64f71b3947a57458b6701695d4f4c8dc6b0
Quang Hiep <quanghiep03198@gmail.com>
2024-07-10 00:17:38 +0700

    chore: add react table filter types

--- src/common/types/react-table.d.ts
+++ src/common/types/react-table.d.ts
@@ -4,6 +4,15 @@ import { RowData } from '@tanstack/react-table'
 declare module '@tanstack/react-table' {
 	interface ColumnMeta<TData extends RowData, TValue> {
 		facetedUniqueValues?: Array<Record<'label' | 'value', any>>
+		filterVariant?: 'text' | 'range' | 'select'
 		sticky?: 'left' | 'right'
+	}
+
+	//add fuzzy filter to the filterFns
+	interface FilterFns {
+		fuzzy: FilterFn<unknown>
+	}
+	interface FilterMeta {
+		itemRank: RankingInfo
 	}
 }
