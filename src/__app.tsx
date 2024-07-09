371bc1a808d5708a67ac71fa6b8ce7415f4b3007
Quang Hiep <quanghiep03198@gmail.com>
2024-07-10 00:35:34 +0700

    chore: seperate root component to custom app

---
+++ src/__app.tsx
@@ -0,0 +1,19 @@
+import { I18nextProvider } from 'react-i18next'
+import { Toaster } from './components/ui/@core/sonner'
+import { i18n } from './i18n'
+import { QueryClientProvider } from './providers/query-client-provider'
+import RouterProvider from './providers/router-provider'
+import { ThemeProvider } from './providers/theme-provider'
+
+const App: React.FC = () => (
+	<QueryClientProvider>
+		<I18nextProvider i18n={i18n}>
+			<ThemeProvider>
+				<RouterProvider />
+				<Toaster position='bottom-right' />
+			</ThemeProvider>
+		</I18nextProvider>
+	</QueryClientProvider>
+)
+
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,9 @@
 import './styles/index.css'
 
 import ReactDOM from 'react-dom/client'
-import { I18nextProvider } from 'react-i18next'
-import { Toaster } from './components/ui/@core/sonner'
-import { QueryClientProvider } from './providers/query-client-provider'
-import RouterProvider from './providers/router-provider'
-import { ThemeProvider } from './providers/theme-provider'
-import { i18n } from './i18n'
+import App from './__app.tsx'
 import reportWebVitals from './report-web-vitals.ts'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<QueryClientProvider>
-		<I18nextProvider i18n={i18n}>
-			<ThemeProvider>
-				<RouterProvider />
-				<Toaster position='bottom-right' />
-			</ThemeProvider>
-		</I18nextProvider>
-	</QueryClientProvider>
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 
 reportWebVitals()
