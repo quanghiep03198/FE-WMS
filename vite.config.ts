6884e8e35daff968aef4915d713973d953173c7f
Quang Hiep <quanghiep03198@gmail.com>
2024-07-18 20:54:06 +0700

    chore: using env in vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,44 +2,48 @@
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react(), TanStackRouterVite()],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src')
-		}
-	},
-	esbuild: {
-		drop: ['console', 'debugger']
-	},
-	server: {
-		port: 3000,
-		host: true,
-		proxy: {
-			'/api': {
-				target: 'http://10.30.0.19:3000/api',
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/api/, '')
+export default defineConfig(({ mode }) => {
+	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+	return {
+		plugins: [react(), TanStackRouterVite()],
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, './src')
 			}
-		}
-	},
-	preview: {
-		port: 4000,
-		host: true
-	},
-	build: {
-		rollupOptions: {
-			output: {
-				manualChunks(id: string) {
-					// if (id.includes('node_modules')) {
-					// 	return 'vendor'
-					// }
-					// creating a chunk to react routes deps. Reducing the vendor chunk size
-					if (id.includes('react-router')) {
-						return '@tanstack/react-router'
+		},
+		esbuild: {
+			drop: ['console', 'debugger']
+		},
+		server: {
+			port: 3000,
+			host: true,
+			proxy: {
+				'/api': {
+					target: process.env.VITE_API_BASE_URL,
+					changeOrigin: true,
+					rewrite: (path) => path.replace(/^\/api/, '')
+				}
+			}
+		},
+		preview: {
+			port: 4000,
+			host: true
+		},
+		build: {
+			rollupOptions: {
+				output: {
+					manualChunks(id: string) {
+						if (id.includes('node_modules')) {
+							return 'vendor'
+						}
+						// creating a chunk to react routes deps. Reducing the vendor chunk size
+						if (id.includes('react-router')) {
+							return '@tanstack/react-router'
+						}
 					}
 				}
 			}
