83761200efb7d8d2eddfa07f8f20be01b82a6bf3
Quang Hiep <quanghiep03198@gmail.com>
2024-07-23 18:53:31 +0700

    refactor: remove unnecessary fetch user profile in guard

--- src/app/_components/_guard/-auth-guard.tsx
+++ src/app/_components/_guard/-auth-guard.tsx
@@ -1,22 +1,9 @@
-import { useGetUserProfileQuery } from '@/app/(auth)/_composables/-use-auth-api'
 import { useAuth } from '@/common/hooks/use-auth'
-import { generateAvatar } from '@/common/utils/generate-avatar'
-import { AuthService } from '@/services/auth.service'
 import { Navigate } from '@tanstack/react-router'
-import { useDeepCompareEffect } from 'ahooks'
 import React from 'react'
 
 const AuthGuard: React.FC<React.PropsWithChildren> = ({ children }) => {
-	const { isAuthenticated, setUserProfile } = useAuth()
-
-	// Get user profile after login successfully
-	const { data, isSuccess, isError } = useGetUserProfileQuery()
-
-	// Check authenticated status every time get profile query is triggered
-	useDeepCompareEffect(() => {
-		if (data && isSuccess) setUserProfile({ ...data, picture: generateAvatar({ name: data.display_name }) })
-		if (isError) AuthService.logout()
-	}, [data, , isSuccess, isError])
+	const { isAuthenticated } = useAuth()
 
 	if (!isAuthenticated) return <Navigate to='/login' />
 
