7d40f9785e208428d834962c053f0c609ebe2a64
Quang Hiep <quanghiep03198@gmail.com>
2024-07-24 21:43:44 +0700

    chore: update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,14 @@
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
-import { defineConfig, loadEnv, splitVendorChunkPlugin } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
 	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
 
 	return {
-		plugins: [react(), splitVendorChunkPlugin(), TanStackRouterVite()],
+		plugins: [react(), TanStackRouterVite()],
 		resolve: {
 			alias: {
 				'@': path.resolve(__dirname, './src')
