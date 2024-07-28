686de4ef09cdcc9d9769fd900e2308f63915a0c2
Quang Hiep <quanghiep03198@gmail.com>
2024-07-28 19:26:06 +0700

    perf: optimize chunk build

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
+import { visualizer } from 'rollup-plugin-visualizer'
 import { defineConfig, loadEnv } from 'vite'
 
 // https://vitejs.dev/config/
@@ -9,7 +10,7 @@ export default defineConfig(({ mode }) => {
 	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
 
 	return {
-		plugins: [react(), TanStackRouterVite()],
+		plugins: [TanStackRouterVite(), react(), visualizer()],
 		resolve: {
 			alias: {
 				'@': path.resolve(__dirname, './src')
@@ -34,11 +35,42 @@ export default defineConfig(({ mode }) => {
 			host: true
 		},
 		build: {
+			chunkSizeWarningLimit: 1024,
 			rollupOptions: {
 				output: {
 					manualChunks(id: string) {
-						if (id.includes('node_modules')) {
-							return 'vendor'
+						if (id.includes('router-provider')) {
+							return 'router-provider'
+						}
+						if (id.includes('react-router')) {
+							return '@tanstack/react-router'
+						}
+						if (id.includes('react-table')) {
+							return '@tanstack/react-table'
+						}
+						if (id.includes('react-virtual')) {
+							return '@tanstack/react-virtual'
+						}
+						if (id.includes('@radix-ui')) {
+							return '@radix-ui'
+						}
+						if (id.includes('react-query')) {
+							return '@tanstack/react-query'
+						}
+						if (id.includes('lucide-react')) {
+							return 'lucide-react'
+						}
+						if (id.includes('ahooks')) {
+							return 'ahooks'
+						}
+						if (id.includes('zustand')) {
+							return 'zustand'
+						}
+						if (id.includes('lodash')) {
+							return 'lodash'
+						}
+						if (id.includes('recharts')) {
+							return 'recharts'
 						}
 					}
 				}
