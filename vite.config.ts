adb7d87d93a534bafebc1edb9e66dd87a8eedf90
Quang Hiep <quanghiep03198@gmail.com>
2024-07-29 15:54:58 +0700

    refactor: vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
-import { visualizer } from 'rollup-plugin-visualizer'
 import { defineConfig, loadEnv } from 'vite'
 
 // https://vitejs.dev/config/
@@ -10,7 +9,7 @@ export default defineConfig(({ mode }) => {
 	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
 
 	return {
-		plugins: [TanStackRouterVite(), react(), visualizer()],
+		plugins: [TanStackRouterVite(), react()],
 		resolve: {
 			alias: {
 				'@': path.resolve(__dirname, './src')
@@ -39,38 +38,25 @@ export default defineConfig(({ mode }) => {
 			rollupOptions: {
 				output: {
 					manualChunks(id: string) {
-						if (id.includes('router-provider')) {
-							return 'router-provider'
-						}
-						if (id.includes('react-router')) {
-							return '@tanstack/react-router'
-						}
-						if (id.includes('react-table')) {
-							return '@tanstack/react-table'
-						}
-						if (id.includes('react-virtual')) {
-							return '@tanstack/react-virtual'
-						}
-						if (id.includes('@radix-ui')) {
-							return '@radix-ui'
-						}
-						if (id.includes('react-query')) {
-							return '@tanstack/react-query'
-						}
-						if (id.includes('lucide-react')) {
-							return 'lucide-react'
-						}
-						if (id.includes('ahooks')) {
-							return 'ahooks'
-						}
-						if (id.includes('zustand')) {
-							return 'zustand'
-						}
-						if (id.includes('lodash')) {
-							return 'lodash'
-						}
-						if (id.includes('recharts')) {
-							return 'recharts'
+						const modules = new Map([
+							['router-provider', /router-provider/],
+							['@tanstack/react-router', /react-router/],
+							['@tanstack/react-table', /react-table/],
+							['@tanstack/react-virtual', /react-virtual/],
+							['@radix-ui', /@radix-ui/],
+							['@tanstack/react-query', /react-query/],
+							['date-fns', /date-fns/],
+							['react-hook-form', /react-hook-form/],
+							['lucide', /lucide-react/],
+							['ahooks', /ahooks/],
+							['zustand', /zustand/],
+							['zod', /zustand/],
+							['lodash', /lodash/],
+							['recharts', /recharts/]
+						])
+
+						for (const [key, regex] of modules) {
+							if (id.match(regex)) return key
 						}
 					}
 				}
