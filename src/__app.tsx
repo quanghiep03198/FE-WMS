aee9b85a9ce4f6e177d03d1cfaae12b5956af56e
Quang Hiep <quanghiep03198@gmail.com>
2024-08-06 15:21:26 +0700

    chore: turn off network detector

--- src/__app.tsx
+++ src/__app.tsx
@@ -1,6 +1,5 @@
 import { Suspense, lazy } from 'react'
 import { I18nextProvider } from 'react-i18next'
-import NetworkDetector from './components/shared/network-detector'
 import { Toaster } from './components/ui/@core/sonner'
 import { i18n } from './i18n'
 import { QueryClientProvider } from './providers/query-client-provider'
@@ -8,18 +7,20 @@ import { ThemeProvider } from './providers/theme-provider'
 
 const RouterProvider = lazy(() => import('./providers/router-provider'))
 
-const App: React.FC = () => (
-	<QueryClientProvider>
-		<I18nextProvider i18n={i18n}>
-			<ThemeProvider>
-				<Suspense fallback={null}>
-					<RouterProvider />
-				</Suspense>
-				<Toaster position='bottom-right' closeButton={true} />
-				<NetworkDetector />
-			</ThemeProvider>
-		</I18nextProvider>
-	</QueryClientProvider>
-)
+const App: React.FC = () => {
+	return (
+		<QueryClientProvider>
+			<I18nextProvider i18n={i18n}>
+				<ThemeProvider>
+					<Suspense fallback={null}>
+						<RouterProvider />
+					</Suspense>
+					<Toaster position='bottom-right' closeButton={true} />
+					{/* <NetworkDetector /> */}
+				</ThemeProvider>
+			</I18nextProvider>
+		</QueryClientProvider>
+	)
+}
 
 export default App
