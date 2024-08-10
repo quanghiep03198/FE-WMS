4894d1786baabff20ece1b562f4d7606747bdd84
quanghiep03198 <quanghiep03198@gmail.com>
2024-08-10 10:02:34 +0700

    refactor: axios instance config class

--- src/configs/axios.config.ts
+++ src/configs/axios.config.ts
@@ -5,100 +5,110 @@ import axios, { HttpStatusCode, type AxiosError, type AxiosErrorFilter, type Axi
 import qs from 'qs'
 import { toast } from 'sonner'
 
-let isRefreshing = false
-let failedQueue: Array<AxiosErrorFilter> = []
+export class AxiosClient {
+	public instance: AxiosInstance
+	private isRefreshing: boolean = false
+	private failedQueue: Array<AxiosErrorFilter> = []
 
-/**
- * Processes the failed request by resolving or rejecting each promise with the provided token or error.
- *
- * @param {AxiosError} error - The error to reject the promises with, or null if the token is valid.
- * @param {string} token - The token to resolve the promises with.
- * @return {void}
- */
-const processRequestQueue = (error: AxiosError, token: string) => {
-	failedQueue.forEach(({ resolve, reject }) => {
-		error ? reject(error) : resolve(token)
-	})
-	failedQueue = []
+	/**
+	 * @description Processes the failed request by resolving or rejecting each promise with the provided token or error.
+	 * @param {AxiosError} error - The error to reject the promises with, or null if the token is valid.
+	 * @param {string} token - The token to resolve the promises with.
+	 * @return {void}
+	 */
+	private processRequestQueue = (error: AxiosError, token: string): void => {
+		this.failedQueue.forEach(({ resolve, reject }) => {
+			error ? reject(error) : resolve(token)
+		})
+		this.failedQueue = []
+	}
+
+	constructor(baseURL?: string) {
+		// * Instance configuration
+		this.instance = axios.create({
+			baseURL: baseURL ?? env('VITE_API_BASE_URL'),
+			timeout: env('VITE_API_BASE_URL', 10_000),
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			paramsSerializer: (params) => {
+				return qs.stringify(params, {
+					skipNulls: true,
+					format: 'RFC1738'
+				})
+			}
+		})
+		// * Instance request interceptor
+		this.instance.interceptors.request.use(
+			(config) => {
+				const accessToken = AuthService.getAccessToken()
+				const locale = StorageService.getLocale()
+				const user = AuthService.getCredentials()
+
+				config.headers['Authorization'] = config.headers['Authorization'] ?? accessToken
+				config.headers['X-User-Company'] = user?.company_code
+				config.headers['Accept-Language'] = locale
+
+				return config
+			},
+			(error) => Promise.reject(error)
+		)
+		// * Instance response interceptor
+		this.instance.interceptors.response.use(
+			(response) => response.data,
+			async (error: AxiosError) => {
+				if (error.code === 'ECONNABORTED') {
+					toast.error('Request timeout')
+					return Promise.reject(new Error('Request timeout'))
+				}
+
+				const originalRequest = error.config
+				const errorResponseStatus = error.response?.status
+
+				if (originalRequest && !originalRequest.retry && errorResponseStatus === HttpStatusCode.Unauthorized) {
+					console.error('[ERROR] ::: Log in session has expired.')
+
+					const user = AuthService.getCredentials()
+					if (!user?.id) {
+						AuthService.logout()
+						return Promise.reject(new Error('Invalid credentials'))
+					}
+
+					if (this.isRefreshing) {
+						return new Promise((resolve, reject) => {
+							this.failedQueue.push({ resolve, reject })
+						})
+							.then((token) => {
+								originalRequest.headers['Authorization'] = `Bearer ${token}`
+								return this.instance(originalRequest)
+							})
+							.catch((error) => Promise.reject(error))
+					}
+
+					originalRequest.retry = true
+					this.isRefreshing = true
+
+					try {
+						const response = await AuthService.refreshToken(user?.id)
+						AuthService.setAccessToken(response.metadata)
+						error.config.headers.Authorization = `Bearer ${response.metadata}`
+						this.processRequestQueue(null, response.metadata)
+						return this.instance.request(originalRequest)
+					} catch (error) {
+						this.processRequestQueue(error, null)
+						AuthService.logout()
+						return Promise.reject(error)
+					} finally {
+						this.isRefreshing = false
+					}
+				}
+
+				return Promise.reject(error)
+			}
+		)
+	}
 }
 
-const axiosInstance: AxiosInstance = axios.create({
-	baseURL: env('VITE_API_BASE_URL'),
-	timeout: env('VITE_API_BASE_URL', 10_000),
-	paramsSerializer: (params) => {
-		return qs.stringify(params, {
-			skipNulls: true,
-			format: 'RFC1738'
-		})
-	}
-})
-
-axiosInstance.interceptors.request.use(
-	(config) => {
-		const accessToken = AuthService.getAccessToken()
-		const locale = StorageService.getLocale()
-		const user = AuthService.getCredentials()
-
-		config.headers['Authorization'] = config.headers['Authorization'] ?? accessToken
-		config.headers['X-User-Company'] = user?.company_code
-		config.headers['Accept-Language'] = locale
-
-		return config
-	},
-	(error) => Promise.reject(error)
-)
-
-axiosInstance.interceptors.response.use(
-	(response) => response.data,
-	async (error: AxiosError) => {
-		if (error.code === 'ECONNABORTED') {
-			toast.error('Request timeout')
-			return Promise.reject(new Error('Request timeout'))
-		}
-
-		const originalRequest = error.config
-		const errorResponseStatus = error.response?.status
-
-		if (originalRequest && !originalRequest.retry && errorResponseStatus === HttpStatusCode.Unauthorized) {
-			console.error('[ERROR] ::: Log in session has expired.')
-
-			const user = AuthService.getCredentials()
-			if (!user?.id) {
-				AuthService.logout()
-				return Promise.reject(new Error('Invalid credentials'))
-			}
-
-			if (isRefreshing) {
-				return new Promise((resolve, reject) => {
-					failedQueue.push({ resolve, reject })
-				})
-					.then((token) => {
-						originalRequest.headers['Authorization'] = `Bearer ${token}`
-						return axiosInstance(originalRequest)
-					})
-					.catch((error) => Promise.reject(error))
-			}
-
-			originalRequest.retry = true
-			isRefreshing = true
-
-			try {
-				const response = await AuthService.refreshToken(user?.id)
-				AuthService.setAccessToken(response.metadata)
-				error.config.headers.Authorization = `Bearer ${response.metadata}`
-				processRequestQueue(null, response.metadata)
-				return axiosInstance.request(originalRequest)
-			} catch (error) {
-				processRequestQueue(error, null)
-				AuthService.logout()
-				return Promise.reject(error)
-			} finally {
-				isRefreshing = false
-			}
-		}
-
-		return Promise.reject(error)
-	}
-)
+const axiosInstance = new AxiosClient().instance
 
 export default axiosInstance
