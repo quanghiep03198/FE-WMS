889f675e71ccba7a3c1e876f8c07459fe372b8bd
quanghiep03198 <quanghiep03198@gmail.com>
2024-08-10 13:54:09 +0700

    feat: progressive web application

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,56 @@
 // @ts-nocheck
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
-import react from '@vitejs/plugin-react-swc'
+import ViteReact from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { defineConfig, loadEnv } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
 	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
 
 	return {
-		plugins: [TanStackRouterVite(), react()],
+		plugins: [
+			TanStackRouterVite(),
+			ViteReact(),
+			VitePWA({
+				registerType: 'autoUpdate',
+				includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+
+				manifest: {
+					name: 'WMS',
+					short_name: 'i-WMS',
+					description: 'Ứng dụng quản lý kho thành phẩm',
+					start_url: '/login',
+					display: 'fullscreen',
+					theme_color: '#ffffff',
+					icons: [
+						{
+							src: 'pwa-64x64.png',
+							sizes: '64x64',
+							type: 'image/png'
+						},
+						{
+							src: 'pwa-192x192.png',
+							sizes: '192x192',
+							type: 'image/png'
+						},
+						{
+							src: 'pwa-512x512.png',
+							sizes: '512x512',
+							type: 'image/png',
+							purpose: 'any'
+						},
+						{
+							src: 'maskable-icon-512x512.png',
+							sizes: '512x512',
+							type: 'image/png',
+							purpose: 'maskable'
+						}
+					]
+				}
+			})
+		],
 		resolve: {
 			alias: {
 				'@': path.resolve(__dirname, './src')
