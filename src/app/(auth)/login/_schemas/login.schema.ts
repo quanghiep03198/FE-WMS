7216461ae12c28204a7280dfabdf41c602a717a9
quanghiep03198 <quanghiep03198@gmail.com>
2024-08-21 22:55:06 +0700

    fix: login validation message

--- src/app/(auth)/login/_schemas/login.schema.ts
+++ src/app/(auth)/login/_schemas/login.schema.ts
@@ -1,10 +1,9 @@
-import { i18n } from '@/i18n'
 import { isEmpty } from 'lodash'
 import z from 'zod'
 
 export const loginSchema = z.object({
-	username: z.string().refine((value) => !isEmpty(value), { message: i18n.t('ns_auth:validation.require_account') }),
-	password: z.string().refine((value) => !isEmpty(value), { message: i18n.t('ns_auth:validation.require_password') })
+	username: z.string().refine((value) => !isEmpty(value), { message: 'ns_auth:validation.require_account' }),
+	password: z.string().refine((value) => !isEmpty(value), { message: 'ns_auth:validation.require_password' })
 })
 
 export type LoginFormValues = z.infer<typeof loginSchema>
