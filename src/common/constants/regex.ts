e0428add8b0cddad6d77ea9ba4b2a4b04da23fe6
quanghiep03198 <quanghiep03198@gmail.com>
2024-08-23 15:28:35 +0700

    chore: remove unused email regex

--- src/common/constants/regex.ts
+++ src/common/constants/regex.ts
@@ -1,6 +1,5 @@
 const Regex = {
 	PHONE: /^\d{10}$/,
-	EMAIL: /(\w+)@(gmail\.com|fpt\.edu\.vn)$/,
 	TIME: /^(?:[01]?\d|2[0-3]):[0-5]\d:[0-5]\d$/,
 	PASSWORD: /^(?=.*[A-Z])(?=.*[0-9]).{8,}$/,
 	VIETNAM_FACTORY_CODE: /^(va|vb)/i,
