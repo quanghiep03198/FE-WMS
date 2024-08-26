72c7bcc44ff582f5a8ab14aebb085d57bed76c7a
quanghiep03198 <quanghiep03198@gmail.com>
2024-08-26 09:09:05 +0700

    chore: update storage service

--- src/services/storage.service.ts
+++ src/services/storage.service.ts
@@ -1,10 +1,10 @@
 import { Languages } from '@/common/constants/enums'
-import { _JSON } from '@/common/utils/json'
+import { JSONs } from '@/common/utils/json'
 import { AppConfigs } from '@/configs/app.config'
 
 export class StorageService {
 	static getLocale() {
 		const locale = localStorage.getItem(AppConfigs.I18N_STORAGE_KEY)
-		return _JSON.parse<Languages>(locale) ?? Languages.ENGLISH
+		return JSONs.parse<Languages>(locale) ?? Languages.ENGLISH
 	}
 }
