5aa96ababd9a175eafef81430d0db3a39cc8cd0a
quanghiep03198 <quanghiep03198@gmail.com>
2024-08-30 11:28:59 +0700

    chore: add page header components

--- src/app/(features)/_components/shared/-page-header.tsx
+++ src/app/(features)/_components/shared/-page-header.tsx
@@ -6,17 +6,13 @@ type PageTitleProps = React.PropsWithChildren &
 	React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>
 
 export const PageHeader: React.FC<PageTitleProps> = forwardRef(({ children, className }, ref) => (
-	<div
-		ref={ref}
-		className={cn('flex flex-col gap-y-1.5 text-left sm:items-center sm:text-center', className)}
-		children={children}
-	/>
+	<div ref={ref} className={cn('space-y-1 text-left sm:items-center sm:text-center', className)} children={children} />
 ))
 
 export const PageTitle: React.FC<TypographyProps> = ({ children, className }) => (
-	<Typography className={cn('text-lg font-semibold leading-none tracking-tight', className)} children={children} />
+	<Typography className={cn('text-2xl font-bold leading-none tracking-tight', className)} children={children} />
 )
 
 export const PageDescription: React.FC<TypographyProps> = ({ children, className }) => (
-	<Typography variant='small' color='muted' className={className} children={children} />
+	<Typography color='muted' className={className} children={children} />
 )
