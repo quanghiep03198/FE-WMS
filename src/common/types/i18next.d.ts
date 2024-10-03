80b263f33ecf7b2111acd9e1ebcdfd4546f09d80
quanghiep03198 <quanghiep03198@gmail.com>
2024-10-03 10:15:33 +0700

    chore: update i18next custom type option

--- src/common/types/i18next.d.ts
+++ src/common/types/i18next.d.ts
@@ -1,7 +1,9 @@
+import 'i18next'
 import { Resources } from '../../i18n'
 
 export declare module 'i18next' {
 	interface CustomTypeOptions {
+		defaultNs: ['ns_common']
 		resources: Resources
 	}
 	// Helper type to extract keys from a namespace
