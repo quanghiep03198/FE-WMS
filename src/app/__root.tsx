7ed7c1a85265a448926a0d1ef6e6d53019dae72d
quanghiep03198 <quanghiep03198@gmail.com>
2024-10-05 13:33:25 +0700

    refactor(app): remove router devtools

--- src/app/__root.tsx
+++ src/app/__root.tsx
@@ -2,23 +2,7 @@
 import { QueryClient } from '@tanstack/react-query'
 import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
 // import { TanStackRouterDevtools } from '@tanstack/router-devtools'
-import { Fragment } from 'react'
 
 export const Route = createRootRouteWithContext<{ queryClient: QueryClient; isAuthenticated: boolean }>()({
-	component: RootRoute
+	component: Outlet
 })
-
-function RootRoute() {
-	return (
-		<Fragment>
-			<Outlet />
-			{/* {env('VITE_NODE_ENV') === 'development' && (
-				<TanStackRouterDevtools
-					position='bottom-left'
-					toggleButtonProps={{ style: { left: 80, bottom: 20 } }}
-					initialIsOpen={false}
-				/>
-			)} */}
-		</Fragment>
-	)
-}
