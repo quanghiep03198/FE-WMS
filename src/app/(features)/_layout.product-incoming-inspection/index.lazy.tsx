32c15b9b49b74f80b127076f60fb2df9440e1884
quanghiep03198 <quanghiep03198@gmail.com>
2024-11-08 10:29:49 +0700

    chore: temporarily deprecate product incoming proudction page

--- src/app/(features)/_layout.product-incoming-inspection/index.lazy.tsx
+++ src/app/(features)/_layout.product-incoming-inspection/index.lazy.tsx
@@ -1,11 +1,9 @@
 import { useBreadcrumbContext } from '@/app/(features)/_contexts/-breadcrumb-context'
-import { Div } from '@/components/ui'
+import UnavailableService from '@/app/_components/_errors/-unavailable-service'
 import { createLazyFileRoute } from '@tanstack/react-router'
 import { Fragment, useEffect } from 'react'
 import { Helmet } from 'react-helmet'
 import { useTranslation } from 'react-i18next'
-import InOutBoundOrderList from './_components/-io-order-list'
-import InoutBoundSizeRun from './_components/-io-size-run'
 
 export const Route = createLazyFileRoute('/(features)/_layout/product-incoming-inspection/')({
 	component: Page
@@ -26,10 +24,7 @@ function Page() {
 	return (
 		<Fragment>
 			<Helmet title={t('ns_common:navigation.production_incoming_inspection')} />
-			<Div className='space-y-10'>
-				<InOutBoundOrderList />
-				<InoutBoundSizeRun />
-			</Div>
+			<UnavailableService />
 		</Fragment>
 	)
 }
