d84f59d47b7213832397db86e3b88f10c4eaafae
quanghiep03198 <quanghiep03198@gmail.com>
2024-11-09 11:59:50 +0700

    chore(sonner): change toast visible toast

--- src/__app.tsx
+++ src/__app.tsx
@@ -1,5 +1,6 @@
 import { Suspense, lazy } from 'react'
 import { I18nextProvider } from 'react-i18next'
+import Loading from './components/shared/loading'
 import { Toaster } from './components/ui/@core/sonner'
 import { i18n } from './i18n'
 import { QueryClientProvider } from './providers/query-client-provider'
@@ -12,10 +13,10 @@ const App: React.FC = () => {
 		<QueryClientProvider>
 			<I18nextProvider i18n={i18n}>
 				<ThemeProvider>
-					<Suspense fallback={null}>
+					<Suspense fallback={<Loading />}>
 						<RouterProvider />
 					</Suspense>
-					<Toaster position='bottom-right' expand={true} closeButton={true} visibleToasts={5} />
+					<Toaster position='bottom-right' expand={true} closeButton={true} visibleToasts={3} duration={2000} />
 				</ThemeProvider>
 			</I18nextProvider>
 		</QueryClientProvider>
