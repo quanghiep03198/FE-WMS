711a4af6246d242b5901a4bd95bf80b79dc12d17
quanghiep03198 <quanghiep03198@gmail.com>
2024-11-09 13:46:08 +0700

    chore(pwa): update update on after deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import './styles/index.css'
 
 import ReactDOM from 'react-dom/client'
+import { registerSW } from 'virtual:pwa-register'
 import App from './__app.tsx'
 import reportWebVitals from './report-web-vitals.ts'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 
+registerSW({ immediate: true })
 reportWebVitals()
