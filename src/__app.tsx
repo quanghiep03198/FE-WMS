a50c1888d240ff120d5a0680abe28197a2af4e30
quanghiep03198 <quanghiep03198@gmail.com>
2024-11-15 12:34:59 +0700

    refactor(app): set default toast duration

--- src/__app.tsx
+++ src/__app.tsx
@@ -2,6 +2,7 @@ import { Suspense, lazy } from 'react'
 import { I18nextProvider } from 'react-i18next'
 import Loading from './components/shared/loading'
 import { Toaster } from './components/ui/@core/sonner'
+import { AppConfigs } from './configs/app.config'
 import { i18n } from './i18n'
 import { QueryClientProvider } from './providers/query-client-provider'
 import { ThemeProvider } from './providers/theme-provider'
@@ -16,7 +17,7 @@ const App: React.FC = () => {
 					<Suspense fallback={<Loading />}>
 						<RouterProvider />
 					</Suspense>
-					<Toaster position='bottom-right' duration={2000} />
+					<Toaster className='pointer-events-auto' position='bottom-right' duration={AppConfigs.TOAST_DURATION} />
 				</ThemeProvider>
 			</I18nextProvider>
 		</QueryClientProvider>
--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -2,4 +2,5 @@ export class AppConfigs {
 	static readonly ACCESS_TOKEN_STORAGE_KEY = 'accessToken'
 	static readonly I18N_STORAGE_KEY = 'i18nextLng'
 	static readonly QUERY_CLIENT_CACHE_STORAGE_KEY = 'queryClientOfflineCache'
+	static readonly TOAST_DURATION = 2000
 }
