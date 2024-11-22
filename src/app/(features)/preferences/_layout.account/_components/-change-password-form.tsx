840a2b690c8f9eb516bae0b9c6fec01e93a2bf1e
haitn <haitn2806@gmail.com>
2024-11-22 11:21:30 +0700

    feat: update-password

--- src/app/(features)/preferences/_layout.account/_components/-change-password-form.tsx
+++ src/app/(features)/preferences/_layout.account/_components/-change-password-form.tsx
@@ -1,14 +1,63 @@
+import { useAuth } from '@/common/hooks/use-auth'
 import { Button, Div, Form as FormProvider, InputFieldControl, Typography } from '@/components/ui'
-import React from 'react'
-import { useForm } from 'react-hook-form'
+import bcrypt from 'bcryptjs-react'
+import { debounce } from 'lodash'
+import React, { useEffect } from 'react'
+import { useForm, useWatch } from 'react-hook-form'
 import tw from 'tailwind-styled-components'
+import { updatePasswordUser } from '../_apis/account.api'
 
 const ChangePasswordForm: React.FC = () => {
-	const form = useForm({})
+	const { mutateAsync: updatePasswordFn } = updatePasswordUser()
+
+	const { user } = useAuth()
+
+	const form = useForm<any>({
+		mode: 'onChange',
+		defaultValues: {
+			currentPassword: '',
+			password: ''
+		}
+	})
+
+	const { control, setError, clearErrors } = form
+
+	const currentPassword = useWatch({ control, name: 'currentPassword' })
+
+	const validateCurrentPassword = debounce((value: string) => {
+		if (!bcrypt.compareSync(value, user.password)) {
+			setError('currentPassword', {
+				type: 'manual',
+				message: 'Current password is incorrect'
+			})
+		} else {
+			clearErrors('currentPassword')
+		}
+	}, 200)
+
+	useEffect(() => {
+		if (currentPassword) {
+			validateCurrentPassword(currentPassword)
+		}
+		return () => validateCurrentPassword.cancel()
+	}, [currentPassword])
+
+	const updatePassword = (data: any) => {
+		if (data.password.length < 6) {
+			setError('password', {
+				type: 'manual',
+				message: 'New password must be at least 6 characters long'
+			})
+			return
+		} else {
+			updatePasswordFn(data)
+			form.reset()
+		}
+	}
 
 	return (
 		<FormProvider {...form}>
-			<Form>
+			<Form onSubmit={form.handleSubmit((data) => updatePassword(data))}>
 				{/* Public profile */}
 				<Fieldset>
 					<Legend>Update password</Legend>
@@ -18,7 +67,7 @@ const ChangePasswordForm: React.FC = () => {
 
 					<Div className='space-y-6'>
 						<InputFieldControl label='Current password' name='currentPassword' placeholder='********' />
-						<InputFieldControl label='New password' name='newPassword' placeholder='********' />
+						<InputFieldControl label='New password' name='password' placeholder='********' />
 						<Button>Change password</Button>
 					</Div>
 				</Fieldset>
