711b0e0cd39734fd8a43139a92dc58d984c9efc9
quanghiep03198 <quanghiep03198@gmail.com>
2024-11-23 22:04:12 +0700

    refactor: add shadow scroll scrollbar option

--- src/components/ui/@custom/scroll-shadow.tsx
+++ src/components/ui/@custom/scroll-shadow.tsx
@@ -7,46 +7,50 @@ export interface ScrollShadowProps extends React.PropsWithChildren, React.Compon
 	 * If size is a number, it will be treated as pixels, otherwise is a string, it will be treated as a CSS value such as px, rem or em.
 	 */
 	size?: React.CSSProperties['height']
+	scrollbar?: boolean
 	ref?: React.Ref<HTMLDivElement>
 }
 
-const ScrollShadow = forwardRef<HTMLDivElement, ScrollShadowProps>(({ size = 320, className, children }, ref) => {
-	const update = useUpdate()
-	const localContainerRef = useRef<HTMLDivElement>(null)
-	const resolvedRef = (ref ?? localContainerRef) as React.MutableRefObject<HTMLDivElement>
+const ScrollShadow = forwardRef<HTMLDivElement, ScrollShadowProps>(
+	({ size = 320, scrollbar = false, className, children }, ref) => {
+		const update = useUpdate()
+		const localContainerRef = useRef<HTMLDivElement>(null)
+		const resolvedRef = (ref ?? localContainerRef) as React.MutableRefObject<HTMLDivElement>
 
-	const containerScroll = useScroll(resolvedRef)
-	const clientSize = useSize(resolvedRef)
+		const containerScroll = useScroll(resolvedRef)
+		const clientSize = useSize(resolvedRef)
 
-	const scrollHeight = resolvedRef.current?.scrollHeight ?? 0
-	const scrollTop = resolvedRef.current?.scrollTop ?? 0
-	const scrollClientHeight = resolvedRef.current?.clientHeight ?? 0
+		const scrollHeight = resolvedRef.current?.scrollHeight ?? 0
+		const scrollTop = resolvedRef.current?.scrollTop ?? 0
+		const scrollClientHeight = resolvedRef.current?.clientHeight ?? 0
 
-	const isScrolledToTop = containerScroll?.top === 0
-	const isScrolledToBottom = scrollHeight - scrollTop - scrollClientHeight < 1
-	const isScrollTopBottom = !isScrolledToTop && !isScrolledToBottom
+		const isScrolledToTop = containerScroll?.top === 0
+		const isScrolledToBottom = scrollHeight - scrollTop - scrollClientHeight < 1
+		const isScrollTopBottom = !isScrolledToTop && !isScrolledToBottom
 
-	const isScrollable = scrollHeight > scrollClientHeight
+		const isScrollable = scrollHeight > scrollClientHeight
 
-	useEffect(update, [scrollHeight, scrollTop, scrollClientHeight, clientSize])
+		useEffect(update, [scrollHeight, scrollTop, scrollClientHeight, clientSize])
 
-	return (
-		<div
-			ref={resolvedRef}
-			style={{ '--scroll-shadow-size': typeof size === 'number' ? `${size}px` : size } as React.CSSProperties}
-			data-top-scroll={isScrollable && isScrolledToTop}
-			data-bottom-scroll={isScrollable && isScrolledToBottom}
-			data-top-bottom-scroll={isScrollable && isScrollTopBottom}
-			className={cn(
-				className,
-				'h-[var(--scroll-shadow-size)] overflow-y-auto !scrollbar-none',
-				'data-[bottom-scroll=true]:[mask-image:linear-gradient(0deg,hsl(var(--sidebar-background))_calc(100%_-_var(--scroll-shadow-size)/4),transparent)]',
-				'data-[top-scroll=true]:[mask-image:linear-gradient(180deg,hsl(var(--sidebar-background))_calc(100%_-_var(--scroll-shadow-size)/4),transparent)]',
-				'data-[top-bottom-scroll=true]:[mask-image:linear-gradient(180deg,transparent,hsl(var(--sidebar-background))_calc(var(--scroll-shadow-size)/4),hsl(var(--sidebar-background))_calc(100%_-_var(--scroll-shadow-size)/4),transparent)]'
-			)}>
-			{children}
-		</div>
-	)
-})
+		return (
+			<div
+				ref={resolvedRef}
+				style={{ '--scroll-shadow-size': typeof size === 'number' ? `${size}px` : size } as React.CSSProperties}
+				data-top-scroll={isScrollable && isScrolledToTop}
+				data-bottom-scroll={isScrollable && isScrolledToBottom}
+				data-top-bottom-scroll={isScrollable && isScrollTopBottom}
+				className={cn(
+					className,
+					scrollbar ? 'overflow-scroll scrollbar' : 'overflow-auto !scrollbar-none',
+					'h-[var(--scroll-shadow-size)]',
+					'data-[bottom-scroll=true]:[mask-image:linear-gradient(0deg,hsl(var(--sidebar-background))_calc(100%_-_var(--scroll-shadow-size)/4),transparent)]',
+					'data-[top-scroll=true]:[mask-image:linear-gradient(180deg,hsl(var(--sidebar-background))_calc(100%_-_var(--scroll-shadow-size)/4),transparent)]',
+					'data-[top-bottom-scroll=true]:[mask-image:linear-gradient(180deg,transparent,hsl(var(--sidebar-background))_calc(var(--scroll-shadow-size)/4),hsl(var(--sidebar-background))_calc(100%_-_var(--scroll-shadow-size)/4),transparent)]'
+				)}>
+				{children}
+			</div>
+		)
+	}
+)
 
 export default memo(ScrollShadow)
