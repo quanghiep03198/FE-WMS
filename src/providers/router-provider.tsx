587cd5181d82d17a190a1fa5030e6e239a714230
quanghiep03198 <quanghiep03198@gmail.com>
2024-11-30 17:57:33 +0700

    chore: assert type of create router option due to turn off strict null check

--- src/providers/router-provider.tsx
+++ src/providers/router-provider.tsx
@@ -4,6 +4,8 @@ import { routeTree } from '@/route-tree.gen'
 import { RouterProvider as BrowserRouterProvider, createRouter } from '@tanstack/react-router'
 import { queryClient } from './query-client-provider'
 
+type CreateRouterOptions = FirstParameter<typeof createRouter>
+
 // Set up a Router instance
 const router = createRouter({
 	routeTree,
@@ -11,7 +13,7 @@ const router = createRouter({
 	defaultPreload: 'intent',
 	defaultNotFoundComponent: NotFoundPage,
 	defaultPreloadStaleTime: 0
-})
+} as unknown as CreateRouterOptions)
 
 // Register things for typesafety
 declare module '@tanstack/react-router' {
