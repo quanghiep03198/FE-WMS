440095e9a0672b6338b2c3caa8f1c5d0dfcb5c4f
quanghiep03198 <quanghiep03198@gmail.com>
2024-12-05 07:46:05 +0700

    chore: update logo assets

--- src/app/_components/_shared/-app-logo.tsx
+++ src/app/_components/_shared/-app-logo.tsx
@@ -4,13 +4,13 @@ import React from 'react'
 const AppLogo: React.FC = () => {
 	return (
 		<Div className='flex items-center gap-x-3 font-jetbrains group-data-[state=expanded]:gap-x-3 xl:gap-x-0'>
-			<Div className='flex aspect-square size-16 flex-col items-center justify-around rounded bg-primary p-px text-center text-[10px] text-sm font-medium tracking-widest text-primary-foreground group-data-[state=collapsed]:size-8 group-data-[state=expanded]:size-14 group-data-[state=expanded]:p-2 group-data-[state=collapsed]:!text-[10px]'>
+			<Div className='flex aspect-square flex-col items-stretch justify-center gap-y-2 rounded bg-primary p-2 text-center font-jetbrains text-[10px] text-sm font-medium leading-none tracking-widest text-primary-foreground shadow-2xl group-data-[state=collapsed]:size-8 group-data-[state=expanded]:size-12 group-data-[state=collapsed]:gap-y-1.5 group-data-[state=collapsed]:p-[3px] group-data-[state=collapsed]:text-[10px]'>
 				WMS
-				<Separator className='h-0.5 max-w-[80%]' />
+				<Separator className='h-[2px] w-full group-data-[state=expanded]:h-[3px]' />
 			</Div>
 			<Typography
 				variant='code'
-				className='duration-50 text-sm font-semibold tracking-widest transition-[width_opacity] group-data-[state=expanded]:w-auto group-data-[state=expanded]:opacity-100 xl:w-0 xl:opacity-0'>
+				className='duration-50 text-sm font-semibold leading-snug tracking-widest transition-[width_opacity] group-data-[state=expanded]:w-auto group-data-[state=expanded]:opacity-100 xl:w-0 xl:opacity-0'>
 				WAREHOUSE
 				<br />
 				MANAGEMENT
