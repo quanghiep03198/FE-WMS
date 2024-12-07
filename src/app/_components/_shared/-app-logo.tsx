dba61ac6fa1fe85dbb2a4de66fc02c358470333c
quanghiep03198 <quanghiep03198@gmail.com>
2024-12-07 13:03:44 +0700

    style(logo): adjust alignment and spacing in AppLogo component

--- src/app/_components/_shared/-app-logo.tsx
+++ src/app/_components/_shared/-app-logo.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const AppLogo: React.FC = () => {
 	return (
 		<Div className='flex items-center gap-x-3 font-jetbrains group-data-[state=expanded]:gap-x-3 xl:gap-x-0'>
-			<Div className='flex aspect-square flex-col items-stretch justify-center gap-y-2 rounded bg-primary p-2 text-center font-jetbrains text-[10px] text-sm font-medium leading-none tracking-widest text-primary-foreground shadow-2xl group-data-[state=collapsed]:size-8 group-data-[state=expanded]:size-12 group-data-[state=collapsed]:gap-y-1.5 group-data-[state=collapsed]:p-[3px] group-data-[state=collapsed]:text-[10px]'>
+			<Div className='flex aspect-square flex-col items-center justify-center gap-y-2 rounded bg-primary p-2 text-center font-jetbrains text-[10px] text-sm font-medium leading-none text-primary-foreground shadow-2xl group-data-[state=collapsed]:size-8 group-data-[state=expanded]:size-12 group-data-[state=collapsed]:gap-y-1.5 group-data-[state=collapsed]:p-[3px] group-data-[state=collapsed]:text-[10px] group-data-[state=collapsed]:tracking-widest'>
 				WMS
 				<Separator className='h-[2px] w-full group-data-[state=expanded]:h-[3px]' />
 			</Div>
