3ab841d1d2ce74b82115f2171f5107b93c65aa0f
quanghiep03198 <quanghiep03198@gmail.com>
2024-12-19 14:10:47 +0700

    feat: refactor ScannerToolbar to use TenancySelect component for improved code organization

--- src/app/(features)/_layout.finished-production-inoutbound/_components/_scanner-toolbar/-index.tsx
+++ src/app/(features)/_layout.finished-production-inoutbound/_components/_scanner-toolbar/-index.tsx
@@ -1,45 +1,26 @@
-import { PresetBreakPoints } from '@/common/constants/enums'
 import { useAuth } from '@/common/hooks/use-auth'
-import useMediaQuery from '@/common/hooks/use-media-query'
-import {
-	Button,
-	ButtonProps,
-	Div,
-	HoverCard,
-	HoverCardContent,
-	Icon,
-	Select,
-	SelectContent,
-	SelectGroup,
-	SelectItem,
-	SelectTrigger,
-	SelectValue,
-	Typography
-} from '@/components/ui'
+import { Button, ButtonProps, Div, Icon } from '@/components/ui'
 import ConfirmDialog from '@/components/ui/@override/confirm-dialog'
-import { TenancyService } from '@/services/tenancy.service'
-import { HoverCardTrigger } from '@radix-ui/react-hover-card'
-import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { useQueryClient } from '@tanstack/react-query'
 import { useBlocker } from '@tanstack/react-router'
 import React, { Fragment, useCallback, useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 import { FP_EPC_LIST_PROVIDE_TAG, FP_ORDER_DETAIL_PROVIDE_TAG } from '../../_apis/rfid.api'
 import { usePageContext } from '../../_contexts/-page-context'
+import TenancySelect from './-tenancy-select'
 
 interface TScanningButtonProps extends Pick<ButtonProps, 'children' | 'variant'> {
 	icon: React.ComponentProps<typeof Icon>['name']
 }
 
 const ScannerToolbar: React.FC = () => {
-	const { user, isAuthenticated } = useAuth()
-	const isSmallScreen = useMediaQuery(PresetBreakPoints.SMALL)
+	const { isAuthenticated } = useAuth()
 
 	const { t, i18n } = useTranslation()
 	const {
 		scanningStatus,
 		connection,
 		reset: resetScanningAction,
-		setConnection,
 		handleToggleScanning
 	} = usePageContext(
 		'scanningStatus',
@@ -50,13 +31,6 @@ const ScannerToolbar: React.FC = () => {
 		'handleToggleScanning',
 		'reset'
 	)
-
-	const { data: tenants } = useQuery({
-		queryKey: ['TENANCY', user.company_code],
-		queryFn: TenancyService.getTenantsByFactory,
-		select: (response) => response.metadata,
-		refetchOnMount: 'always'
-	})
 
 	const queryClient = useQueryClient()
 
@@ -75,7 +49,7 @@ const ScannerToolbar: React.FC = () => {
 	const handleReset = useCallback(blocker.reset, [blocker.status])
 	const handleProceed = useCallback(blocker.proceed, [blocker.status])
 
-	const handleResetScanningAction = () => {
+	const handleResetScanningAction = useCallback(() => {
 		queryClient.removeQueries({
 			queryKey: [FP_ORDER_DETAIL_PROVIDE_TAG],
 			exact: false,
@@ -87,39 +61,12 @@ const ScannerToolbar: React.FC = () => {
 			type: 'all'
 		})
 		resetScanningAction()
-	}
+	}, [])
 
 	return (
 		<Fragment>
 			<Div className='flex items-center justify-between sm:flex-col sm:items-stretch sm:justify-stretch sm:gap-y-2'>
-				<Select
-					value={connection}
-					disabled={typeof scanningStatus !== 'undefined'}
-					onValueChange={(value) => setConnection(value)}>
-					<HoverCard openDelay={50} closeDelay={50}>
-						<HoverCardTrigger asChild className='w-full basis-1/5 sm:basis-full md:basis-1/3 lg:basis-1/3'>
-							<SelectTrigger>
-								<Div className='flex flex-1 items-center gap-x-3'>
-									<Icon name='Server' size={18} stroke='hsl(var(--primary))' />
-									<SelectValue placeholder={'Select database'} />
-								</Div>
-							</SelectTrigger>
-						</HoverCardTrigger>
-						<HoverCardContent side={isSmallScreen ? 'top' : 'right'} align='start' sideOffset={8}>
-							<Typography variant='small'>{t('ns_inoutbound:description.select_database')}</Typography>
-						</HoverCardContent>
-					</HoverCard>
-					<SelectContent>
-						<SelectGroup>
-							{Array.isArray(tenants) &&
-								tenants.map((item) => (
-									<SelectItem key={item.id} value={item.id}>
-										{t('ns_common:others.server', { alias: item.alias, defaultValue: item.alias })}
-									</SelectItem>
-								))}
-						</SelectGroup>
-					</SelectContent>
-				</Select>
+				<TenancySelect />
 
 				<Div className='inline-grid grid-cols-2 items-stretch gap-x-1 *:w-full'>
 					<Button
---
+++ src/app/(features)/_layout.finished-production-inoutbound/_components/_scanner-toolbar/-tenancy-select.tsx
@@ -0,0 +1,77 @@
+import { PresetBreakPoints } from '@/common/constants/enums'
+import { useAuth } from '@/common/hooks/use-auth'
+import useMediaQuery from '@/common/hooks/use-media-query'
+import {
+	Div,
+	HoverCard,
+	HoverCardContent,
+	HoverCardTrigger,
+	Icon,
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+	Typography
+} from '@/components/ui'
+import { TenancyService } from '@/services/tenancy.service'
+import { useQuery } from '@tanstack/react-query'
+import { memo } from 'react'
+import { useTranslation } from 'react-i18next'
+import { usePageContext } from '../../_contexts/-page-context'
+
+const TenancySelect: React.FC = () => {
+	const isSmallScreen = useMediaQuery(PresetBreakPoints.SMALL)
+	const { user } = useAuth()
+	const { t } = useTranslation()
+	const { scanningStatus, connection, setConnection } = usePageContext(
+		'scanningStatus',
+		'connection',
+		'reset',
+		'setScanningStatus',
+		'setConnection',
+		'handleToggleScanning',
+		'reset'
+	)
+
+	const { data: tenants } = useQuery({
+		queryKey: ['TENANCY', user.company_code],
+		queryFn: TenancyService.getTenantsByFactory,
+		select: (response) => response.metadata,
+		refetchOnMount: 'always'
+	})
+
+	return (
+		<Select
+			value={connection}
+			disabled={typeof scanningStatus !== 'undefined'}
+			onValueChange={(value) => setConnection(value)}>
+			<HoverCard openDelay={50} closeDelay={50}>
+				<HoverCardTrigger asChild className='w-full basis-1/5 sm:basis-full md:basis-1/3 lg:basis-1/3'>
+					<SelectTrigger>
+						<Div className='flex flex-1 items-center gap-x-3'>
+							<Icon name='Server' size={18} stroke='hsl(var(--primary))' />
+							<SelectValue placeholder={'Select database'} />
+						</Div>
+					</SelectTrigger>
+				</HoverCardTrigger>
+				<HoverCardContent side={isSmallScreen ? 'top' : 'right'} align='start' sideOffset={8}>
+					<Typography variant='small'>{t('ns_inoutbound:description.select_database')}</Typography>
+				</HoverCardContent>
+			</HoverCard>
+			<SelectContent>
+				<SelectGroup>
+					{Array.isArray(tenants) &&
+						tenants.map((item) => (
+							<SelectItem key={item.id} value={item.id}>
+								{t('ns_common:others.server', { alias: item.alias, defaultValue: item.alias })}
+							</SelectItem>
+						))}
+				</SelectGroup>
+			</SelectContent>
+		</Select>
+	)
+}
+
+export default memo(TenancySelect)
