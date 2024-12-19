8e5b0af3623e4affde57762596cf7f212804467c
quanghiep03198 <quanghiep03198@gmail.com>
2024-12-19 13:56:55 +0700

    feat: enhance env function to support generic type for improved type safety

--- src/common/utils/env.ts
+++ src/common/utils/env.ts
@@ -1,3 +1,3 @@
-export default function env(key: keyof InternalImportMetaEnv, defaultValue?: any) {
-	return import.meta.env[key] ?? defaultValue
+export default function env<T = string>(key: keyof InternalImportMetaEnv, defaultValue?: any) {
+	return (import.meta.env[key] ?? defaultValue) as T
 }
