7e64ee3b491c81b05fff2aff724e13fa96ed3e3e
quanghiep03198 <quanghiep03198@gmail.com>
2024-12-19 13:51:31 +0700

    feat: add network connection health check URL to app configuration

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -1,4 +1,5 @@
 export class AppConfigs {
+	static readonly NETWORK_CONNECTION_HEALTH_CHECK_URL = 'https://httpbin.org/get'
 	static readonly ACCESS_TOKEN_STORAGE_KEY = 'accessToken'
 	static readonly I18N_STORAGE_KEY = 'i18nextLng'
 	static readonly QUERY_CLIENT_CACHE_STORAGE_KEY = 'queryClientOfflineCache'
