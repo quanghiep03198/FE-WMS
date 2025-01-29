c1b140898d91fa839669b8ff791f3fcf6438d35c
quanghiep03198 <quanghiep03198@gmail.com>
2025-01-29 13:54:21 +0700

    fix: update defaultValue type to unknown in env utility function

--- src/common/utils/env.ts
+++ src/common/utils/env.ts
@@ -1,3 +1,3 @@
-export default function env<T = string>(key: keyof InternalImportMetaEnv, defaultValue?: any) {
+export default function env<T = string>(key: keyof InternalImportMetaEnv, defaultValue?: unknown) {
 	return (import.meta.env[key] ?? defaultValue) as T
 }
