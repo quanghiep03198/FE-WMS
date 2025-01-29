61cb9547a60114736736c2731696113184e88b3b
quanghiep03198 <quanghiep03198@gmail.com>
2025-01-29 14:01:17 +0700

    fix: update Sentry configuration to enable in production and adjust trace propagation targets

--- src/main.tsx
+++ src/main.tsx
@@ -21,10 +21,9 @@ Sentry.init({
 		if (event.exception) Sentry.captureException(event.exception)
 		return event
 	},
-
-	debug: runtimeEnvironment === 'development',
+	enabled: runtimeEnvironment === 'production',
 	tracesSampleRate: 1.0, //  Capture 100% of the transactions
-	tracePropagationTargets: ['localhost', env('VITE_APP_HOST'), env('VITE_API_BASE_URL')],
+	tracePropagationTargets: [env('VITE_APP_HOST'), env('VITE_API_BASE_URL')],
 	replaysSessionSampleRate: runtimeEnvironment === 'production' ? 0.1 : 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
 	replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 })
