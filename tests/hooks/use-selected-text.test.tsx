f0c18c8cdff23c59147d2e707a3000385c0efdb9
quanghiep03198 <quanghiep03198@gmail.com>
2025-03-24 19:31:07 +0700

    refactor: update imports to use named exports for consistency in tests

--- tests/hooks/use-selected-text.test.tsx
+++ tests/hooks/use-selected-text.test.tsx
@@ -1,6 +1,6 @@
 import { act, renderHook } from '@testing-library/react'
 import { describe, expect, it, vi } from 'vitest'
-import { useSelectedText } from '../../src/common/hooks/use-selected-text'
+import useSelectedText from '../../src/common/hooks/use-selected-text'
 
 describe('useSelectedText', () => {
 	it('should initialize with an empty string', () => {
--- tests/utils/util.test.ts
+++ tests/utils/util.test.ts
@@ -1,37 +1,37 @@
-import { Util } from '@/common/utils/util'
+import { isPrimitive } from '@/common/utils/common'
 import { describe, expect, it } from 'vitest'
 
 describe('Check if value has primitive datatype', () => {
 	// Should return null if argument is null
 	it('should return "true" when argument is null', () => {
-		const result = Util.isPrimitive(null)
+		const result = isPrimitive(null)
 		expect(result).toBe(true)
 	})
 	it('should return "false" when argument is undefined', () => {
-		const result = Util.isPrimitive(undefined)
+		const result = isPrimitive(undefined)
 		expect(result).toBe(true)
 	})
 
 	it('should return "false" when argument is an object', () => {
-		const result = Util.isPrimitive({ name: 'John', age: 20 })
+		const result = isPrimitive({ name: 'John', age: 20 })
 		expect(result).toBe(false)
 	})
 
 	it('should return "false" if argument is an array', () => {
-		const result = Util.isPrimitive(['Apple', 'Pineaple', 'Orange'])
+		const result = isPrimitive(['Apple', 'Pineaple', 'Orange'])
 		expect(result).toBe(false)
 	})
 
 	// Should return null if argument is empty string
 	it('should return "true" when argument is a string', () => {
 		const input = 'Hello World'
-		const result = Util.isPrimitive(input)
+		const result = isPrimitive(input)
 		expect(result).toBe(true)
 	})
 
 	it('should return true when argument is a number', () => {
 		const input = 10
-		const result = Util.isPrimitive(input)
+		const result = isPrimitive(input)
 		expect(result).toBe(true)
 	})
 })
