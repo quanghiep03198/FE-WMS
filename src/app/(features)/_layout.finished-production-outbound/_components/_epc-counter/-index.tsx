f650870cd729da01baf067b60bca2a3e201b5c8d
quanghiep03198 <quanghiep03198@gmail.com>
2025-03-27 01:11:42 +0700

    refactor: adjust typography sizes in ScanningCounter for improved readability

--- src/app/(features)/_layout.finished-production-outbound/_components/_epc-counter/-index.tsx
+++ src/app/(features)/_layout.finished-production-outbound/_components/_epc-counter/-index.tsx
@@ -57,11 +57,11 @@ const ScanningCounter: React.FC = () => {
 
 	return (
 		<Div className='relative z-10 mb-2 flex items-center justify-center gap-x-3'>
-			<Typography variant='h5' className='inline-flex items-center gap-x-2'>
+			<Typography variant='h6' className='inline-flex items-center gap-x-2 text-xl @3xl:text-2xl'>
 				{t('ns_inoutbound:counter_box.label')}
 			</Typography>
-			<Separator className='h-0.5 w-1.5 self-center bg-foreground' />
-			<Typography variant='h4' className='inline-flex gap-x-1 tracking-wide @6xl:text-3xl'>
+			<Separator className='h-0.5 w-1.5 bg-foreground' />
+			<Typography variant='h4' className='inline-flex gap-x-1 self-baseline text-2xl tracking-wide @3xl:text-4xl'>
 				{formatIntlNumber(count)}
 				<Typography variant='small' className='text-xs font-medium'>
 					pcs
