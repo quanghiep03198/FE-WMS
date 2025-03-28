dcaf322e67b8cc6d7f35c26e9d5d4d8d4f8821d8
quanghiep03198 <quanghiep03198@gmail.com>
2025-03-28 16:36:11 +0700

    refactor: adjust table width calculation and disable auto-reset of expanded rows in DataTable

--- src/components/ui/@react-table/components/table.tsx
+++ src/components/ui/@react-table/components/table.tsx
@@ -78,7 +78,7 @@ function TableDataGrid<TData, TValue>({
 		<Wrapper
 			ref={wrapperRef}
 			style={{
-				'--table-width': wrapperSize?.width - 8 + 'px'
+				'--table-width': wrapperSize?.width - 10 + 'px'
 			}}>
 			{caption && <TableHeadCaption id={captionId} aria-description={caption} />}
 			<ScrollArea tabIndex={0} ref={containerRef} {...containerProps}>
