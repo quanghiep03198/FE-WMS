d212713e279a0019dbc5a6758daed0a8549df0c4
quanghiep03198 <quanghiep03198@gmail.com>
2025-03-30 22:30:56 +0700

    feat: add custom event emitter hook for managing global events

---
+++ src/common/hooks/use-event-emitter.tsx
@@ -0,0 +1,38 @@
+import { useCallback, useEffect, useRef, useState } from 'react'
+
+class GlobalEventEmitter extends EventTarget {}
+
+export const globalEventEmitter = new GlobalEventEmitter()
+
+export default function useEventEmitter<T>(eventName: string): [T, (eventData: T, skipRender?: boolean) => void] {
+	const [eventData, setEventData] = useState<T>()
+	const skipRerender = useRef(false)
+
+	const dispatchEvent = useCallback(
+		(eventData: T, skipRender = true) => {
+			skipRerender.current = skipRender
+			const event = new CustomEvent(eventName, { detail: eventData })
+			globalEventEmitter.dispatchEvent(event)
+		},
+		[eventName]
+	)
+
+	useEffect(() => {
+		const listener = (event: Event) => {
+			if (skipRerender.current) {
+				skipRerender.current = false
+				return
+			}
+			setEventData((event as CustomEvent).detail)
+		}
+
+		globalEventEmitter.addEventListener(eventName, listener)
+
+		// Cleanup subscription on unmount
+		return () => {
+			globalEventEmitter.removeEventListener(eventName, listener)
+		}
+	}, [eventName, skipRerender])
+
+	return [eventData, dispatchEvent]
+}
