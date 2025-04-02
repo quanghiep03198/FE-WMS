b1f7717899fb10b7a69289a397316b4cb6d80832
quanghiep03198 <quanghiep03198@gmail.com>
2025-04-02 22:39:03 +0700

    refactor: enhance TableHead component styling for better alignment and responsiveness

--- src/components/ui/@core/table.tsx
+++ src/components/ui/@core/table.tsx
@@ -63,7 +63,7 @@ const TableHead = React.forwardRef<HTMLTableCellElement, React.ThHTMLAttributes<
 			ref={ref}
 			style={{ position: 'inherit' }}
 			className={cn(
-				'w-full bg-background px-4 py-2 font-semibold text-table-head-foreground group-hover:bg-secondary/50',
+				'w-full bg-background px-4 py-2 font-semibold text-table-head-foreground group-hover:bg-secondary/50 data-[type=number]:text-right [&:has([role=button])]:text-center [&:has([role=checkbox])]:text-center [&:has([role=combobox])]:p-0 [&:has([role=listbox])]:p-0 [&:has([role=textbox])]:p-0',
 				className
 			)}
 			{...props}
