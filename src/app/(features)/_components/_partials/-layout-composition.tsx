00ea051170a94e04fa6a326894ef6624c7dad363
quanghiep03198 <quanghiep03198@gmail.com>
2025-04-02 15:46:23 +0700

    feat: add MonthPickerFilter component and enhance PageHeader responsiveness

--- src/app/(features)/_components/_partials/-layout-composition.tsx
+++ src/app/(features)/_components/_partials/-layout-composition.tsx
@@ -9,7 +9,7 @@ const Container = tw(Div)<DivProps>`
 const ScrollArea = tw(ScrollShadow)<ScrollShadowProps>`
 	relative flex flex-grow flex-col overflow-y-auto !scrollbar-none overflow-x-hidden bg-background scrollbar scrollbar-track-transparent scrollbar-thumb-border dark:scrollbar-thumb-secondary
 `
-const Main = tw(Div)<DivProps>`grid w-full flex-1 basis-full grid-rows-[80px_auto] overflow-hidden`
+const Main = tw(Div)<DivProps>`grid w-full flex-1 basis-full grid-rows-[80px_auto]`
 
 const OutletWrapper = tw.div`sm:px-4 px-6 py-4 flex-1 basis-full [view-transition-name:main-content]`
 
