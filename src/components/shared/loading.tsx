6773e1995faa51bb0c58870b997744867f9018b5
quanghiep03198 <quanghiep03198@gmail.com>
2025-04-04 11:27:33 +0700

    refactor: update Loading component to use Fragment and add <head> for title

--- src/components/shared/loading.tsx
+++ src/components/shared/loading.tsx
@@ -2,7 +2,7 @@ import AppLogo from '@/app/_components/_shared/-app-logo'
 import GridBackground from '@/app/_components/_shared/-grid-background'
 import { useEventListener } from 'ahooks'
 import nProgress from 'nprogress'
-import { useEffect } from 'react'
+import { Fragment, useEffect } from 'react'
 import { Div } from '../ui'
 
 export default function Loading() {
@@ -20,11 +20,16 @@ export default function Loading() {
 	useEventListener('load', () => document.startViewTransition())
 
 	return (
-		<Div data-state='expanded' className='group relative flex h-screen items-center justify-center'>
-			<Div className='z-10 animate-[fade_.5s_cubic-bezier(.25,.25,.5,1)_.125s_both!important]'>
-				<AppLogo />
+		<Fragment>
+			<head>
+				<title>Loading ...</title>
+			</head>
+			<Div data-state='expanded' className='group relative flex h-screen items-center justify-center'>
+				<Div className='z-10 animate-[fade_.5s_cubic-bezier(.25,.25,.5,1)_.125s_both!important]'>
+					<AppLogo />
+				</Div>
+				<GridBackground />
 			</Div>
-			<GridBackground />
-		</Div>
+		</Fragment>
 	)
 }
