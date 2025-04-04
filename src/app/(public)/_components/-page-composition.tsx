14821de1e88a6eccbf19fd7cf1bb5cd6749204f8
quanghiep03198 <quanghiep03198@gmail.com>
2025-04-04 20:07:01 +0700

    refactor: update styling and structure of header, footer, and features section components

--- src/app/(public)/_components/-page-composition.tsx
+++ src/app/(public)/_components/-page-composition.tsx
@@ -15,7 +15,7 @@ const PageComposition: React.FC = () => {
 			style={{
 				'--primary-alt': 'hsl(var(--success))'
 			}}>
-			<Spotlight fill='white' className='-top-20 left-0 z-[-1] xl:-top-40' />
+			<Spotlight fill='white' className='-top-20 left-0 z-[-1] xl:-left-40 xl:-top-40' />
 			<Main ref={pageContext?.contentScrollRef}>
 				<Header />
 				<Div>
@@ -37,7 +37,7 @@ const PageComposition: React.FC = () => {
 }
 
 const Container = tw.div`relative h-screen z-10 overflow-hidden scroll-m-2 text-foreground antialiased group`
-const Main = tw.div`overflow-y-auto h-full scroll-smooth scrollbar-none flex flex-col items-stretch`
+const Main = tw.div`overflow-y-auto h-full scrollbar-none flex flex-col items-stretch`
 const Wrapper = tw.div`relative z-0 py-20 first:py-10 xxl:min-h-[70vh] sm:py-4 px-4 sm:mb-10 flex flex-grow justify-center items-stretch max-w-7xl xxl:max-w-8xl w-full mx-auto`
 
 export default PageComposition
