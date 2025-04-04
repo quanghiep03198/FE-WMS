4c8dad9780beffd55c12e26a1ebedef59c25808e
quanghiep03198 <quanghiep03198@gmail.com>
2025-04-04 22:30:47 +0700

    fix: use useWatch for improved form field tracking in PurchaseOrderAutoComplete

--- src/app/(features)/_layout.finished-production-outbound/_components/_outbound-form/-purchase-order-autocomplete.tsx
+++ src/app/(features)/_layout.finished-production-outbound/_components/_outbound-form/-purchase-order-autocomplete.tsx
@@ -2,13 +2,14 @@ import { useSearchPurchaseOrderQuery } from '@/app/(features)/_apis/use-order.ap
 import { AutoCompleteFieldControl } from '@/components/ui'
 import { useDebounce } from 'ahooks'
 import React from 'react'
-import { useFormContext } from 'react-hook-form'
+import { useFormContext, useWatch } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
 
 const PurchaseOrderAutoComplete: React.FC = () => {
 	const { t } = useTranslation()
-	const { watch } = useFormContext()
-	const debouncedSearchTerm = useDebounce(watch('po'), { wait: 500 })
+	const { control } = useFormContext()
+	const value = useWatch({ control, name: 'po' })
+	const debouncedSearchTerm = useDebounce(value, { wait: 500 })
 	const { data: purchaseOrders } = useSearchPurchaseOrderQuery(debouncedSearchTerm)
 
 	return (
