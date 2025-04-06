7a53e1242578016a55d9ca3883098bd6326cebc9
quanghiep03198 <quanghiep03198@gmail.com>
2025-04-06 23:36:39 +0700

    fix: adjust z-index and positioning of sidebar toggle button for improved visibility

--- src/components/ui/@core/sidebar.tsx
+++ src/components/ui/@core/sidebar.tsx
@@ -250,7 +250,7 @@ const SidebarRail = React.forwardRef<HTMLButtonElement, React.ComponentProps<'bu
 				onClick={toggleSidebar}
 				title='Toggle Sidebar'
 				className={cn(
-					'absolute inset-y-0 z-50 flex w-4 -translate-x-1/2 transition-all ease-linear after:absolute after:inset-y-1/2 after:left-full after:h-10 after:w-1.5 after:-translate-y-1/2 after:rounded group-data-[side=left]:-right-4 group-data-[side=right]:left-0 hover:after:bg-sidebar-border sm:hidden md:hidden',
+					'absolute inset-y-0 z-20 flex w-4 -translate-x-1/2 transition-all ease-linear after:absolute after:inset-y-0 after:left-1/2 after:w-[2px] group-data-[side=left]:-right-4 group-data-[side=right]:left-0 hover:after:bg-sidebar-border sm:hidden md:hidden',
 					'[[data-side=left]_&]:cursor-w-resize [[data-side=right]_&]:cursor-e-resize',
 					'[[data-side=left][data-state=collapsed]_&]:cursor-e-resize [[data-side=right][data-state=collapsed]_&]:cursor-w-resize',
 					'group-data-[collapsible=offcanvas]:translate-x-0 group-data-[collapsible=offcanvas]:after:left-full group-data-[collapsible=offcanvas]:hover:bg-sidebar',
