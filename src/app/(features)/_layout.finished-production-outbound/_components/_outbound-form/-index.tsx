78febf39ea0a4ba5c780048ee4810ce92dca9040
quanghiep03198 <quanghiep03198@gmail.com>
2025-04-26 17:05:09 +0700

    refactor(outbound-form): update tab titles for more clarity (#24)

--- src/app/(features)/_layout.finished-production-outbound/_components/_outbound-form/-index.tsx
+++ src/app/(features)/_layout.finished-production-outbound/_components/_outbound-form/-index.tsx
@@ -7,15 +7,15 @@ const OutboundForm: React.FC = () => {
 	return (
 		<Tabs defaultValue='standard' className='w-full flex-1 basis-full'>
 			<TabsList className='grid grid-cols-2'>
-				<TabsTrigger value='standard'>Standard</TabsTrigger>
+				<TabsTrigger value='standard'>Non-Separated PO</TabsTrigger>
 				<TabsTrigger value='detailed' className='w-full'>
-					Detailed
+					Separated PO
 				</TabsTrigger>
 			</TabsList>
-			<TabsContent value='standard' className='animate-in fade-in-0 slide-in-from-right-6'>
+			<TabsContent value='standard' className='animate-in fade-in-0 slide-in-from-right-4'>
 				<StandardOutboundForm />
 			</TabsContent>
-			<TabsContent value='detailed' className='animate-in fade-in-0 slide-in-from-left-6'>
+			<TabsContent value='detailed' className='animate-in fade-in-0 slide-in-from-left-4'>
 				<DetailedOutboundForm />
 			</TabsContent>
 		</Tabs>
