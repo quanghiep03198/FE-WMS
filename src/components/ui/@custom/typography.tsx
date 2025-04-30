3377afbd8af062da06a2d553a5573c3c6163e1b5
quanghiep03198 <quanghiep03198@gmail.com>
2025-05-01 00:46:31 +0700

    fix(typography): format JSX for better readability

--- src/components/ui/@custom/typography.tsx
+++ src/components/ui/@custom/typography.tsx
@@ -50,7 +50,10 @@ export const Typography = forwardRef<HTMLElement, TypographyProps>((props, ref) 
 	const Element = !variant || variant === 'default' || as ? as : (variant as React.ElementType)
 
 	return (
-		<Element ref={resolvedRef} className={cn(typographyVariants({ variant, color, className }))} {...restProps}>
+		<Element
+			ref={resolvedRef}
+			className={cn(typographyVariants({ variant, color, className: className ?? 'text-inherit' }))}
+			{...restProps}>
 			{children}
 		</Element>
 	)
