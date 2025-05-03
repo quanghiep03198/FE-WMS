258b5dd2dc1c52d2b5da1c292ed4758aec3ec958
quanghiep03198 <quanghiep03198@gmail.com>
2025-05-03 14:32:35 +0700

    fix(vite.config): change PWA registration type from prompt to autoUpdate

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,7 @@ export default defineConfig(({ mode }) => {
 				targets: [{ src: './infrastructure/web.config', dest: '' }]
 			}),
 			pwa({
-				registerType: 'prompt',
+				registerType: 'autoUpdate',
 				disable: mode === 'development',
 				includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
 				manifest: {
