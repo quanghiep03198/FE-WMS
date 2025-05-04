fb1df2b3c431727e924570a152f3fc78c95899c7
quanghiep03198 <quanghiep03198@gmail.com>
2025-05-04 20:37:50 +0700

    refactor(notification): simplify props usage and enhance icon styling

--- src/app/(features)/_components/_partials/-notifications.tsx
+++ src/app/(features)/_components/_partials/-notifications.tsx
@@ -2,9 +2,7 @@ import { cn } from '@/common/utils/cn'
 import { buttonVariants, Icon, Popover, PopoverContent, PopoverTrigger, Typography } from '@/components/ui'
 import React from 'react'
 
-type Props = {}
-
-const Notification: React.FC = (props: Props) => {
+const Notification: React.FC = () => {
 	return (
 		<Popover>
 			<PopoverTrigger className={cn(buttonVariants({ variant: 'ghost', size: 'icon' }))}>
@@ -16,7 +14,7 @@ const Notification: React.FC = (props: Props) => {
 				sideOffset={16}
 				className='grid h-full max-h-80 min-h-64 place-content-center'>
 				<Typography variant='small' color='muted' className='flex items-center justify-center gap-x-2'>
-					<Icon name='Inbox' />
+					<Icon name='Inbox' className='size-6' strokeWidth={1.5} />
 					Empty
 				</Typography>
 			</PopoverContent>
