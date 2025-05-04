5d22b2a0dc74b7b78336cf502a7c7d3fd5164a12
quanghiep03198 <quanghiep03198@gmail.com>
2025-05-04 20:52:29 +0700

    refactor(app): integrate QueryErrorResetBoundary for improved error handling

--- src/__app.tsx
+++ src/__app.tsx
@@ -1,4 +1,5 @@
 import * as Sentry from '@sentry/react'
+import { QueryErrorResetBoundary } from '@tanstack/react-query'
 import React from 'react'
 import { I18nextProvider } from 'react-i18next'
 import { ErrorBoundaryFallback } from './app/_components/_errors/-error-boundary-fallback'
@@ -11,24 +12,35 @@ import { ThemeProvider } from './providers/theme-provider'
 
 const App: React.FC = () => {
 	return (
-		<Sentry.ErrorBoundary
-			fallback={({ error, resetError, ...props }) => (
-				<ErrorBoundaryFallback error={error as Error} resetError={resetError} {...props} />
-			)}
-			showDialog>
-			<QueryClientProvider>
-				<I18nextProvider i18n={i18n}>
-					<ThemeProvider>
-						<RouterProvider />
-						<Toaster
-							className='pointer-events-auto'
-							position='bottom-right'
-							duration={AppConfigs.TOAST_DURATION}
+		<QueryErrorResetBoundary>
+			{({ reset: resetQueryError }) => (
+				<Sentry.ErrorBoundary
+					fallback={({ error, resetError, ...props }) => (
+						<ErrorBoundaryFallback
+							error={error as Error}
+							resetError={() => {
+								resetQueryError()
+								resetError()
+							}}
+							{...props}
 						/>
-					</ThemeProvider>
-				</I18nextProvider>
-			</QueryClientProvider>
-		</Sentry.ErrorBoundary>
+					)}
+					showDialog={true}>
+					<QueryClientProvider>
+						<I18nextProvider i18n={i18n}>
+							<ThemeProvider>
+								<RouterProvider />
+								<Toaster
+									className='pointer-events-auto'
+									position='bottom-right'
+									duration={AppConfigs.TOAST_DURATION}
+								/>
+							</ThemeProvider>
+						</I18nextProvider>
+					</QueryClientProvider>
+				</Sentry.ErrorBoundary>
+			)}
+		</QueryErrorResetBoundary>
 	)
 }
 
