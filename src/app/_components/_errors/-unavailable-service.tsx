7d12cf40b588f52109c5dec2657b797151101855
quanghiep03198 <quanghiep03198@gmail.com>
2025-05-06 18:58:58 +0700

    fix(unavailable-service): add missing class to Typography and define Image component

--- src/app/_components/_errors/-unavailable-service.tsx
+++ src/app/_components/_errors/-unavailable-service.tsx
@@ -1,6 +1,7 @@
 import { Div, Typography } from '@/components/ui'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import tw from 'tailwind-styled-components'
 
 const UnavailableService: React.FC = () => {
 	const { t } = useTranslation('ns_common')
@@ -11,11 +12,14 @@ const UnavailableService: React.FC = () => {
 				503
 			</Typography>
 			<Typography variant='h3'>{t('ns_common:errors.503')}</Typography>
-			<Typography variant='p' color='muted'>
+			<Typography variant='p' color='muted' className='mb-4'>
 				{t('ns_common:errors.503_message')}
 			</Typography>
+			<Image src='/onboarding.svg' />
 		</Div>
 	)
 }
+
+const Image = tw.img`max-w-md object-center object-contain w-full animate-[fade-in_0.35s_ease-out_0s]`
 
 export default UnavailableService
