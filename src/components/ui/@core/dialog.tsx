9a7b83a88cf85d521629205467bbe88d7a0f000a
quanghiep03198 <quanghiep03198@gmail.com>
2025-05-09 17:49:51 +0700

    fix(dialog): add relative positioning to DialogContent for improved layout

--- src/components/ui/@core/dialog.tsx
+++ src/components/ui/@core/dialog.tsx
@@ -40,7 +40,7 @@ const DialogContent: React.FC<React.ComponentProps<typeof DialogPrimitive.Conten
 			<DialogPrimitive.Content
 				ref={ref}
 				className={cn(
-					'z-50 grid w-full max-w-lg gap-4 rounded-lg border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] sm:rounded-none md:h-screen md:rounded-none',
+					'relative z-50 grid w-full max-w-lg gap-4 rounded-lg border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] sm:rounded-none md:h-screen md:rounded-none',
 					className
 				)}
 				{...props}>
