c7d2318926e87231f5cd5ab01e62b592dfbd3975
quanghiep03198 <quanghiep03198@gmail.com>
2025-05-18 02:12:37 +0700

    refactor(theme-toggle): enhance theme toggle functionality with useUpdateEffect

--- src/app/_components/_shared/-theme-toggle.tsx
+++ src/app/_components/_shared/-theme-toggle.tsx
@@ -1,14 +1,15 @@
 import { Theme } from '@/common/constants/enums'
 import useTheme from '@/common/hooks/use-theme'
 import { Button, Icon, Tooltip, TooltipProps } from '@/components/ui'
-import { useKeyPress } from 'ahooks'
-import React from 'react'
+import { useKeyPress, useUpdateEffect } from 'ahooks'
+import React, { useRef } from 'react'
 import { useTranslation } from 'react-i18next'
 
 const ThemeToggle: React.FC<
 	React.ComponentProps<typeof Button.prototype> & { tooltipProps: Partial<TooltipProps> }
 > = ({ variant = 'ghost', tooltipProps }) => {
 	const { theme, setTheme } = useTheme()
+	const iconRef = useRef<SVGSVGElement>(null)
 	const darkTheme = theme === Theme.DARK
 	const { t } = useTranslation()
 	const toggleTheme = () => setTheme(darkTheme ? Theme.LIGHT : Theme.DARK)
@@ -18,6 +19,14 @@ const ThemeToggle: React.FC<
 		toggleTheme()
 	})
 
+	useUpdateEffect(() => {
+		if (iconRef.current) {
+			iconRef.current.classList.value = ''
+			if (theme === Theme.DARK) iconRef.current.classList.add('animate-[rotate-in-reverse_0.25s_ease-out_forwards]')
+			else iconRef.current.classList.add('animate-[rotate-in_0.25s_ease-out_forwards]')
+		}
+	}, [iconRef, theme])
+
 	return (
 		<Tooltip message={t('ns_common:actions.toggle_theme')} triggerProps={{ asChild: true }} {...tooltipProps}>
 			<Button
@@ -26,15 +35,7 @@ const ThemeToggle: React.FC<
 				className='relative'
 				aria-pressed={darkTheme}
 				onClick={() => toggleTheme()}>
-				<Icon
-					aria-label={darkTheme ? 'Dark Mode' : 'Light Mode'}
-					name={darkTheme ? 'Moon' : 'Sun'}
-					className={
-						darkTheme
-							? 'animate-[rotate-in-reverse_0.25s_ease-out_forwards]'
-							: 'animate-[rotate-in_0.25s_ease-out_forwards]'
-					}
-				/>
+				<Icon aria-label={darkTheme ? 'Dark Mode' : 'Light Mode'} name={darkTheme ? 'Moon' : 'Sun'} ref={iconRef} />
 			</Button>
 		</Tooltip>
 	)
