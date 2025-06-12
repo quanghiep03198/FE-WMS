d60ed38a9498446ab31c3db7fa8295fc5684bcf5
quanghiep03198 <quanghiep03198@gmail.com>
2025-06-12 19:20:45 +0700

    fix(__root.tsx): remove QueryErrorResetBoundary for simplified component structure

--- src/app/__root.tsx
+++ src/app/__root.tsx
@@ -1,13 +1,11 @@
 import { Div } from '@/components/ui'
-import { QueryClient, QueryErrorResetBoundary } from '@tanstack/react-query'
+import { QueryClient } from '@tanstack/react-query'
 import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
 
 export const Route = createRootRouteWithContext<{ queryClient: QueryClient; isAuthenticated: boolean }>()({
 	component: () => (
-		<QueryErrorResetBoundary>
-			<Div style={{ viewTransitionName: 'app' }}>
-				<Outlet />
-			</Div>
-		</QueryErrorResetBoundary>
+		<Div style={{ viewTransitionName: 'app' }}>
+			<Outlet />
+		</Div>
 	)
 })
