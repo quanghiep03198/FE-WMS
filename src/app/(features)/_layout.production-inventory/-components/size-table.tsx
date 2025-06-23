be8c1d75e48e0cfe764a2fd61a97f74a9f376be2
quanghiep03198 <quanghiep03198@gmail.com>
2025-06-23 10:10:27 +0700

    fix(size-table): update header styles for improved visibility and consistency

--- src/app/(features)/_layout.production-inventory/-components/size-table.tsx
+++ src/app/(features)/_layout.production-inventory/-components/size-table.tsx
@@ -25,7 +25,7 @@ const SizeTable: React.FC<SizeTableProps> = ({ data, total, ...props }) => {
 	return (
 		<Table {...props}>
 			<TableRow className='[&>*]:border-b'>
-				<TableVerticalHeader className='text-foreground'>Size</TableVerticalHeader>
+				<TableVerticalHeader>Size</TableVerticalHeader>
 				{sortedData.map((item) => (
 					<TableCellHead variant='small' key={item.size_numcode} className='bg-table-head'>
 						{item.size_numcode}
@@ -38,9 +38,7 @@ const SizeTable: React.FC<SizeTableProps> = ({ data, total, ...props }) => {
 				)}
 			</TableRow>
 			<TableRow>
-				<TableVerticalHeader className='text-foreground'>
-					{t('ns_common:common_fields.quantity')}
-				</TableVerticalHeader>
+				<TableVerticalHeader>{t('ns_common:common_fields.quantity')}</TableVerticalHeader>
 				{sortedData.map((item) => (
 					<TableCell key={item.size_numcode}>{formatIntlNumber(item.qty)}</TableCell>
 				))}
@@ -50,10 +48,10 @@ const SizeTable: React.FC<SizeTableProps> = ({ data, total, ...props }) => {
 	)
 }
 
-const Table = tw.div`[&>*>:last-child]:top-0 [&>*>:first-child]:basis-28 [&>*>:first-child]:min-w-28 [&>*>:first-child]:bg-background [&>*>:first-child]:sticky [&>*>:first-child]:left-0 [&>*]:text-sm w-full overflow-x-auto`
+const Table = tw.div`relative [&>*>:last-child]:top-0 [&>*>:first-child]:basis-28 [&>*>:first-child]:min-w-28 [&>*>:first-child]:bg-background [&>*>:first-child]:sticky [&>*>:first-child]:left-0 [&>*]:text-sm w-full overflow-x-auto`
 const TableRow = tw.div`flex [&>*]:px-4 [&>:not(:last-child)]:border-l [&>*]:py-2 [&>*]:whitespace-nowrap [&>:last-child]:drop-shadow-[1px_0px_0px_hsl(var(--border))] [&>:first-child]:border-l-0 [&>:last-child]:border-r-0 [&>:last-child]:border-l [&>:last-child]:flex-1 [&>*]:basis-24 [&>*]:min-w-24`
 const TableCell = tw.div`text-foreground text-left bg-background`
 const TableCellHead = tw.div`text-table-head-foreground font-medium text-left bg-table-head`
-const TableVerticalHeader = tw.div`sticky left-0 z-10 lowercase first-letter:uppercase !border-l-0 shadow-[1px_0px_hsl(var(--border))] font-medium`
+const TableVerticalHeader = tw.div`sticky left-0 z-10 lowercase first-letter:uppercase !border-l-0 shadow-[1px_0px_hsl(var(--border))] font-medium text-foreground`
 
 export default SizeTable
