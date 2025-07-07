605b9248c3c5e32465e82cb2a8a60f47173527a7
quanghiep03198 <quanghiep03198@gmail.com>
2025-07-07 23:45:28 +0700

    fix: refactor table components to use React.FC for improved type safety

--- src/components/ui/@core/table.tsx
+++ src/components/ui/@core/table.tsx
@@ -2,95 +2,73 @@ import * as React from 'react'
 
 import { cn } from '@/common/utils/cn'
 
-const Table = React.forwardRef<HTMLTableElement, React.HTMLAttributes<HTMLTableElement>>(
-	({ className, ...props }, ref) => (
-		<table
-			cellSpacing={0}
-			ref={ref}
-			className={cn('w-full caption-bottom border-collapse rounded-md text-sm', className)}
-			{...props}
-		/>
-	)
+const Table: React.FC<React.ComponentProps<'table'>> = ({ className, ...props }) => (
+	<table
+		cellSpacing={0}
+		className={cn('w-full caption-bottom border-collapse rounded-md text-sm', className)}
+		{...props}
+	/>
 )
+
 Table.displayName = 'Table'
 
-const TableHeader = React.forwardRef<HTMLTableSectionElement, React.HTMLAttributes<HTMLTableSectionElement>>(
-	({ className, ...props }, ref) => (
-		<thead ref={ref} className={cn('[&>tr>th:first-child]:border-l-0', className)} {...props} />
-	)
+const TableHeader: React.FC<React.ComponentProps<'thead'>> = ({ className, ...props }) => (
+	<thead className={cn('[&>tr>th:first-child]:border-l-0', className)} {...props} />
 )
 TableHeader.displayName = 'TableHeader'
 
-const TableBody = React.forwardRef<HTMLTableSectionElement, React.HTMLAttributes<HTMLTableSectionElement>>(
-	({ className, ...props }, ref) => (
-		<tbody
-			ref={ref}
-			className={cn('[&>tr>td:first-child]:border-l-0 [&>tr]:last:border-b-0', className)}
-			{...props}
-		/>
-	)
+const TableBody: React.FC<React.ComponentProps<'tbody'>> = ({ className, ...props }) => (
+	<tbody className={cn('[&>tr>td:first-child]:border-l-0 [&>tr]:last:border-b-0', className)} {...props} />
 )
+
 TableBody.displayName = 'TableBody'
 
-const TableFooter = React.forwardRef<HTMLTableSectionElement, React.HTMLAttributes<HTMLTableSectionElement>>(
-	({ className, ...props }, ref) => (
-		<tfoot
-			ref={ref}
-			className={cn('border-t bg-muted/50 font-medium [&>tr]:last:border-b-0', className)}
-			{...props}
-		/>
-	)
+const TableFooter: React.FC<React.ComponentProps<'tfoot'>> = ({ className, ...props }) => (
+	<tfoot className={cn('border-t bg-muted/50 font-medium [&>tr]:last:border-b-0', className)} {...props} />
 )
+
 TableFooter.displayName = 'TableFooter'
 
-const TableRow = React.forwardRef<HTMLTableRowElement, React.HTMLAttributes<HTMLTableRowElement>>(
-	({ className, ...props }, ref) => (
-		<tr
-			ref={ref}
-			className={cn(
-				'[&:has([aria-disabled=true])_td]:bg-muted [&:has([aria-disabled=true])_td]:text-muted-foreground [&:last-child>td]:border-b-0 [&>*]:border-b [&>*]:border-l',
-				className
-			)}
-			{...props}
-		/>
-	)
+const TableRow: React.FC<React.ComponentProps<'tr'>> = ({ className, ...props }) => (
+	<tr
+		className={cn(
+			'[&:has([aria-disabled=true])_td]:bg-muted [&:has([aria-disabled=true])_td]:text-muted-foreground [&:last-child>td]:border-b-0 [&>*:first-child]:border-l-0 [&>*]:border-b [&>*]:border-l',
+			className
+		)}
+		{...props}
+	/>
 )
+
 TableRow.displayName = 'TableRow'
 
-const TableHead = React.forwardRef<HTMLTableCellElement, React.ThHTMLAttributes<HTMLTableCellElement>>(
-	({ className, ...props }, ref) => (
-		<th
-			ref={ref}
-			style={{ position: 'inherit' }}
-			className={cn(
-				'w-full bg-background px-4 py-2 font-semibold text-table-head-foreground group-hover:bg-secondary/50 data-[type=number]:text-right [&:has([role=button])]:text-center [&:has([role=checkbox])]:text-center [&:has([role=combobox])]:p-0 [&:has([role=listbox])]:p-0 [&:has([role=textbox])]:p-0',
-				className
-			)}
-			{...props}
-		/>
-	)
+const TableHead: React.FC<React.ComponentProps<'th'>> = ({ className, ...props }) => (
+	<th
+		style={{ position: 'inherit' }}
+		className={cn(
+			'w-full bg-background px-4 py-2 font-semibold text-table-head-foreground group-hover:bg-secondary/50 data-[type=number]:text-right [&:has([role=button])]:text-center [&:has([role=checkbox])]:text-center [&:has([role=combobox])]:p-0 [&:has([role=listbox])]:p-0 [&:has([role=textbox])]:p-0',
+			className
+		)}
+		{...props}
+	/>
 )
+
 TableHead.displayName = 'TableHead'
 
-const TableCell = React.forwardRef<HTMLTableCellElement, React.TdHTMLAttributes<HTMLTableCellElement>>(
-	({ className, ...props }, ref) => (
-		<td
-			ref={ref}
-			className={cn(
-				'bg-background px-4 py-2 first:border-l-0 last:border-r-0 group-hover:bg-table-row-active group-aria-expanded:bg-table-row-active group-aria-selected:bg-table-row-selected data-[disabled=true]:bg-muted data-[type=number]:text-right [&:has([role=button])]:text-center [&:has([role=checkbox])]:text-center [&:has([role=combobox])]:p-0 [&:has([role=listbox])]:p-0 [&:has([role=textbox])]:p-0',
-				className
-			)}
-			{...props}
-		/>
-	)
+const TableCell: React.FC<React.ComponentProps<'td'>> = ({ className, ...props }) => (
+	<td
+		className={cn(
+			'bg-background px-4 py-2 first:border-l-0 last:border-r-0 group-hover:bg-table-row-active group-aria-expanded:bg-table-row-active group-aria-selected:bg-table-row-selected data-[disabled=true]:bg-muted data-[type=number]:text-right [&:has([role=button])]:text-center [&:has([role=checkbox])]:text-center [&:has([role=combobox])]:p-0 [&:has([role=listbox])]:p-0 [&:has([role=textbox])]:p-0',
+			className
+		)}
+		{...props}
+	/>
 )
 TableCell.displayName = 'TableCell'
 
-const TableCaption = React.forwardRef<HTMLTableCaptionElement, React.HTMLAttributes<HTMLTableCaptionElement>>(
-	({ className, ...props }, ref) => (
-		<caption ref={ref} className={cn('mt-4 text-sm text-muted-foreground', className)} {...props} />
-	)
+const TableCaption: React.FC<React.ComponentProps<'tfoot'>> = ({ className, ...props }) => (
+	<caption className={cn('mt-4 text-sm text-muted-foreground', className)} {...props} />
 )
+
 TableCaption.displayName = 'TableCaption'
 
 export { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow }
