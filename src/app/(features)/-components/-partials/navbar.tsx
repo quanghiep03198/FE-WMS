8b9015ef77c670197aed75ab1314a9d9f9396eed
quanghiep03198 <quanghiep03198@gmail.com>
2025-07-09 13:18:06 +0700

    fix: replace Fragment with Div in Navbar component for improved semantics

--- src/app/(features)/-components/-partials/navbar.tsx
+++ src/app/(features)/-components/-partials/navbar.tsx
@@ -2,7 +2,7 @@ import { LanguageDropdown } from '@/app/-components/-shared/language-selector'
 import useAuth from '@/common/hooks/use-auth'
 import { Div, Separator } from '@/components/ui'
 import { useKeyPress } from 'ahooks'
-import React, { Fragment } from 'react'
+import React from 'react'
 import ThemeToggle from '../../../-components/-shared/theme-toggle'
 import NavBreadcrumb from './nav-breadcrumb'
 import NavSidebarToggler from './nav-sidebar-toggler'
@@ -19,30 +19,28 @@ const Navbar: React.FC = () => {
 	})
 
 	return (
-		<Fragment>
+		<Div
+			as='header'
+			role='menubar'
+			className='sticky top-0 z-20 flex h-[var(--header-height)] items-center bg-background px-6 sm:px-4'>
 			<Div
-				as='header'
-				role='menubar'
-				className='sticky top-0 z-20 flex h-[var(--header-height)] items-center bg-background px-6 sm:px-4'>
-				<Div
-					as='nav'
-					role='menu'
-					className='flex w-full basis-full items-center justify-between rounded-md border border-border px-3 py-2'>
-					<Div role='group' className='flex items-center gap-x-4'>
-						<NavSidebarToggler />
-						<Separator orientation='vertical' className='hidden h-5 w-1 xl:block' />
-						<NavBreadcrumb />
-					</Div>
-					<Div role='group' className='flex flex-1 items-center justify-end gap-x-2'>
-						<SearchDialog />
-						<LanguageDropdown triggerProps={{ variant: 'ghost' }} />
-						<ThemeToggle />
-						<Notification />
-						<NavUserControl />
-					</Div>
+				as='nav'
+				role='menu'
+				className='flex w-full basis-full items-center justify-between rounded-md border border-border px-3 py-2'>
+				<Div role='group' className='flex items-center gap-x-4'>
+					<NavSidebarToggler />
+					<Separator orientation='vertical' className='hidden h-5 w-1 xl:block' />
+					<NavBreadcrumb />
+				</Div>
+				<Div role='group' className='flex flex-1 items-center justify-end gap-x-2'>
+					<SearchDialog />
+					<LanguageDropdown triggerProps={{ variant: 'ghost' }} />
+					<ThemeToggle />
+					<Notification />
+					<NavUserControl />
 				</Div>
 			</Div>
-		</Fragment>
+		</Div>
 	)
 }
 
