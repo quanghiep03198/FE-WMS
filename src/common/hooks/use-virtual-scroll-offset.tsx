3d18b3adc8e10d214f862cc4fab0a92637c1bf8a
quanghiep03198 <quanghiep03198@gmail.com>
2025-07-14 23:20:52 +0700

    refactor: replace useMemo with useRef for improved performance in useVirutalScrollOffset hook

--- src/common/hooks/use-virtual-scroll-offset.tsx
+++ src/common/hooks/use-virtual-scroll-offset.tsx
@@ -1,20 +1,19 @@
 import { notUndefined, Virtualizer } from '@tanstack/react-virtual'
-import { useMemo } from 'react'
+import { useRef } from 'react'
 
 export default function useVirutalScrollOffset(virtualizer: Virtualizer<any, any>) {
 	const virtualItems = virtualizer.getVirtualItems()
 
-	return useMemo(
-		() =>
-			virtualItems?.length > 0
-				? {
-						before: notUndefined(virtualItems[0]).start - virtualizer.options.scrollMargin,
-						after:
-							virtualItems?.length > 0
-								? virtualizer.getTotalSize() - notUndefined(virtualItems[virtualItems?.length - 1]).end
-								: 0
-					}
-				: { before: 0, after: 0 },
-		[virtualItems]
-	)
+	const offsetRef = useRef<Record<'before' | 'after', number>>({ before: 0, after: 0 })
+
+	if (virtualItems?.length > 0)
+		offsetRef.current = {
+			before: notUndefined(virtualItems[0]).start - virtualizer.options.scrollMargin,
+			after:
+				virtualItems?.length > 0
+					? virtualizer.getTotalSize() - notUndefined(virtualItems[virtualItems?.length - 1]).end
+					: 0
+		}
+
+	return offsetRef.current
 }
