157fcd9800dbffcbeca7418b7cb210fb4cc3d02b
quanghiep03198 <quanghiep03198@gmail.com>
2025-07-15 23:55:02 +0700

    refactor(checkbox): convert Checkbox to functional component and remove ref forwarding

--- src/components/ui/@core/checkbox.tsx
+++ src/components/ui/@core/checkbox.tsx
@@ -4,12 +4,8 @@ import * as React from 'react'
 
 import { cn } from '@/common/utils/cn'
 
-const Checkbox = React.forwardRef<
-	React.ElementRef<typeof CheckboxPrimitive.Root>,
-	React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
->(({ className, ...props }, ref) => (
+const Checkbox: React.FC<React.ComponentProps<typeof CheckboxPrimitive.Root>> = ({ className, ...props }) => (
 	<CheckboxPrimitive.Root
-		ref={ref}
 		className={cn(
 			'peer h-4 w-4 shrink-0 rounded-sm border border-primary shadow data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-transparent disabled:cursor-not-allowed disabled:opacity-50',
 			className
@@ -19,7 +15,7 @@ const Checkbox = React.forwardRef<
 			<CheckIcon className='h-4 w-4' />
 		</CheckboxPrimitive.Indicator>
 	</CheckboxPrimitive.Root>
-))
+)
 Checkbox.displayName = CheckboxPrimitive.Root.displayName
 
 export { Checkbox }
