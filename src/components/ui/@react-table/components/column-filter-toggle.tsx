9ef87c7d850c9802bda4f53468aff6d7e119e9eb
quanghiep03198 <quanghiep03198@gmail.com>
2025-07-18 00:21:37 +0700

    refactor(column-filter-toggle): enhance toggle state management and improve context usage

--- src/components/ui/@react-table/components/column-filter-toggle.tsx
+++ src/components/ui/@react-table/components/column-filter-toggle.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Icon } from '../../@core/icon'
 import { Toggle } from '../../@core/toggle'
@@ -6,14 +7,19 @@ import { useTableContext } from '../context/table.context'
 
 const ColumnFilterToggle: React.FC = () => {
 	const { t } = useTranslation()
-	const { event$ } = useTableContext('event$')
+	const { event$, defaultFilterOpen } = useTableContext('event$', 'defaultFilterOpen')
+	const [pressed, setPressed] = useState<boolean>(defaultFilterOpen)
 
 	return (
 		<Tooltip message={t('ns_common:table.filter')} triggerProps={{ asChild: true }}>
 			<Toggle
 				variant='outline'
 				className='size-9 place-content-center p-0 aria-pressed:bg-accent aria-pressed:text-accent-foreground hover:text-foreground'
-				onPressedChange={(pressed) => event$.emit({ shouldFilterOpen: pressed })}>
+				pressed={pressed}
+				onPressedChange={(pressed) => {
+					setPressed(pressed)
+					event$.emit({ shouldFilterOpen: pressed })
+				}}>
 				<Icon name='Filter' />
 			</Toggle>
 		</Tooltip>
