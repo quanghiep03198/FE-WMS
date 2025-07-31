9a2fbb1c823585a4d48f4237b11d611a39f771c9
quanghiep03198 <quanghiep03198@gmail.com>
2025-07-31 10:18:29 +0700

    refactor(alert): enhance Alert component to accept additional div props

--- src/components/ui/@custom/alert.tsx
+++ src/components/ui/@custom/alert.tsx
@@ -1,6 +1,6 @@
 import tw from 'tailwind-styled-components'
 
-export const Alert = tw.div`data-[state=open]:animate-in shadow-lg data-[state=open]:fade-in-0 [transition-behavior:allow-discrete] data-[state=open]:slide-in-from-top-4 transition-all data-[state=closed]:hidden data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:slide-out-to-top-4 fixed top-0 left-0 right-auto flex items-center w-full bg-destructive text-destructive-foreground px-4 py-3 z-50 gap-3`
+export const Alert = tw.div`data-[state=open]:animate-in shadow-lg data-[state=open]:fade-in-0 [transition-behavior:allow-discrete] data-[state=open]:slide-in-from-top-4 transition-all data-[state=closed]:hidden data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:slide-out-to-top-4 fixed top-0 left-0 right-auto flex items-center w-full bg-destructive text-destructive-foreground px-4 py-3 z-[9999] gap-3`
 export const AlertContent = tw.div`inline-flex flex-col`
 export const AlertTitle = tw.h5`font-medium`
 export const AlertDescription = tw.p`text-sm`
