015e64c8811d0e32e31e3a72cea9258a3130ce13
quanghiep03198 <quanghiep03198@gmail.com>
2025-08-02 15:34:16 +0700

    refactor(outbound-report): update report hooks and implement new outbound report query

---
+++ src/app/(features)/_layout.outbound-report/-hooks/use-outbound-report-asm.tsx
@@ -0,0 +1,21 @@
+import { ReportService } from '@/services/report.service'
+import { useQuery } from '@tanstack/react-query'
+import { pick } from 'lodash'
+
+export enum OutboundReportQueryKeys {
+	DAILY_OUTBOUND = 'DAILY_OUTBOUND'
+}
+
+export const useGetOutboundReport = (
+	tenantId: string,
+	params?: { 'auto-refresh': false | number; 'date.eq': string }
+) => {
+	return useQuery({
+		queryKey: [OutboundReportQueryKeys.DAILY_OUTBOUND, tenantId, pick(params, 'date.eq')],
+		queryFn: async () => await ReportService.getOutboundReport(tenantId, pick(params, 'date.eq')),
+		enabled: !!tenantId,
+		refetchOnMount: true,
+		refetchInterval: params['auto-refresh'],
+		select: (response) => response.metadata
+	})
+}
