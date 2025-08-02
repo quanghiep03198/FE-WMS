04bdebe0b37b84726977cb2add82091ccc532a45
quanghiep03198 <quanghiep03198@gmail.com>
2025-08-02 15:33:41 +0700

    refactor(report): update inbound report hooks and add new query implementation

---
+++ src/app/(features)/_layout.inbound-report/-hooks/use-inbound-report-asm.tsx
@@ -0,0 +1,20 @@
+import { ReportService } from '@/services/report.service'
+import { useQuery } from '@tanstack/react-query'
+import { pick } from 'lodash'
+
+export enum InboundReportQueryKeys {
+	DAILY_INBOUND = 'DAILY_INBOUND'
+}
+
+export const useGetInboundReport = (
+	tenantId: string,
+	params?: { 'auto-refresh': false | number; 'date.eq': string }
+) => {
+	return useQuery({
+		queryKey: [InboundReportQueryKeys.DAILY_INBOUND, tenantId, pick(params, 'date.eq')],
+		queryFn: async () => await ReportService.getInboundReport(tenantId, pick(params, 'date.eq')),
+		enabled: !!tenantId,
+		refetchInterval: params['auto-refresh'],
+		select: (response) => response.metadata
+	})
+}
