29a67fcb963e6f85a6f66f68f06034b928ebfb62
quanghiep03198 <quanghiep03198@gmail.com>
2025-08-02 15:38:13 +0700

    refactor(order-asm): declare enum for order queries

---
+++ src/app/(features)/-hooks/use-order-asm.tsx
@@ -0,0 +1,42 @@
+import { OrderService } from '@/services/order.service'
+import { useQuery } from '@tanstack/react-query'
+
+export enum OrderQueryKeys {
+	SEARCH_COMMAND_NUMBER = 'SEARCH_COMMAND_NUMBER',
+	SEARCH_PURCHASE_ORDER = 'SEARCH_PURCHASE_ORDER',
+	COMMAND_NUMBER_DETAIL = 'COMMAND_NUMBER_DETAIL'
+}
+
+export const useSearchCommandNumberQuery = (searchTerm: string, shouldFetch = true) => {
+	return useQuery({
+		queryKey: [OrderQueryKeys.SEARCH_COMMAND_NUMBER, searchTerm],
+		queryFn: async () => await OrderService.searchCommandNumber({ q: searchTerm }),
+		enabled: shouldFetch,
+		select: (response) => {
+			if (!Array.isArray(response.metadata)) return []
+			return response.metadata
+		}
+	})
+}
+
+export const useSearchPurchaseOrderQuery = (searchTerm: string, shouldFetch = true) => {
+	return useQuery({
+		queryKey: [OrderQueryKeys.SEARCH_PURCHASE_ORDER, searchTerm],
+		queryFn: async () => await OrderService.searchPurchaseOrder({ q: searchTerm }),
+		enabled: shouldFetch,
+		select: (response) => {
+			if (!Array.isArray(response.metadata)) return []
+			return response.metadata
+		}
+	})
+}
+
+export const useGetCommandNumberDetailQuery = (commandNumber: string) => {
+	return useQuery({
+		queryKey: [OrderQueryKeys.COMMAND_NUMBER_DETAIL, commandNumber],
+		queryFn: async () => await OrderService.getCommandNumberDetail(commandNumber),
+		enabled: !!commandNumber,
+		refetchOnMount: true,
+		select: (response) => response.metadata
+	})
+}
