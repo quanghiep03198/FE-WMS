542b569c9404f8461b7bf6cab141f864030a6b2a
quanghiep03198 <quanghiep03198@gmail.com>
2025-08-17 23:00:53 +0700

    feat(defective-goods): add mutation for creating defective goods and update query function

--- src/app/(features)/_layout.b-grade-goods-inbound/-hooks/use-defective-goods-asm.tsx
+++ src/app/(features)/_layout.b-grade-goods-inbound/-hooks/use-defective-goods-asm.tsx
@@ -1,7 +1,8 @@
 import useQueryParams from '@/common/hooks/use-query-params'
 import { IDefectiveGoods } from '@/common/types/entities'
 import { DefectiveGoodsService } from '@/services/defective-goods.service'
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
+import { CreateDefectiveGoodsFormValues } from '../-schemas/defective-goods.schema'
 
 export enum DefectiveGoodsQueryKey {
 	DEFECTIVE_GOODS = 'DEFECTIVE_GOODS'
@@ -12,6 +13,16 @@ export const useGetDefectiveGoodsQuery = () => {
 
 	return useQuery({
 		queryKey: [DefectiveGoodsQueryKey.DEFECTIVE_GOODS, searchParams],
-		queryFn: async () => await DefectiveGoodsService.getDefectiveGoodsList(searchParams)
+		queryFn: async () => await DefectiveGoodsService.getDefectiveGoods(searchParams),
+		select: (response) => response.metadata
+	})
+}
+
+export const useCreateDefectiveGoodsMutation = () => {
+	const { searchParams } = useQueryParams<Pick<Pagination<IDefectiveGoods>, 'page' | 'limit'>>({ page: 1, limit: 10 })
+
+	return useMutation({
+		mutationKey: [DefectiveGoodsQueryKey.DEFECTIVE_GOODS, searchParams],
+		mutationFn: async (data: CreateDefectiveGoodsFormValues) => await DefectiveGoodsService.createDefectiveGoods(data)
 	})
 }
--- src/services/defective-goods.service.ts
+++ src/services/defective-goods.service.ts
@@ -1,11 +1,24 @@
 // import { CreateDefectiveGoodsFormValues } from '@/app/(features)/_layout.b-grade-goods-inbound/-schemas/defective-goods.schema'
+import {
+	CreateDefectiveGoodsFormValues,
+	UpdateDefectiveGoodsFormValues
+} from '@/app/(features)/_layout.b-grade-goods-inbound/-schemas/defective-goods.schema'
 import { IDefectiveGoods } from '@/common/types/entities'
 import axiosInstance from '@/configs/axios.config'
 
 export class DefectiveGoodsService {
-	static async getDefectiveGoodsList(params: Pick<Pagination<IDefectiveGoods>, 'page' | 'limit'>) {
+	static async getDefectiveGoods(params: Pick<Pagination<IDefectiveGoods>, 'page' | 'limit'>) {
 		return await axiosInstance.get<void, ResponseBody<Pagination<IDefectiveGoods>>>('/defective-goods', {
 			params
 		})
 	}
+
+	static async createDefectiveGoods(payload: CreateDefectiveGoodsFormValues) {
+		return await axiosInstance.post<unknown, ResponseBody<unknown>, CreateDefectiveGoodsFormValues>(
+			'/defective-goods/create',
+			payload
+		)
+	}
+
+	static async updateDefectiveGoods(id: string, payload: UpdateDefectiveGoodsFormValues) {}
 }
