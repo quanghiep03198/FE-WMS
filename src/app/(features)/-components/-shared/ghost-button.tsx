768ce13faf6b03fc453e6507dabeb488541a0da2
quanghiep03198 <quanghiep03198@gmail.com>
2025-09-20 15:27:21 +0700

    feat: add GhostButton component with Tailwind styling

---
+++ src/app/(features)/-components/-shared/ghost-button.tsx
@@ -0,0 +1,3 @@
+import tw from 'tailwind-styled-components'
+
+export const GhostButton = tw.button`text-muted-foreground transition-colors duration-200 hover:text-foreground flex items-center relative justify-center`
