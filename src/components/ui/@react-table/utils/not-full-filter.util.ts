b7472d9f93f5761af7112d23681fd784c4f2324d
quanghiep03198 <quanghiep03198@gmail.com>
2025-09-26 01:05:47 +0700

    feat(filter): add notNullFilter utility function to exclude null or undefined values

---
+++ src/components/ui/@react-table/utils/not-full-filter.util.ts
@@ -0,0 +1,7 @@
+import { FilterFn } from '@tanstack/react-table'
+import { isNil } from 'lodash'
+
+export const notNullFilter: FilterFn<any> = (row, columnId) => {
+	const value = row.getValue(columnId)
+	return !isNil(value) // Include if not null or undefined
+}
