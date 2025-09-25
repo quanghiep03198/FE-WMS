ab403eff798b82c05e5fe1ada48b8cc55be5c8dc
quanghiep03198 <quanghiep03198@gmail.com>
2025-09-26 01:01:51 +0700

    fix(column-resizer): adjust styles for column resizer to improve layout and responsiveness

--- src/components/ui/@react-table/components/column-resizer.tsx
+++ src/components/ui/@react-table/components/column-resizer.tsx
@@ -14,7 +14,9 @@ export default function ColumnResizer<TData, TValue>({ header }: ColumnResizerPr
 			onTouchStart={header.getResizeHandler()}
 			onTouchMove={header.getResizeHandler()}
 			className={cn(
-				'absolute right-0 top-0 z-0 h-full w-1 !cursor-col-resize touch-none select-none bg-border opacity-0 transition-opacity duration-500 group-hover:opacity-100',
+				'absolute inset-y-0 right-0 z-0 w-1 !cursor-col-resize touch-none select-none bg-border opacity-0 transition-opacity duration-500 group-hover:opacity-100',
+				'group-data-[border=all]/data-grid-wrapper:h-full',
+				'group-data-[border=bottom-only]/data-grid-wrapper:top-1/2 group-data-[border=bottom-only]/data-grid-wrapper:h-2/3 group-data-[border=bottom-only]/data-grid-wrapper:-translate-y-1/2 group-data-[border=bottom-only]/data-grid-wrapper:rounded-md',
 				header.column.getCanResize() && 'hover:bg-primary',
 				header.column.getIsResizing() && 'bg-primary opacity-10'
 			)}
