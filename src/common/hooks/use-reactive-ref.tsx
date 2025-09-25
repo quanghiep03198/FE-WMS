f120b70cb0a87c8f822769f3d2237d2431894542
quanghiep03198 <quanghiep03198@gmail.com>
2025-09-26 01:05:56 +0700

    feat(hooks): add useReactiveRef hook for creating reactive references

---
+++ src/common/hooks/use-reactive-ref.tsx
@@ -0,0 +1,11 @@
+import { useReactive } from 'ahooks'
+import { RefObject } from 'react'
+
+/**
+ * @description Reactive Ref Hook. Creates a reactive reference to a value, allowing for automatic updates when the value changes.
+ * @param {T} value initial value
+ * @returns {RefObject<T>} A reactive reference object containing the current value.
+ */
+export function useReactiveRef<T>(value: T) {
+	return useReactive<RefObject<T>>({ current: value })
+}
