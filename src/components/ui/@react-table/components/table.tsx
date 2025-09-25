794dca0c5379b8fef4573a94f1e6646662cc56a5
quanghiep03198 <quanghiep03198@gmail.com>
2025-09-26 01:05:24 +0700

    fix(table): remove unnecessary type annotation from Wrapper component

--- src/components/ui/@react-table/components/table.tsx
+++ src/components/ui/@react-table/components/table.tsx
@@ -93,9 +93,7 @@ function DataTable<TData, TValue>(props: TableProps<TData, TValue>) {
 	)
 }
 
-const Wrapper = tw.div<
-	React.ComponentProps<'div'>
->`flex flex-col items-stretch border outline-none ring-0 ring-offset-0 ring-offset-transparent overflow-clip rounded-md`
+const Wrapper = tw.div`flex flex-col items-stretch border outline-none ring-0 ring-offset-0 ring-offset-transparent overflow-clip rounded-md`
 const ScrollArea = tw.div` relative flex flex-col items-stretch overflow-scroll max-w-full w-full scrollbar-track-scrollbar/20 outline-none border-none ring-0 ring-offset-0 ring-offset-transparent`
 
 DataTable.displayName = 'DataTable'
