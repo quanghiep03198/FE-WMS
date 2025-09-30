1ccf9a6d8411de203d59933ec0d6b61517502f6c
quanghiep03198 <quanghiep03198@gmail.com>
2025-10-01 00:08:19 +0700

    fix(navbar): simplify preference link rendering in Navbar component

--- src/app/(features)/preferences/-components/-partials/navbar.tsx
+++ src/app/(features)/preferences/-components/-partials/navbar.tsx
@@ -11,20 +11,18 @@ const Navbar: React.FC = () => {
 		<Div
 			as='nav'
 			className='z-20 flex gap-x-1 gap-y-2 overflow-auto bg-background/90 py-6 scrollbar-none xl:absolute xl:top-40 xl:w-80 xl:flex-col'>
-			{navigationConfig
-				.filter((item) => item.type === 'preference')
-				.map((item) => (
-					<Link
-						key={item.id}
-						to={item.path}
-						activeProps={{ className: 'text-primary hover:text-primary bg-primary/10' }}
-						className={cn(
-							'flex text-base font-normal xl:w-full',
-							buttonVariants({ variant: 'ghost', className: 'justify-start gap-x-3' })
-						)}>
-						<Typography>{t(item.title, { defaultValue: item.title })}</Typography>
-					</Link>
-				))}
+			{navigationConfig.preferences.map((item, index) => (
+				<Link
+					key={index.toString()}
+					to={item.url}
+					activeProps={{ className: 'text-primary hover:text-primary bg-primary/10' }}
+					className={cn(
+						'flex text-base font-normal xl:w-full',
+						buttonVariants({ variant: 'ghost', className: 'justify-start gap-x-3' })
+					)}>
+					<Typography>{t(item.title, { defaultValue: item.title })}</Typography>
+				</Link>
+			))}
 		</Div>
 	)
 }
