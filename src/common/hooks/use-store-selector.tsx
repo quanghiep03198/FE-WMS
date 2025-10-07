8f9805e571a4d77584abdbc05cd3f7439dd154e1
quanghiep03198 <quanghiep03198@gmail.com>
2025-10-07 11:29:51 +0700

    feat(use-store-selector): add custom hook for selecting state from Zustand store

---
+++ src/common/hooks/use-store-selector.tsx
@@ -0,0 +1,22 @@
+import { pick } from 'lodash'
+import { use } from 'react'
+import { StoreApi, useStore } from 'zustand'
+import { useShallow } from 'zustand/react/shallow'
+
+/**
+ * @description A custom hook to select state from a Zustand store within a React context.
+ * @author quanghiep03198
+ * @param {React.Context<StoreApi<T extends { [K in keyof T]: any }>>} context - The React context containing the Zustand store.
+ * @returns A function that takes selectors and returns the selected state.
+ */
+export const useStoreSelector = <T extends { [K in keyof T]: any }>(context: React.Context<StoreApi<T>>) => {
+	return <K extends keyof T>(...selectors: K[]) => {
+		const store = use(context)
+		if (!store) throw new Error('Missing store provider')
+		if (!selectors) return useStore(store)
+		return useStore(
+			store,
+			useShallow((state) => pick(state, selectors))
+		) as Pick<T, K>
+	}
+}
