ae08dabc05fc57e8defd3d2bf475f015a3352349
quanghiep03198 <quanghiep03198@gmail.com>
2025-10-19 13:05:32 +0700

    refactor: streamline HostCompatibleGuard logic and improve readability

--- src/app/-components/-guard/host-compatible-guard.tsx
+++ src/app/-components/-guard/host-compatible-guard.tsx
@@ -1,33 +1,35 @@
 import useAuth from '@/common/hooks/use-auth'
 import env from '@/common/utils/env'
+import { isIPv4 } from '@/common/utils/ip'
 import { Div, Typography } from '@/components/ui'
 import { __hostRegistry } from '@/configs/host-registry.config'
+import { HttpStatusCode } from 'axios'
+
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import tw from 'tailwind-styled-components'
-import { z } from 'zod'
-
-const isIPv4 = (ip: string): boolean => {
-	return z.ipv4().safeParse(ip).success
-}
 
 const HostCompatibleGuard: React.FC<React.PropsWithChildren> = ({ children }) => {
 	const { t } = useTranslation()
 	const { user } = useAuth()
 	const currentHostRegistry = __hostRegistry.get(user.company_code)
-	const movedPermanentlyURL = `${window.location.protocol}//${currentHostRegistry}/${window.location.pathname}`
 
-	const shouldValidate = env('VITE_NODE_ENV') === 'production'
+	const shouldCheck = env('VITE_NODE_ENV') === 'production'
+
 	const isNotCompatible =
+		shouldCheck &&
 		(isIPv4(window.location.hostname)
 			? window.location.hostname !== currentHostRegistry.ip
-			: window.location.hostname !== currentHostRegistry.domain) && shouldValidate
+			: window.location.hostname !== currentHostRegistry.domain)
+
+	const movedPermanentlyURL = isIPv4(window.location.hostname)
+		? `${window.location.protocol}//${currentHostRegistry.ip}:${window.location.port}/${window.location.pathname}`
+		: `${window.location.protocol}//${currentHostRegistry.domain}/${window.location.pathname}`
 
 	if (isNotCompatible)
 		return (
 			<Div className='flex min-h-[var(--outlet-wrapper-height)] w-full flex-1 flex-col items-center justify-center gap-y-3'>
 				<Typography variant='code' color='destructive' className='font-semibold'>
-					303
+					{HttpStatusCode.SeeOther}
 				</Typography>
 				<Typography variant='h1'>{t('ns_common:errors.303')}</Typography>
 				<Typography
@@ -35,19 +37,16 @@ const HostCompatibleGuard: React.FC<React.PropsWithChildren> = ({ children }) =>
 					color='muted'
 					dangerouslySetInnerHTML={{
 						__html: t('ns_common:errors.303_message', {
-							url: /* html */ `<a href='${movedPermanentlyURL}' style='font-weight: 600; color:hsl(var(--active));'>URL</a>`,
+							url: /* html */ `<a href='${movedPermanentlyURL}' style='font-weight: 600; color: hsl(var(--active));'>URL</a>`,
 							factoryCode: t(`ns_common:factory.${user.company_code}`, { defaultValue: user.company_code }),
 							defaultValue: null
 						})
 					}}
 				/>
-				<Image src='/onboarding.svg' />
 			</Div>
 		)
 
 	return children
 }
-
-const Image = tw.img`max-w-md object-center object-contain w-full select-none mt-6`
 
 export default HostCompatibleGuard
