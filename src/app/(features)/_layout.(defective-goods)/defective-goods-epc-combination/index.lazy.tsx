3e2c04184c826272c1ce261bc4e3e45dfb4fe3c8
quanghiep03198 <quanghiep03198@gmail.com>
2025-10-19 14:23:20 +0700

    feat: add IpPolicyGuard to enhance security in defective goods routes

--- src/app/(features)/_layout.(defective-goods)/defective-goods-epc-combination/index.lazy.tsx
+++ src/app/(features)/_layout.(defective-goods)/defective-goods-epc-combination/index.lazy.tsx
@@ -1,4 +1,5 @@
 import HostCompatibleGuard from '@/app/-components/-guard/host-compatible-guard'
+import IpPolicyGuard from '@/app/-components/-guard/ip-policy-guard'
 import { cn } from '@/common/utils/cn'
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui'
 import { createLazyFileRoute } from '@tanstack/react-router'
@@ -38,56 +39,58 @@ function RouteComponent() {
 		<Fragment>
 			<title>{t('ns_common:navigation.defective_goods_epc_combination')}</title>
 			<meta name='description' content='Matching EPCs for defective goods' />
-			<HostCompatibleGuard>
-				<Container>
-					<PageContextProvider>
-						<ResizablePanelGroup
-							direction='horizontal'
-							className='rounded-md border'
-							style={
-								{
-									'--bar-height': '52px'
-								} as React.CSSProperties
-							}>
-							<ResizablePanel
-								minSize={listPanelOpen ? 30 : 0}
-								maxSize={listPanelOpen ? 40 : 0}
-								defaultSize={listPanelOpen ? 30 : 0}
-								className={cn(
-									'transtion-max-width linear hidden h-full duration-150 will-change-transform @7xl:block',
-									'group-has-[div[data-resize-handle-state=drag]]/container:transition-none',
-									listPanelOpen && 'border-0'
-								)}>
-								<DatalistPanel />
-							</ResizablePanel>
-							{listPanelOpen && (
-								<ResizableHandle
-									withHandle
-									className='hidden w-[0.5px] border-0 shadow-none ring-transparent @7xl:flex'
-								/>
-							)}
-							<ResizablePanel defaultSize={50} minSize={40} className='h-full'>
-								<DefectiveGoodsForm />
-							</ResizablePanel>
-							{isUsingUHFReader && <ResizableHandle disabled />}
-							<ResizablePanel
-								minSize={isUsingUHFReader ? 25 : 0}
-								maxSize={isUsingUHFReader ? 25 : 0}
-								defaultSize={isUsingUHFReader ? 25 : 0}
-								className={cn(
-									'transtion-max-width linear h-full duration-150 will-change-transform',
-									'group-has-[div[data-resize-handle-state=drag]]/container:transition-none',
-									!isUsingUHFReader && 'border-0'
-								)}>
-								<ReaderPlaygroundProvider>
-									{isUsingUHFReader && <RFIDReaderPlayground />}
-								</ReaderPlaygroundProvider>
-							</ResizablePanel>
-						</ResizablePanelGroup>
-						<DetailDialog />
-					</PageContextProvider>
-				</Container>
-			</HostCompatibleGuard>
+			<IpPolicyGuard>
+				<HostCompatibleGuard>
+					<Container>
+						<PageContextProvider>
+							<ResizablePanelGroup
+								direction='horizontal'
+								className='rounded-md border'
+								style={
+									{
+										'--bar-height': '52px'
+									} as React.CSSProperties
+								}>
+								<ResizablePanel
+									minSize={listPanelOpen ? 30 : 0}
+									maxSize={listPanelOpen ? 40 : 0}
+									defaultSize={listPanelOpen ? 30 : 0}
+									className={cn(
+										'transtion-max-width linear hidden h-full duration-150 will-change-transform @7xl:block',
+										'group-has-[div[data-resize-handle-state=drag]]/container:transition-none',
+										listPanelOpen && 'border-0'
+									)}>
+									<DatalistPanel />
+								</ResizablePanel>
+								{listPanelOpen && (
+									<ResizableHandle
+										withHandle
+										className='hidden w-[0.5px] border-0 shadow-none ring-transparent @7xl:flex'
+									/>
+								)}
+								<ResizablePanel defaultSize={50} minSize={40} className='h-full'>
+									<DefectiveGoodsForm />
+								</ResizablePanel>
+								{isUsingUHFReader && <ResizableHandle disabled />}
+								<ResizablePanel
+									minSize={isUsingUHFReader ? 25 : 0}
+									maxSize={isUsingUHFReader ? 25 : 0}
+									defaultSize={isUsingUHFReader ? 25 : 0}
+									className={cn(
+										'transtion-max-width linear h-full duration-150 will-change-transform',
+										'group-has-[div[data-resize-handle-state=drag]]/container:transition-none',
+										!isUsingUHFReader && 'border-0'
+									)}>
+									<ReaderPlaygroundProvider>
+										{isUsingUHFReader && <RFIDReaderPlayground />}
+									</ReaderPlaygroundProvider>
+								</ResizablePanel>
+							</ResizablePanelGroup>
+							<DetailDialog />
+						</PageContextProvider>
+					</Container>
+				</HostCompatibleGuard>
+			</IpPolicyGuard>
 		</Fragment>
 	)
 }
--- src/app/(features)/_layout.(defective-goods)/defective-goods-inoutbound/index.lazy.tsx
+++ src/app/(features)/_layout.(defective-goods)/defective-goods-inoutbound/index.lazy.tsx
@@ -1,4 +1,5 @@
 import HostCompatibleGuard from '@/app/-components/-guard/host-compatible-guard'
+import IpPolicyGuard from '@/app/-components/-guard/ip-policy-guard'
 import useMediaQuery from '@/common/hooks/use-media-query'
 import { cn } from '@/common/utils/cn'
 import { Div, ResizableHandle, ResizablePanel, ResizablePanelGroup, Typography } from '@/components/ui'
@@ -39,72 +40,74 @@ function RouteComponent() {
 			<title>{t('ns_common:navigation.defective_goods_inoutbound')}</title>
 			<meta name='description' content='Defective goods inoutbound' />
 
-			<HostCompatibleGuard>
-				<PageContextProvider>
-					<Div
-						style={
-							{
-								'--header-height': '64px',
-								'--bar-height': '48px'
-							} as React.CSSProperties
-						}
-						className={cn(
-							'flex h-[var(--outlet-wrapper-height)] border-collapse flex-col divide-y divide-border rounded-md border @container',
-							!isSmallScreen && 'overflow-hidden'
-						)}>
-						<ReaderPlaygroundProvider>
-							<Div
-								className={cn(
-									'col-span-full flex h-[var(--header-height)] items-center bg-background px-4 py-2',
-									isSmallScreen ? 'justify-end' : 'justify-between'
-								)}>
-								<Typography variant='h4' className='hidden @5xl:block'>
-									{t('ns_common:navigation.defective_goods_inoutbound')}
-								</Typography>
-								<InoutboundForm />
-							</Div>
-							<ResizablePanelGroup
-								direction={isSmallScreen ? 'vertical' : 'horizontal'}
-								className={cn('h-[calc(var(--outlet-wrapper-height)-var(--header-height))]')}
-								style={
-									{
-										'--rfid-playground-panel-height': rfidPlaygroundPanelSize
-											? rfidPlaygroundPanelSize.height + 'px'
-											: '100%',
-										'--detail-table-panel-height': detailTablePanelSize
-											? detailTablePanelSize.height + 'px'
-											: '100%'
-									} as React.CSSProperties
-								}>
-								<ResizablePanel
-									minSize={isSmallScreen ? 35 : 65}
-									maxSize={isSmallScreen ? 50 : 75}
-									defaultSize={isSmallScreen ? 30 : 65}>
-									<PanelContent ref={detailTablePanelRef}>
-										<DetailTable />
-									</PanelContent>
-								</ResizablePanel>
-								<ResizableHandle withHandle={true} className='z-30' />
-								<ResizablePanel
-									maxSize={isSmallScreen ? 65 : 35}
-									minSize={isSmallScreen ? 50 : 25}
-									defaultSize={isSmallScreen ? 65 : 30}>
-									<PanelContent ref={rfidPlaygroundPanelRef}>
-										<RfidReaderPlayground
-											style={
-												{
-													'--playground-header-height': 'var(--bar-height)',
-													'--playground-actions-height': 'var(--bar-height)'
-												} as React.CSSProperties
-											}
-										/>
-									</PanelContent>
-								</ResizablePanel>
-							</ResizablePanelGroup>
-						</ReaderPlaygroundProvider>
-					</Div>
-				</PageContextProvider>
-			</HostCompatibleGuard>
+			<IpPolicyGuard>
+				<HostCompatibleGuard>
+					<PageContextProvider>
+						<Div
+							style={
+								{
+									'--header-height': '64px',
+									'--bar-height': '48px'
+								} as React.CSSProperties
+							}
+							className={cn(
+								'flex h-[var(--outlet-wrapper-height)] border-collapse flex-col divide-y divide-border rounded-md border @container',
+								!isSmallScreen && 'overflow-hidden'
+							)}>
+							<ReaderPlaygroundProvider>
+								<Div
+									className={cn(
+										'col-span-full flex h-[var(--header-height)] items-center bg-background px-4 py-2',
+										isSmallScreen ? 'justify-end' : 'justify-between'
+									)}>
+									<Typography variant='h4' className='hidden @5xl:block'>
+										{t('ns_common:navigation.defective_goods_inoutbound')}
+									</Typography>
+									<InoutboundForm />
+								</Div>
+								<ResizablePanelGroup
+									direction={isSmallScreen ? 'vertical' : 'horizontal'}
+									className={cn('h-[calc(var(--outlet-wrapper-height)-var(--header-height))]')}
+									style={
+										{
+											'--rfid-playground-panel-height': rfidPlaygroundPanelSize
+												? rfidPlaygroundPanelSize.height + 'px'
+												: '100%',
+											'--detail-table-panel-height': detailTablePanelSize
+												? detailTablePanelSize.height + 'px'
+												: '100%'
+										} as React.CSSProperties
+									}>
+									<ResizablePanel
+										minSize={isSmallScreen ? 35 : 65}
+										maxSize={isSmallScreen ? 50 : 75}
+										defaultSize={isSmallScreen ? 30 : 65}>
+										<PanelContent ref={detailTablePanelRef}>
+											<DetailTable />
+										</PanelContent>
+									</ResizablePanel>
+									<ResizableHandle withHandle={true} className='z-30' />
+									<ResizablePanel
+										maxSize={isSmallScreen ? 65 : 35}
+										minSize={isSmallScreen ? 50 : 25}
+										defaultSize={isSmallScreen ? 65 : 30}>
+										<PanelContent ref={rfidPlaygroundPanelRef}>
+											<RfidReaderPlayground
+												style={
+													{
+														'--playground-header-height': 'var(--bar-height)',
+														'--playground-actions-height': 'var(--bar-height)'
+													} as React.CSSProperties
+												}
+											/>
+										</PanelContent>
+									</ResizablePanel>
+								</ResizablePanelGroup>
+							</ReaderPlaygroundProvider>
+						</Div>
+					</PageContextProvider>
+				</HostCompatibleGuard>
+			</IpPolicyGuard>
 		</Fragment>
 	)
 }
