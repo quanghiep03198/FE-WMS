70b3b187904610c87861ad70f2ad5fbe27815d16
quanghiep03198 <quanghiep03198@gmail.com>
2025-10-21 23:45:18 +0700

    perf: migrate to vite rolldown to boost up build time

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import { defineConfig, loadEnv, normalizePath } from 'vite'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import { VitePWA as pwa, type VitePWAOptions } from 'vite-plugin-pwa'
 import { viteStaticCopy as staticCopy } from 'vite-plugin-static-copy'
+
 /**
  * @see https://vitejs.dev/config/
  */
@@ -25,55 +26,15 @@ export default defineConfig(({ mode }) => {
 			}),
 			reactRouter(),
 			staticCopy({
-				targets: [{ src: './infrastructure/web.config', dest: '' }]
+				targets: [{ src: './infrastructure/*', dest: '' }]
 			}),
 			nodePolyfills(),
 			pwa({
 				registerType: 'autoUpdate',
-				manifestFilename: 'site.webmanifest',
 				disable: mode === 'development',
 				mode: mode as VitePWAOptions['mode'],
 				includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
-				manifest: {
-					name: 'Warehouse Management System',
-					short_name: 'WMS',
-					description: 'Production warehouse management system',
-					start_url: '/login',
-					display: 'fullscreen',
-					theme_color: '#262626',
-					orientation: 'landscape-primary',
-					screenshots: [
-						{
-							src: 'logo.svg',
-							sizes: '64x64',
-							type: 'image/svg+xml'
-						}
-					],
-					icons: [
-						{
-							src: 'pwa-64x64.png',
-							sizes: '64x64',
-							type: 'image/png'
-						},
-						{
-							src: 'pwa-192x192.png',
-							sizes: '192x192',
-							type: 'image/png'
-						},
-						{
-							src: 'pwa-512x512.png',
-							sizes: '512x512',
-							type: 'image/png',
-							purpose: 'any'
-						},
-						{
-							src: 'maskable-icon-512x512.png',
-							sizes: '512x512',
-							type: 'image/png',
-							purpose: 'maskable'
-						}
-					]
-				},
+				manifest: false,
 				workbox: {
 					navigateFallback: '/index.html',
 					navigateFallbackDenylist: [/^\/api\//, /^\/sw\.js$/, /^\/workbox-.*\.js$/, /\.(wasm|map)$/],
@@ -165,17 +126,6 @@ export default defineConfig(({ mode }) => {
 				ignoreEmptyLines: true
 			}
 		},
-		optimizeDeps: {
-			esbuildOptions: {
-				target: 'esnext'
-			}
-		},
-		esbuild: {
-			drop: mode === 'production' ? ['console', 'debugger'] : undefined,
-			logOverride: {
-				'this-is-undefined-in-esm': 'silent'
-			}
-		},
 		server: {
 			port: 3000,
 			host: true,
@@ -203,78 +153,70 @@ export default defineConfig(({ mode }) => {
 			cssCodeSplit: true,
 			reportCompressedSize: true,
 			chunkSizeWarningLimit: 1024,
-			rollupOptions: {
-				onwarn(warning, warn) {
-					if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
-						return
-					}
-					warn(warning)
-				},
+			assetsInlineLimit: 0,
+			rolldownOptions: {
+				dropLabels: mode === 'production' ? ['console', 'debugger'] : undefined,
+				logLevel: mode === 'production' ? 'silent' : 'debug',
 				output: {
-					manualChunks(id: string) {
-						const modules = new Array<[string, RegExp]>(
-							['@dnd-kit', /@dnd-kit\/*/],
-							['@radix-ui', /@radix-ui\/*/],
-							['@tanstack/react-query', /@tanstack\/react-query/],
-							['@tanstack/react-router', /@tanstack\/react-router/],
-							['@tanstack/react-table', /@tanstack\/react-table/],
-							['@tanstack/react-virtual', /react-virtual/],
-							['@tiptap/core', /@tiptap\/core/],
-							['@tiptap/extension-color', /@tiptap\/extension-color/],
-							['@tiptap/extension-file-handler', /@tiptap\/extension-file-handler/],
-							['@tiptap/extension-gapcursor', /@tiptap\/extension-gapcursor/],
-							['@tiptap/extension-heading', /@tiptap\/extension-heading/],
-							['@tiptap/extension-highlight', /@tiptap\/extension-highlight/],
-							['@tiptap/extension-image', /@tiptap\/extension-image/],
-							['@tiptap/extension-link', /@tiptap\/extension-link/],
-							['@tiptap/extension-placeholder', /@tiptap\/extension-placeholder/],
-							['@tiptap/extension-table', /@tiptap\/extension-table/],
-							['@tiptap/extension-table-cell', /@tiptap\/extension-table-cell/],
-							['@tiptap/extension-table-header', /@tiptap\/extension-table-header/],
-							['@tiptap/extension-table-row', /@tiptap\/extension-table-row/],
-							['@tiptap/extension-text-align', /@tiptap\/extension-text-align/],
-							['@tiptap/extension-text-style', /@tiptap\/extension-text-style/],
-							['@tiptap/extension-underline', /@tiptap\/extension-underline/],
-							['@tiptap/pm', /@tiptap\/pm/],
-							['@tiptap/react', /@tiptap\/react/],
-							['@tiptap/starter-kit', /@tiptap\/starter-kit/],
-							['ahooks', /ahooks/],
-							['axios', /axios/],
-							['bcryptjs-react', /bcryptjs-react/],
-							['clsx', /clsx/],
-							['cmdk', /cmdk/],
-							['copy-to-clipboard', /copy-to-clipboard/],
-							['d3-shape', /d3-shape/],
-							['date-fns', /date-fns/],
-							['file-saver', /file-saver/],
-							['filesize', /filesize/],
-							['flat', /flat/],
-							['i18next', /i18next/],
-							['i18next-browser-languagedetector', /i18next-browser-languagedetector/],
-							['immer', /immer/],
-							['lodash', /lodash/],
-							['lucide', /lucide-react/],
-							['lz-string', /lz-string/],
-							['nprogress', /nprogress/],
-							['qs', /qs/],
-							['react-day-picker', /react-day-picker/],
-							['react-hook-form', /react-hook-form/],
-							['react-resizable-panels', /react-resizable-panels/],
-							['recharts', /recharts/],
-							['@sentry/react', /@sentry\/react/],
-							['sonner', /sonner/],
-							['socket.io-client', /socket.io-client/],
-							['tailwind-merge', /tailwind-merge/],
-							['tailwind-styled-components', /tailwind-styled-components/],
-							['uuid', /uuid/],
-							['zod', /zod/],
-							['zustand', /zustand/]
-						)
-
-						for (const [key, regex] of modules) {
-							if (id.match(regex)) return key
-						}
-						return null
+					advancedChunks: {
+						groups: [
+							{ name: '@dnd-kit', test: /@dnd-kit/ },
+							{ name: '@radix-ui', test: /@radix-ui/ },
+							{ name: '@tanstack/react-query', test: /@tanstack\/react-query/ },
+							{ name: '@tanstack/react-router', test: /@tanstack\/react-router/ },
+							{ name: '@tanstack/react-table', test: /@tanstack\/react-table/ },
+							{ name: '@tanstack/react-virtual', test: /@tanstack\/react-virtual/ },
+							{ name: '@tiptap/core', test: /@tiptap\/core/ },
+							{ name: '@tiptap/extension-color', test: /@tiptap\/extension-color/ },
+							{ name: '@tiptap/extension-file-handler', test: /@tiptap\/extension-file-handler/ },
+							{ name: '@tiptap/extension-gapcursor', test: /@tiptap\/extension-gapcursor/ },
+							{ name: '@tiptap/extension-heading', test: /@tiptap\/extension-heading/ },
+							{ name: '@tiptap/extension-highlight', test: /@tiptap\/extension-highlight/ },
+							{ name: '@tiptap/extension-image', test: /@tiptap\/extension-image/ },
+							{ name: '@tiptap/extension-link', test: /@tiptap\/extension-link/ },
+							{ name: '@tiptap/extension-placeholder', test: /@tiptap\/extension-placeholder/ },
+							{ name: '@tiptap/extension-table', test: /@tiptap\/extension-table/ },
+							{ name: '@tiptap/extension-table-cell', test: /@tiptap\/extension-table-cell/ },
+							{ name: '@tiptap/extension-table-header', test: /@tiptap\/extension-table-header/ },
+							{ name: '@tiptap/extension-table-row', test: /@tiptap\/extension-table-row/ },
+							{ name: '@tiptap/extension-text-align', test: /@tiptap\/extension-text-align/ },
+							{ name: '@tiptap/extension-text-style', test: /@tiptap\/extension-text-style/ },
+							{ name: '@tiptap/extension-underline', test: /@tiptap\/extension-underline/ },
+							{ name: '@tiptap/pm', test: /@tiptap\/pm/ },
+							{ name: '@tiptap/react', test: /@tiptap\/react/ },
+							{ name: '@tiptap/starter-kit', test: /@tiptap\/starter-kit/ },
+							{ name: 'ahooks', test: /ahooks/ },
+							{ name: 'axios', test: /axios/ },
+							{ name: 'bcryptjs-react', test: /bcryptjs-react/ },
+							{ name: 'clsx', test: /clsx/ },
+							{ name: 'cmdk', test: /cmdk/ },
+							{ name: 'copy-to-clipboard', test: /copy-to-clipboard/ },
+							{ name: 'd3-shape', test: /d3-shape/ },
+							{ name: 'date-fns', test: /date-fns/ },
+							{ name: 'file-saver', test: /file-saver/ },
+							{ name: 'filesize', test: /filesize/ },
+							{ name: 'flat', test: /flat/ },
+							{ name: 'i18next', test: /i18next/ },
+							{ name: 'i18next-browser-languagedetector', test: /i18next-browser-languagedetector/ },
+							{ name: 'immer', test: /immer/ },
+							{ name: 'lodash', test: /lodash/ },
+							{ name: 'lucide', test: /lucide-react/ },
+							{ name: 'lz-string', test: /lz-string/ },
+							{ name: 'nprogress', test: /nprogress/ },
+							{ name: 'qs', test: /qs/ },
+							{ name: 'react-day-picker', test: /react-day-picker/ },
+							{ name: 'react-hook-form', test: /react-hook-form/ },
+							{ name: 'react-resizable-panels', test: /react-resizable-panels/ },
+							{ name: 'recharts', test: /recharts/ },
+							{ name: '@sentry/react', test: /@sentry\/react/ },
+							{ name: 'sonner', test: /sonner/ },
+							{ name: 'socket.io-client', test: /socket.io-client/ },
+							{ name: 'tailwind-merge', test: /tailwind-merge/ },
+							{ name: 'tailwind-styled-components', test: /tailwind-styled-components/ },
+							{ name: 'uuid', test: /uuid/ },
+							{ name: 'zod', test: /zod/ },
+							{ name: 'zustand', test: /zustand/ }
+						]
 					}
 				}
 			}
