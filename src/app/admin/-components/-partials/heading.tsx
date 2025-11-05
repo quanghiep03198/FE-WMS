710a4a2a1ed5c02dae9c67b7033d0bf13b8bbede
hieu92264 <nguyenthehieu20182021@gmail.com>
2025-11-05 10:02:36 +0700

    feat: update heading component to use translation for titles and descriptions

--- src/app/admin/-components/-partials/heading.tsx
+++ src/app/admin/-components/-partials/heading.tsx
@@ -5,16 +5,16 @@ import React from 'react'
 import { useTranslation } from 'react-i18next'
 
 const Heading: React.FC = () => {
-	const { t } = useTranslation('ns_common')
+	const { t } = useTranslation()
 	const router = useRouter()
 
 	return (
 		<Div className='sticky top-0 z-20 mx-auto flex w-full max-w-7xl basis-40 items-start justify-between border-b border-border bg-background/85 py-10 backdrop-blur xxl:max-w-8xl'>
 			<Div className='space-y-2'>
 				<Typography variant='h1' className='font-bold tracking-tight'>
-					Admin Dashboard
+					{t('ns_admin:title')}
 				</Typography>
-				<Typography color='muted'>Manage permission of user account</Typography>
+				<Typography color='muted'>{t('ns_admin:description')}</Typography>
 			</Div>
 			<Div className='inline-flex items-center'>
 				<Tooltip message={t('ns_common:actions.back')} triggerProps={{ asChild: true }}>
