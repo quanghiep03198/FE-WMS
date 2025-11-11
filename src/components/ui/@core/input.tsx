97e9218b8bc8aa45b77e1fa50c485b9323338e77
quanghiep03198 <quanghiep03198@gmail.com>
2025-11-11 16:36:47 +0700

    feat: add styling for time input type to hide calendar picker indicator

--- src/components/ui/@core/input.tsx
+++ src/components/ui/@core/input.tsx
@@ -9,6 +9,8 @@ const Input: React.FC<InputProps> = ({ className, type, ref, ...props }) => {
 			type={type}
 			className={cn(
 				'flex h-9 w-full rounded-md border border-input bg-background px-3 py-1 text-sm shadow-sm transition-colors placeholder:text-muted-foreground focus-within:border-primary focus:outline-none disabled:text-muted-foreground',
+				type === 'time' &&
+					'appearance-none [&::-webkit-calendar-picker-indicator]:hidden [&::-webkit-calendar-picker-indicator]:appearance-none',
 				className
 			)}
 			ref={ref}
