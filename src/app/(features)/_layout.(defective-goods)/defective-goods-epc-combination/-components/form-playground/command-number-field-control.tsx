4d3751cafeeb00939561831e923ea741089bebdf
quanghiep03198 <quanghiep03198@gmail.com>
2025-11-25 16:45:21 +0700

    fix: autocomplete bugs

--- src/app/(features)/_layout.(defective-goods)/defective-goods-epc-combination/-components/form-playground/command-number-field-control.tsx
+++ src/app/(features)/_layout.(defective-goods)/defective-goods-epc-combination/-components/form-playground/command-number-field-control.tsx
@@ -1,20 +1,21 @@
-import { ComboboxFieldControl } from '@/components/ui'
+import { AutoCompleteFieldControl } from '@/components/ui'
 import { ComboboxFieldControlProps } from '@/components/ui/@field-control/combobox'
 import { debounce } from 'lodash'
 import React, { useState } from 'react'
-import { FieldValues } from 'react-hook-form'
+import { FieldValues, useFormContext } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
 import { useSearchCommandNumberQuery } from '../../../../-hooks/use-order-asm'
 
 type CommandNumberFieldControlProps = Partial<ComboboxFieldControlProps<FieldValues, Record<'mo_no', string>>>
 
 const CommandNumberFieldControl: React.FC<CommandNumberFieldControlProps> = (props) => {
-	const [searchTerm, setSearchTerm] = useState<string>('')
+	const { getValues } = useFormContext()
+	const [searchTerm, setSearchTerm] = useState<string>(getValues('mo_no') || '')
 	const { t } = useTranslation()
 	const { data } = useSearchCommandNumberQuery(searchTerm)
 
 	return (
-		<ComboboxFieldControl
+		<AutoCompleteFieldControl
 			name='mo_no'
 			label={t('ns_erp:fields.mo_no')}
 			datalist={data}
