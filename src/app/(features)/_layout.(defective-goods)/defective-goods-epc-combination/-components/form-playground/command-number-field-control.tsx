9fc2d8024b71b5139822895011e37aba5b05a2f3
quanghiep03198 <quanghiep03198@gmail.com>
2025-11-25 22:39:35 +0700

    fix: refactor purchase order field control and update related components

--- src/app/(features)/_layout.(defective-goods)/defective-goods-epc-combination/-components/form-playground/command-number-field-control.tsx
+++ src/app/(features)/_layout.(defective-goods)/defective-goods-epc-combination/-components/form-playground/command-number-field-control.tsx
@@ -1,28 +1,35 @@
-import { AutoCompleteFieldControl } from '@/components/ui'
-import { ComboboxFieldControlProps } from '@/components/ui/@field-control/combobox'
-import { debounce } from 'lodash'
-import React, { useState } from 'react'
-import { FieldValues, useFormContext } from 'react-hook-form'
+import { useSearchCommandNumberQuery } from '@/app/(features)/-hooks/use-order-asm'
+import { AutoCompleteFieldControl, type AutoCompleteFieldControlProps } from '@/components/ui'
+import { useDebounce } from 'ahooks'
+import { capitalize } from 'lodash'
+import React from 'react'
+import { FieldValues, useFormContext, useWatch } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
-import { useSearchCommandNumberQuery } from '../../../../-hooks/use-order-asm'
 
-type CommandNumberFieldControlProps = Partial<ComboboxFieldControlProps<FieldValues, Record<'mo_no', string>>>
+type CommandNumberFieldControlProps = Partial<AutoCompleteFieldControlProps<FieldValues, Record<'mo_no', string>>>
 
 const CommandNumberFieldControl: React.FC<CommandNumberFieldControlProps> = (props) => {
-	const { getValues } = useFormContext()
-	const [searchTerm, setSearchTerm] = useState<string>(getValues('mo_no') || '')
+	const { control } = useFormContext()
 	const { t } = useTranslation()
-	const { data } = useSearchCommandNumberQuery(searchTerm)
+	const value = useWatch({ control, name: 'mo_no' })
+	const debouncedSearchTerm = useDebounce(value, { wait: 200 })
+	const { data, isLoading } = useSearchCommandNumberQuery(debouncedSearchTerm)
 
 	return (
 		<AutoCompleteFieldControl
 			name='mo_no'
 			label={t('ns_erp:fields.mo_no')}
+			placeholder={capitalize(
+				t('ns_common:form_placeholder.fill', {
+					object: t('ns_erp:fields.mo_no'),
+					defaultValue: 'Search purchase command number ...'
+				})
+			)}
 			datalist={data}
 			labelField='mo_no'
 			valueField='mo_no'
 			shouldFilter={false}
-			onInput={debounce((value) => setSearchTerm(value), 200)}
+			loading={isLoading}
 			{...props}
 		/>
 	)
--- src/app/(features)/_layout.(defective-goods)/defective-goods-epc-combination/-components/form-playground/device-radio-group.tsx
+++ src/app/(features)/_layout.(defective-goods)/defective-goods-epc-combination/-components/form-playground/device-radio-group.tsx
@@ -1,8 +1,9 @@
+import { cn } from '@/common/utils/cn'
 import { Div, Icon, Label, RadioGroup, RadioGroupItem } from '@/components/ui'
 import { memo } from 'react'
 import { useSwitchRFIDDevice } from '../../../-hooks/use-switch-rfid-device'
 
-const DeviceRadioGroup: React.FC = () => {
+const DeviceRadioGroup: React.FC<{ shouldNotAllowUhf: boolean }> = ({ shouldNotAllowUhf }) => {
 	const { currentDevice, setCurrentDevice } = useSwitchRFIDDevice()
 
 	return (
@@ -11,8 +12,10 @@ const DeviceRadioGroup: React.FC = () => {
 			onValueChange={(value: 'usb' | 'uhf') => setCurrentDevice(value)}
 			className='flex items-center gap-x-6'>
 			<Div className='flex items-center gap-3'>
-				<RadioGroupItem value='uhf' id='android-device-option' />
-				<Label htmlFor='android-device-option' className='inline-flex items-center gap-x-2'>
+				<RadioGroupItem value='uhf' id='android-device-option' disabled={shouldNotAllowUhf} />
+				<Label
+					htmlFor='android-device-option'
+					className={cn('inline-flex items-center gap-x-2', shouldNotAllowUhf && 'text-muted-foreground')}>
 					UHF <Icon name='Router' size={18} />
 				</Label>
 			</Div>
