c8c3671cdb567123aff3ebf1344751eb3ef89254
quanghiep03198 <quanghiep03198@gmail.com>
2025-11-27 00:20:26 +0700

    fix(qr-scanner): update stroke and fill styles to use hex color codes

--- src/app/(features)/-components/-shared/qr-scanner.tsx
+++ src/app/(features)/-components/-shared/qr-scanner.tsx
@@ -6,12 +6,12 @@ export const QRScanner: React.FC<IScannerProps> = (props) => {
 			const { boundingBox, cornerPoints } = detectedCode
 
 			// Draw bounding box
-			ctx.strokeStyle = 'hsl(var(--success))'
+			ctx.strokeStyle = '#22c55e'
 			ctx.lineWidth = 4
 			ctx.strokeRect(boundingBox.x, boundingBox.y, boundingBox.width, boundingBox.height)
 
 			// Draw corner points
-			ctx.fillStyle = 'hsl(var(--success))'
+			ctx.fillStyle = '#22c55e'
 			cornerPoints.forEach((point) => {
 				ctx.beginPath()
 				ctx.arc(point.x, point.y, 5, 0, 2 * Math.PI)
