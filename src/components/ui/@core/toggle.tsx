162238625f148fbea0dc0a2dacc5947097e19f0d
quanghiep03198 <quanghiep03198@gmail.com>
2025-12-05 23:17:02 +0700

    refactor(toggle): change Toggle component to use React.FC for improved type safety

--- src/components/ui/@core/toggle.tsx
+++ src/components/ui/@core/toggle.tsx
@@ -25,12 +25,13 @@ const toggleVariants = cva(
 	}
 )
 
-const Toggle = React.forwardRef<
-	React.ElementRef<typeof TogglePrimitive.Root>,
-	React.ComponentPropsWithoutRef<typeof TogglePrimitive.Root> & VariantProps<typeof toggleVariants>
->(({ className, variant, size, ...props }, ref) => (
-	<TogglePrimitive.Root ref={ref} className={cn(toggleVariants({ variant, size, className }))} {...props} />
-))
+const Toggle: React.FC<React.ComponentProps<typeof TogglePrimitive.Root> & VariantProps<typeof toggleVariants>> = ({
+	className,
+	variant,
+	size,
+	ref,
+	...props
+}) => <TogglePrimitive.Root ref={ref} className={cn(toggleVariants({ variant, size, className }))} {...props} />
 
 Toggle.displayName = TogglePrimitive.Root.displayName
 
