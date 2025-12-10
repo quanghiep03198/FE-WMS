f61295a46fabfc90c12f0d1290d557fb8941115e
quanghiep03198 <quanghiep03198@gmail.com>
2025-12-10 10:20:13 +0700

    fix: make 'auto-refresh' optional in PageQueryParams and initialize date parameter

--- src/app/(features)/-hooks/use-report-page-query-params.tsx
+++ src/app/(features)/-hooks/use-report-page-query-params.tsx
@@ -1,10 +1,13 @@
 import useQueryParams from '@/common/hooks/use-query-params'
+import { format } from 'date-fns'
 
 export type PageQueryParams = {
 	'date.eq': string
-	'auto-refresh': number | false
+	'auto-refresh'?: number | false
 }
 
 export const useReportPageQueryParams = () => {
-	return useQueryParams<PageQueryParams>()
+	return useQueryParams<PageQueryParams>({
+		'date.eq': format(new Date(), 'yyyy-MM-dd')
+	})
 }
