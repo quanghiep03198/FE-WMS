7ac1d81eb07ce1aebcf471b174cd49cca49aa87a
quanghiep03198 <quanghiep03198@gmail.com>
2025-12-24 06:40:39 +0700

    refactor: update useMeasureElement to include estimateSize and improve default parameter handling

--- src/common/hooks/use-measure-element.tsx
+++ src/common/hooks/use-measure-element.tsx
@@ -1,14 +1,17 @@
 import { DependencyList, useMemo } from 'react'
 
-export default function useMeasureElement<T extends HTMLElement>(shouldMeasure?: boolean, deps?: DependencyList) {
-	shouldMeasure ??= true
-	deps ??= []
-
+export default function useMeasureElement<T extends HTMLElement>(
+	{ shouldMeasure, estimateSize }: { shouldMeasure: boolean; estimateSize: number } = {
+		shouldMeasure: true,
+		estimateSize: 40
+	},
+	deps: DependencyList = []
+) {
 	return useMemo(
 		() =>
 			shouldMeasure && typeof window !== 'undefined' && navigator.userAgent.indexOf('Firefox') === -1
-				? (element: T) => element?.getBoundingClientRect()?.height
+				? (element: T) => element?.getBoundingClientRect()?.height ?? estimateSize
 				: undefined,
-		[window, navigator.userAgent, shouldMeasure, ...deps]
+		[window, navigator.userAgent, devicePixelRatio, shouldMeasure, ...deps]
 	)
 }
