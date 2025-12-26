f3e65b721212ec5b7f03bc4135876462e610a02f
quanghiep03198 <quanghiep03198@gmail.com>
2025-12-26 15:52:24 +0700

    refactor: streamline window size state initialization in Layout component

--- src/app/(features)/_layout.tsx
+++ src/app/(features)/_layout.tsx
@@ -38,15 +38,14 @@ function Layout() {
 		defaultValue: '*:!font-sans',
 		listenStorageChange: true
 	})
+	const [windowSize, setWindowSize] = useRafState({
+		width: 0,
+		height: 0
+	})
 
 	useEffectOnce(() => {
 		if (document.body.classList.contains(font)) document.body.classList.remove(font)
 		document.body.classList.add(font)
-	})
-
-	const [windowSize, setWindowSize] = useRafState({
-		width: 0,
-		height: 0
 	})
 
 	useLayoutEffect(() => {
@@ -81,7 +80,7 @@ function Layout() {
 							<Div
 								as='main'
 								id='outlet-wrapper'
-								className='flex-1 basis-full overflow-hidden px-6 pb-[var(--outlet-padding-bottom)] sm:px-2 md:px-2'>
+								className='flex-1 basis-full px-6 pb-[var(--outlet-padding-bottom)] sm:px-2 md:px-2'>
 								<ErrorBoundary
 									fallbackRender={({ error, resetErrorBoundary }) => {
 										return (
