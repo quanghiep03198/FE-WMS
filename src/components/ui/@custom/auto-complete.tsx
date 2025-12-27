ee23e710a7fe6e355354133271339c612df0b423
quanghiep03198 <quanghiep03198@gmail.com>
2025-12-27 22:27:21 +0700

    feat: add AutoComplete component with filtering and selection functionality

---
+++ src/components/ui/@custom/auto-complete.tsx
@@ -0,0 +1,143 @@
+import { cn } from '@/common/utils/cn'
+import { CaretSortIcon, CheckIcon } from '@radix-ui/react-icons'
+
+import React, { useId, useMemo, useRef, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import tw from 'tailwind-styled-components'
+import { v4 as uuidv4 } from 'uuid'
+import { Icon } from '../@core/icon'
+import { Popover, PopoverContent, PopoverTrigger } from '../@core/popover'
+import { DebouncedInput } from './debounced-input'
+import { Div } from './div'
+import { Typography } from './typography'
+
+type AutoCompleteProps<D extends object> = {
+	placeholder?: string
+	loading?: boolean
+	disabled?: boolean
+	shouldFilter?: boolean
+	datalist: Array<D>
+	labelField: keyof D
+	valueField: keyof D
+	template?: React.FC<
+		{ value: D } & React.ComponentProps<
+			'div' extends keyof HTMLElementTagNameMap ? keyof HTMLElementTagNameMap : React.ElementType
+		>
+	>
+	onInput?: (value: string) => any
+	onSelect?: (value: string) => unknown
+} & React.ComponentProps<'input'>
+
+function AutoComplete<D extends object>(props: AutoCompleteProps<D>) {
+	const [open, setOpen] = useState(false)
+	const { t } = useTranslation()
+
+	const {
+		name,
+		placeholder,
+		datalist,
+		loading,
+		labelField,
+		valueField,
+		shouldFilter = true,
+		className,
+		value: currentValue,
+		onInput,
+		onSelect,
+		ref: forwardedRef,
+		...inputProps
+	} = props
+	const id = useId()
+	const internalRef = useRef<HTMLInputElement>(null)
+	const resolvedRef = (forwardedRef || internalRef) as React.RefObject<HTMLInputElement>
+
+	const filteredDatalist = useMemo(() => {
+		if (!shouldFilter) return datalist ?? []
+
+		const filterFn = (item: D) => {
+			const value = item[valueField]
+			if (value === null || value === undefined) return false
+			const currVal = currentValue ?? ''
+			return String(value).toLowerCase().includes(String(currVal).toLowerCase())
+		}
+
+		return Array.isArray(datalist) ? datalist.filter(filterFn) : []
+	}, [datalist, shouldFilter, currentValue])
+
+	const handleKeyDown = (e: React.KeyboardEvent) => {
+		if (e.key === 'Enter') e.preventDefault()
+		else if (e.key === 'Escape') setOpen(false)
+		else {
+			setOpen(true)
+		}
+	}
+
+	return (
+		<Popover open={open} onOpenChange={setOpen} modal={false}>
+			<PopoverTrigger type='button' className='relative w-full' onClick={(e) => e.preventDefault()}>
+				<DebouncedInput
+					id={id}
+					ref={resolvedRef}
+					name={name}
+					value={currentValue}
+					autoComplete='off'
+					placeholder={placeholder}
+					className={cn(
+						'peer rounded-md py-1 pl-3 pr-9 text-sm shadow-sm transition-colors duration-200 focus-within:!border-primary aria-[invalid=true]:border-destructive aria-[invalid=true]:focus-within:border-destructive',
+						className
+					)}
+					style={{
+						border: '1px solid hsl(var(--border))'
+					}}
+					data-icon={props['data-icon']}
+					onKeyDown={handleKeyDown}
+					onClick={() => setOpen(true)}
+					onChange={(value) => {
+						if (typeof onInput === 'function') onInput(value)
+					}}
+					{...inputProps}
+				/>
+				<CaretSortIcon className='absolute right-3 top-1/2 ml-auto h-4 w-4 -translate-y-1/2 opacity-50 peer-data-[icon=false]:hidden' />
+			</PopoverTrigger>
+			<PopoverContent
+				className='max-h-52 w-[var(--radix-popover-trigger-width)] overflow-auto p-1'
+				onOpenAutoFocus={(e) => e.preventDefault()}>
+				{loading ? (
+					<Div className='flex items-center justify-center p-10 text-center'>
+						<Icon name='LoaderCircle' size={18} className='animate-[spin_1s_linear_infinite]' />
+					</Div>
+				) : filteredDatalist?.length > 0 ? (
+					filteredDatalist?.map((item) => {
+						return (
+							<AutoCompleteItem
+								key={uuidv4()}
+								onClick={(e) => {
+									e.stopPropagation()
+									setOpen(false)
+									if (typeof onSelect === 'function') onSelect(String(item[valueField]))
+								}}>
+								<Typography variant='small' className='line-clamp-1 flex-1'>
+									{String(item[labelField])}
+								</Typography>
+								<CheckIcon
+									className={cn(
+										'ml-auto transition-opacity duration-200',
+										currentValue === item[valueField] ? 'opacity-100' : 'opacity-0'
+									)}
+								/>
+							</AutoCompleteItem>
+						)
+					})
+				) : (
+					<Typography variant='small' color='muted' className='block h-full p-10 text-center'>
+						{t('ns_common:table.no_data')}
+					</Typography>
+				)}
+			</PopoverContent>
+		</Popover>
+	)
+}
+
+const AutoCompleteItem = tw.div`flex cursor-pointer items-center rounded-md p-2 h-8 hover:bg-secondary hover:text-secondary-foreground`
+
+export default AutoComplete
