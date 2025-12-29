57a4fea33ac07a8cd089d43d9d97ddb95fe136c6
quanghiep03198 <quanghiep03198@gmail.com>
2025-12-29 08:47:23 +0700

    refactor: update styling for animated chip and FAQ section components

--- src/app/(public)/-components/animated-chip.tsx
+++ src/app/(public)/-components/animated-chip.tsx
@@ -40,7 +40,7 @@ const BeamAnimated: React.FC = () => {
 				viewBox='0 0 650 200'
 				fill='none'
 				xmlns='http://www.w3.org/2000/svg'
-				className='mx-auto xl:w-[650px]'
+				className='mx-auto sm:max-w-xs xl:w-[650px]'
 				ref={svgRef}>
 				<defs>
 					<linearGradient offset={1} id='right-to-left'>
@@ -262,7 +262,7 @@ const BeamAnimated: React.FC = () => {
 				className={cn(
 					'absolute top-1/2 z-20 flex aspect-square w-full items-center justify-center rounded-xl border-2 border-neutral-200 dark:border-neutral-700 sm:rounded-md',
 					'bg-gradient-to-br from-background to-accent to-[30%]',
-					'left-[58%] sm:max-w-[80px]',
+					'left-[58%] sm:max-w-[96px]',
 					'left-[56%] md:max-w-[196px]',
 					'left-[52%] lg:max-w-[210px]',
 					'xl:left-[60%] xl:max-w-[200px] xl:-translate-x-1/2 xxl:left-[calc(60%+1rem)] xxl:max-w-[216px]',
@@ -271,12 +271,12 @@ const BeamAnimated: React.FC = () => {
 				<div className='relative grid h-full w-full flex-1 place-content-center'>
 					<div
 						className={cn(
-							'flex aspect-square size-24 select-none flex-col items-center justify-center gap-y-6 rounded-lg p-4 sm:size-11 sm:gap-y-2 sm:rounded-sm sm:p-2 sm:text-lg md:gap-y-4 md:p-4',
+							'flex aspect-square size-24 select-none flex-col items-center justify-center gap-y-6 rounded-lg p-4 sm:size-12 sm:gap-y-2 sm:rounded-sm sm:p-2 sm:text-lg md:p-4',
 							renderCount > 0
-								? '-translate-x-5 -translate-y-5 border-2 border-neutral-500 bg-primary text-primary-foreground shadow-[24px_24px_16px_#0a0a0a98] [transition:background-color_800ms_ease-in-out_1600ms,transform_400ms_cubic-bezier(0.68,-0.6,0.32,1.6)_1600ms,box-shadow_300ms_ease-out_1600ms] sm:-translate-x-2.5 sm:-translate-y-2.5 sm:border'
+								? '-translate-x-5 -translate-y-5 border-2 border-neutral-500 bg-primary text-primary-foreground shadow-[24px_24px_16px_#0a0a0a98] [transition:background-color_800ms_ease-in-out_1600ms,transform_400ms_cubic-bezier(0.68,-0.6,0.32,1.6)_1600ms,box-shadow_300ms_ease-out_1600ms] sm:-translate-x-2.5 sm:-translate-y-2.5 sm:border sm:shadow-[16px_16px_12px_#0a0a0a98]'
 								: 'translate-x-0 translate-y-0 !border-neutral-600 bg-neutral-500 text-neutral-700 shadow-none'
 						)}>
-						<span className='h-6 text-center font-jetbrains text-2xl font-semibold tracking-wider transition-none duration-0 sm:text-xs md:text-2xl xl:text-2xl'>
+						<span className='h-6 text-center font-jetbrains text-2xl font-semibold tracking-wider transition-none duration-0 sm:text-sm md:text-2xl xl:text-2xl'>
 							WMS
 						</span>
 						<Separator className='h-1 w-full bg-primary-foreground sm:h-0.5 md:h-1 lg:h-1' />
--- src/app/(public)/-components/faq-section.tsx
+++ src/app/(public)/-components/faq-section.tsx
@@ -92,7 +92,7 @@ const FAQsSection: React.FunctionComponent = () => {
 					<Div className='size-3 rounded-full bg-success' />
 				</Div>
 				<Typography className='py-2 text-center font-medium'>FAQs</Typography>
-				<ScrollShadow ref={chatBoxRef} className='flex h-64 flex-1 flex-col gap-y-3 !overflow-hidden p-4'>
+				<ScrollShadow ref={chatBoxRef} className='flex h-64 flex-1 flex-col gap-y-3 p-4'>
 					{faqs.map((faq, index) => (
 						<Fragment key={index}>
 							<Div
