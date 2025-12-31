8d19330510d8b318d3a78302052b8f612532295d
quanghiep03198 <quanghiep03198@gmail.com>
2025-12-31 10:44:33 +0700

    refactor: reorganize mutation keys and implement optimistic updates for container condition mutation

--- src/app/(features)/_layout.truckload-delivery/-hooks/use-truckload-delivery-asm.tsx
+++ src/app/(features)/_layout.truckload-delivery/-hooks/use-truckload-delivery-asm.tsx
@@ -1,4 +1,8 @@
-import { TruckloadDeliveryDispatchOrder, TruckloadDeliveryService } from '@/services/truckload-delivery.service'
+import {
+	ITruckloadDelivery,
+	TruckloadDeliveryDispatchOrder,
+	TruckloadDeliveryService
+} from '@/services/truckload-delivery.service'
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { uniqBy } from 'lodash-es'
 import { TruckloadDeliveryStatus } from '../-constants'
@@ -6,9 +10,13 @@ import { SignatureType } from '../-contexts/page-context'
 import { UpdateDispatchOrderFormValues, UpsertPurchaseOrdersFormValues } from '../-schemas'
 
 export enum TruckloadDeliveryQueryKeys {
-	TRUCKLOAD_DELIVERY = 'TRUCKLOAD_DELIVERY',
-	UPSERT_PURCHASE_ORDERS = 'UPSERT_PURCHASE_ORDERS',
-	DELETE_PURCHASE_ORDER = 'DELETE_PURCHASE_ORDER'
+	TRUCKLOAD_DELIVERY = 'TRUCKLOAD_DELIVERY'
+}
+
+export enum TruckloadDeliveryMutationKeys {
+	UPDATE_CONTAINER_STATUS = 'UPDATE_CONTAINER_STATUS',
+	DELETE_PURCHASE_ORDER = 'DELETE_PURCHASE_ORDER',
+	UPSERT_PURCHASE_ORDERS = 'UPSERT_PURCHASE_ORDERS'
 }
 
 export const useGetTruckloadDeliveryQuery = () => {
@@ -57,7 +65,7 @@ export const useDeleteTruckloadDeliveryMutation = () => {
 	const invalidateQueries = useInvalidateQueries()
 
 	return useMutation({
-		mutationKey: [TruckloadDeliveryQueryKeys.DELETE_PURCHASE_ORDER],
+		mutationKey: [TruckloadDeliveryMutationKeys.DELETE_PURCHASE_ORDER],
 		mutationFn: (id: number) => {
 			return TruckloadDeliveryService.deleteOne(id)
 		},
@@ -80,7 +88,7 @@ export const useUpsertPurchaseOrdersMutation = () => {
 	const invalidateQueries = useInvalidateQueries()
 
 	return useMutation({
-		mutationKey: [TruckloadDeliveryQueryKeys.UPSERT_PURCHASE_ORDERS],
+		mutationKey: [TruckloadDeliveryMutationKeys.UPSERT_PURCHASE_ORDERS],
 		mutationFn: (payload: UpsertPurchaseOrdersFormValues) => TruckloadDeliveryService.upsertPurchaseOrders(payload),
 		onSuccess: () => invalidateQueries()
 	})
@@ -102,8 +110,10 @@ export const useUpdateDispatchOrderSignatureMutation = () => {
 
 export const useUpdateContainerConditionMutation = () => {
 	const invalidateQueries = useInvalidateQueries()
+	const queryClient = useQueryClient()
 
 	return useMutation({
+		mutationKey: [TruckloadDeliveryMutationKeys.UPDATE_CONTAINER_STATUS],
 		mutationFn: async (payload: {
 			dispatch_order: TruckloadDeliveryDispatchOrder
 			punctured_container?: boolean
@@ -112,7 +122,43 @@ export const useUpdateContainerConditionMutation = () => {
 		}) => {
 			return await TruckloadDeliveryService.updateContainerCondition(payload)
 		},
-		onSuccess: invalidateQueries
+		onMutate: async (variables) => {
+			// Cancel any outgoing refetches (so they don't overwrite our optimistic update)
+			await queryClient.cancelQueries({ queryKey: [TruckloadDeliveryQueryKeys.TRUCKLOAD_DELIVERY] })
+
+			// Snapshot the previous value
+			const previousData = queryClient.getQueryData<ResponseBody<ITruckloadDelivery[]>>([
+				TruckloadDeliveryQueryKeys.TRUCKLOAD_DELIVERY
+			])
+
+			// Optimistically update to the new value
+			queryClient.setQueryData<ResponseBody<ITruckloadDelivery[]>>(
+				[TruckloadDeliveryQueryKeys.TRUCKLOAD_DELIVERY],
+				(oldData) => {
+					if (!oldData) return oldData
+					return {
+						...oldData,
+						metadata: oldData.metadata.map((item) => {
+							if (item.dispatch_order === variables.dispatch_order) return { ...item, ...variables }
+							return item
+						})
+					}
+				}
+			)
+
+			// Return a context object with the snapshotted value
+			console.log('previousData :>> ', previousData)
+			return { previousData }
+		},
+		onError: (_error, _variables, context) => {
+			if (context && context.previousData) {
+				queryClient.setQueryData<ResponseBody<ITruckloadDelivery[]>>(
+					[TruckloadDeliveryQueryKeys.TRUCKLOAD_DELIVERY],
+					context.previousData
+				)
+			}
+		},
+		onSettled: invalidateQueries
 	})
 }
 
