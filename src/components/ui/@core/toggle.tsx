54c449a1484777d9c6917e11a445b44066a266a0
quanghiep03198 <quanghiep03198@gmail.com>
2026-01-06 11:22:50 +0700

    fix: add gap between icon and text in Toggle component for improved layout

--- src/components/ui/@core/toggle.tsx
+++ src/components/ui/@core/toggle.tsx
@@ -5,7 +5,7 @@ import * as React from 'react'
 import { cn } from '@/common/utils/cn'
 
 const toggleVariants = cva(
-	'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors hover:bg-muted hover:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-accent data-[state=on]:text-accent-foreground',
+	'inline-flex [&:has(svg)]:gap-x-2 items-center justify-center rounded-md text-sm font-medium transition-colors hover:bg-muted hover:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-accent data-[state=on]:text-accent-foreground',
 	{
 		variants: {
 			variant: {
@@ -14,7 +14,8 @@ const toggleVariants = cva(
 			},
 			size: {
 				default: 'h-9 px-3',
-				sm: 'h-8 px-2',
+				icon: 'size-9',
+				sm: 'h-8 px-2 text-xs',
 				lg: 'h-10 px-3'
 			}
 		},
