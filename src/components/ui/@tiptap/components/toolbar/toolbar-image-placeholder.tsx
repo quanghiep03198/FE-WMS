f5622b329f1ac304494cedb9405d499ab49f799f
quanghiep03198 <quanghiep03198@gmail.com>
2026-01-06 13:04:04 +0700

    refactor: convert ImagePlaceholderToolbar to a functional component for improved readability

--- src/components/ui/@tiptap/components/toolbar/toolbar-image-placeholder.tsx
+++ src/components/ui/@tiptap/components/toolbar/toolbar-image-placeholder.tsx
@@ -5,35 +5,28 @@ import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { useEditorContext } from '../../context/editor-context'
 
-const ImagePlaceholderToolbar = React.forwardRef<HTMLButtonElement, ButtonProps>(
-	({ className, onClick, children, ...props }, ref) => {
-		const { editor } = useEditorContext()
-		const { t } = useTranslation()
+const ImagePlaceholderToolbar: React.FC<ButtonProps> = ({ className, onClick, children, ...props }) => {
+	const { editor } = useEditorContext()
+	const { t } = useTranslation()
 
-		return (
-			<Tooltip message={t('ns_common:editor.image')} triggerProps={{ asChild: true }}>
-				<Button
-					variant='ghost'
-					size='icon'
-					type='button'
-					className={cn(
-						'aspect-square size-8 p-0',
-						editor?.isActive('image-placeholder') && 'bg-accent',
-						className
-					)}
-					onClick={(e) => {
-						e.preventDefault()
-						editor?.chain().focus().insertImagePlaceholder().run()
-						onClick?.(e)
-					}}
-					ref={ref}
-					{...props}>
-					{children ?? <Icon name='Image' className='h-4 w-4' />}
-				</Button>
-			</Tooltip>
-		)
-	}
-)
+	return (
+		<Tooltip message={t('ns_common:editor.image')} triggerProps={{ asChild: true }}>
+			<Button
+				variant='ghost'
+				size='icon'
+				type='button'
+				className={cn('aspect-square size-8 p-0', editor?.isActive('image-placeholder') && 'bg-accent', className)}
+				onClick={(e) => {
+					e.preventDefault()
+					editor?.chain().focus().insertImagePlaceholder().run()
+					onClick?.(e)
+				}}
+				{...props}>
+				{children ?? <Icon name='Image' className='h-4 w-4' />}
+			</Button>
+		</Tooltip>
+	)
+}
 
 ImagePlaceholderToolbar.displayName = 'ImagePlaceholderToolbar'
 
