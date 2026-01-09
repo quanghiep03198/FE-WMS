9f036a266e386508e65685bec82057b3aaa0e09c
quanghiep03198 <quanghiep03198@gmail.com>
2026-01-09 16:39:21 +0700

    fix: update error handling in readerSettingsFormSchema for improved validation

--- src/app/(features)/_layout.(defective-goods)/-schemas/reader-settings.schema.ts
+++ src/app/(features)/_layout.(defective-goods)/-schemas/reader-settings.schema.ts
@@ -1,10 +1,20 @@
-import { enum as enums, ipv4, number, object, type infer as Infer } from 'zod'
+import { enum as enums, ipv4, number, object, string, type infer as Infer } from 'zod'
 import { ReaderAntenna } from '../-constants'
 
 export const readerSettingsFormSchema = object({
-	readerIP: ipv4({ message: 'ns_validation:invalid_ipv4' }),
-	readerAnt: enums(ReaderAntenna, { message: 'ns_validation:invalid_value' }),
-	readerPower: number().nonnegative().min(5).max(30)
+	readerIP: ipv4({ error: 'ns_validation:invalid_ipv4' }),
+	readerAnt: enums(ReaderAntenna, { error: 'ns_validation:invalid_value' }),
+	readerPower: number({ error: 'ns_validation:req' })
+		.nonnegative()
+		.min(5)
+		.max(30)
+		.or(
+			string()
+				.refine((value) => !isNaN(+value) && Number.parseInt(value) >= 5 && Number.parseInt(value) <= 30, {
+					error: 'ns_validation:invalid_value'
+				})
+				.transform((value) => Number.parseInt(value))
+		)
 })
 
 export type ReaderSettingsFormValues = Infer<typeof readerSettingsFormSchema>
