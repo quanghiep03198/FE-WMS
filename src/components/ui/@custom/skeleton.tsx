3de82294dc891a54337fd2dbcae01c6a432a4b34
quanghiep03198 <quanghiep03198@gmail.com>
2026-01-10 13:16:14 +0700

    refactor: simplify style handling in Skeleton component by removing inline style

--- src/components/ui/@custom/skeleton.tsx
+++ src/components/ui/@custom/skeleton.tsx
@@ -5,12 +5,8 @@ const Skeleton: React.FC<React.ComponentProps<'div'>> = ({ className, ...props }
 	return (
 		<div
 			{...props}
-			style={{
-				...props.style,
-				willChange: 'background-position'
-			}}
 			className={cn(
-				'min-h-3 w-full animate-shimmer rounded-[2px] bg-[linear-gradient(90deg,#e5e5e5,15%,#f5f5f5,85%,#e5e5e5)] bg-[length:200%_100%] dark:bg-[linear-gradient(90deg,#171717,15%,#262626,85%,#171717)]',
+				'min-h-3 w-full animate-shimmer rounded-[2px] bg-[linear-gradient(90deg,#e5e5e5,15%,#f5f5f5,85%,#e5e5e5)] bg-[length:200%_100%] will-change-[background-position] dark:bg-[linear-gradient(90deg,#171717,15%,#262626,85%,#171717)]',
 				className
 			)}
 		/>
