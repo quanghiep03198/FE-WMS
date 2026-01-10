947bf40e81b853af9d0e8a90d1ace120309849a0
quanghiep03198 <quanghiep03198@gmail.com>
2026-01-11 00:43:07 +0700

    refactor: adjust layout and height properties in ExploreRFIDAgentBanner for improved responsiveness

--- src/app/(public)/-components/explore-rfid-agent-banner.tsx
+++ src/app/(public)/-components/explore-rfid-agent-banner.tsx
@@ -8,9 +8,9 @@ export function ExploreRFIDAgentBanner() {
 	return (
 		<Div
 			className={
-				'sticky top-0 z-50 h-10 min-h-10 bg-primary p-2 text-primary-foreground shadow-lg duration-500 animate-in fade-in-20 slide-in-from-top-full sm:h-16'
+				'sticky top-0 z-50 bg-primary p-2 text-primary-foreground shadow-lg duration-500 animate-in fade-in-20 slide-in-from-top-full sm:h-20 md:h-10 lg:h-10 xl:h-10'
 			}>
-			<Div className='mx-auto flex max-w-7xl items-center justify-center xxl:max-w-8xl'>
+			<Div className='mx-auto flex h-full max-w-7xl items-center justify-center xxl:max-w-8xl'>
 				<Typography className='inline-flex flex-wrap items-center gap-x-2 sm:flex-col'>
 					<Typography as='span'>
 						🚀 RFID Agent {!!latestRelease && latestRelease?.tag_name} has been released !!!
