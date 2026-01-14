1a8e9114302cc5c5f2fc67bba728fae9a1ccdffc
quanghiep03198 <quanghiep03198@gmail.com>
2026-01-14 09:36:31 +0700

    refactor: reduce sidebar transition duration for improved responsiveness

--- src/components/ui/@core/sidebar.tsx
+++ src/components/ui/@core/sidebar.tsx
@@ -175,7 +175,7 @@ const Sidebar: React.FC<
 			{/* This is what handles the sidebar gap on desktop */}
 			<div
 				className={cn(
-					'relative h-svh w-[--sidebar-width] bg-transparent transition-[width] duration-150 ease-linear',
+					'relative h-svh w-[--sidebar-width] bg-transparent transition-[width] duration-100 ease-linear',
 					'group-data-[collapsible=offcanvas]:w-0',
 					'group-data-[side=right]:rotate-180',
 					variant === 'floating' || variant === 'inset'
@@ -185,7 +185,7 @@ const Sidebar: React.FC<
 			/>
 			<div
 				className={cn(
-					'fixed inset-y-0 flex h-svh w-[--sidebar-width] transition-[left,right,width] duration-150 ease-linear sm:hidden md:hidden',
+					'fixed inset-y-0 flex h-svh w-[--sidebar-width] transition-[left,right,width] duration-100 ease-linear sm:hidden md:hidden',
 					side === 'left'
 						? 'left-0 group-data-[collapsible=offcanvas]:left-[calc(var(--sidebar-width)*-1)]'
 						: 'right-0 group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]',
