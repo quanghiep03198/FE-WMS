c07ee25832d421a89bd200a7c7a9bcd8094fafea
quanghiep03198 <quanghiep03198@gmail.com>
2026-01-19 14:11:47 +0700

    refactor: enhance CollapsibleContent with custom class handling and animations

--- src/components/ui/@core/collapsible.tsx
+++ src/components/ui/@core/collapsible.tsx
@@ -1,9 +1,21 @@
+import { cn } from '@/common/utils/cn'
 import * as CollapsiblePrimitive from '@radix-ui/react-collapsible'
 
 const Collapsible = CollapsiblePrimitive.Root
 
 const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger
 
-const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent
+const CollapsibleContent: React.FC<React.ComponentProps<typeof CollapsiblePrimitive.CollapsibleContent>> = ({
+	className,
+	...props
+}) => (
+	<CollapsiblePrimitive.CollapsibleContent
+		{...props}
+		className={cn(
+			'transition-allow-discrete data-[state=closed]:animate-collapsible-up data-[state=open]:animate-collapsible-down',
+			className
+		)}
+	/>
+)
 
 export { Collapsible, CollapsibleContent, CollapsibleTrigger }
