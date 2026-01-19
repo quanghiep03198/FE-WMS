687373a798aed5294cef342591f757b0817e9c58
quanghiep03198 <quanghiep03198@gmail.com>
2026-01-19 15:39:50 +0700

    refactor: update layout and styling in PlaceHolderItems component

--- src/app/(features)/-components/shared/placeholder-items.tsx
+++ src/app/(features)/-components/shared/placeholder-items.tsx
@@ -5,9 +5,9 @@ import React from 'react'
 const PlaceHolderItems: React.FC<React.ComponentProps<'div'>> = ({ className, ...props }) => {
 	return (
 		<Div {...props} className={cn('relative grid h-28 w-full place-content-center place-items-center', className)}>
-			<PlaceholderItem className='absolute top-0 z-20' />
-			<PlaceholderItem className='absolute top-6 z-10 scale-[85%] opacity-80' />
-			<PlaceholderItem className='absolute top-12 z-0 scale-[70%] opacity-60' />
+			<PlaceholderItem className='absolute top-0 z-20 dark:brightness-95' />
+			<PlaceholderItem className='absolute top-6 z-10 scale-[0.8] dark:brightness-75' />
+			<PlaceholderItem className='absolute top-12 z-0 scale-[0.6] dark:brightness-50' />
 		</Div>
 	)
 }
@@ -16,15 +16,13 @@ const PlaceholderItem: React.FC<React.ComponentProps<'div'>> = ({ className, ...
 	return (
 		<Div
 			className={cn(
-				'flex w-full max-w-48 items-center gap-x-4 rounded-md border bg-background p-2 shadow-md',
+				'grid w-full max-w-48 grid-cols-[auto_1fr] items-center gap-x-4 gap-y-2 rounded-md border bg-background p-2 shadow-md',
 				className
 			)}
 			{...props}>
-			<Div className='aspect-square size-8 rounded bg-accent' />
-			<Div className='flex-1 space-y-2'>
-				<Div className='aspect-square h-3 w-full rounded bg-accent' />
-				<Div className='aspect-square h-3 w-2/3 rounded bg-accent' />
-			</Div>
+			<Div className='col-start-1 row-span-2 aspect-square size-8 rounded bg-accent' />
+			<Div className='col-start-2 row-start-1 aspect-square h-3 w-full rounded bg-accent' />
+			<Div className='col-start-2 row-start-2 aspect-square h-3 w-2/3 rounded bg-accent' />
 		</Div>
 	)
 }
