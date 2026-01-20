9f29800bfdec88e052ba682fe0a0b59c736e3059
quanghiep03198 <quanghiep03198@gmail.com>
2026-01-20 11:07:49 +0700

    refactor: add React Scan initialization and update Sentry configuration for environment detection

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,22 @@ import './styles/index.css'
 
 import * as Sentry from '@sentry/react'
 import { createRoot } from 'react-dom/client'
+import { scan } from 'react-scan'
 import App from './__app.tsx'
 import env from './common/utils/env.ts'
 import { router } from './providers/router-provider.tsx'
 import reportWebVitals from './report-web-vitals.ts'
 
+// Runtime Environment Detection
 const runtimeEnvironment = env<RuntimeEnvironment>('VITE_NODE_ENV')
 
+const isProduction = runtimeEnvironment === 'production'
+const isDevelopment = runtimeEnvironment === 'development'
+
+// React Scan Initialization
+scan({ enabled: isDevelopment })
+
+// Sentry Initialization
 Sentry.init({
 	dsn: env('VITE_SENTRY_DSN'),
 	integrations: [
@@ -20,13 +29,14 @@ Sentry.init({
 		if (event.exception) Sentry.captureException(event.exception)
 		return event
 	},
-	enabled: runtimeEnvironment === 'production',
+	enabled: isProduction,
 	tracesSampleRate: 1.0, //  Capture 100% of the transactions
 	tracePropagationTargets: [env('VITE_APP_HOST'), env('VITE_API_BASE_URL')],
-	replaysSessionSampleRate: runtimeEnvironment === 'production' ? 0.1 : 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+	replaysSessionSampleRate: isProduction ? 0.1 : 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
 	replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 })
 
+// App Rendering
 const container = document.getElementById('root')
 
 createRoot(container, {
@@ -35,4 +45,5 @@ createRoot(container, {
 	}
 }).render(<App />)
 
+// Reporting Web Vitals
 reportWebVitals()
