6ece447d71a6401fc72b0c28627ef3269a58419e
quanghiep03198 <quanghiep03198@gmail.com>
2026-01-24 23:34:41 +0700

    feat: update ButtonProps interface for better type compatibility and enhance TableRow class handling

--- src/components/ui/@core/button.tsx
+++ src/components/ui/@core/button.tsx
@@ -30,9 +30,7 @@ const buttonVariants = cva(
 	}
 )
 
-export interface ButtonProps
-	extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-		VariantProps<typeof buttonVariants> {
+export interface ButtonProps extends React.ComponentProps<'button'>, VariantProps<typeof buttonVariants> {
 	asChild?: boolean
 }
 
