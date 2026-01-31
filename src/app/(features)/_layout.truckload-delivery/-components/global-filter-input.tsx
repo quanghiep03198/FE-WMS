c8b1e7af9a299df30292a1dacaabd441ae67a1ba
quanghiep03198 <quanghiep03198@gmail.com>
2026-01-31 15:27:43 +0700

    refactor: replace Div with ButtonGroup for improved layout in GlobalFilterInput

--- src/app/(features)/_layout.truckload-delivery/-components/global-filter-input.tsx
+++ src/app/(features)/_layout.truckload-delivery/-components/global-filter-input.tsx
@@ -2,7 +2,7 @@
 
 import { PresetBreakPoints } from '@/common/constants/enums'
 import useMediaQuery from '@/common/hooks/use-media-query'
-import { Div, Icon } from '@/components/ui'
+import { ButtonGroup, buttonVariants, Icon } from '@/components/ui'
 import { DebouncedInput } from '@/components/ui/@custom/debounced-input'
 import { ITruckloadDelivery } from '@/services/truckload-delivery.service'
 import { Table } from '@tanstack/react-table'
@@ -19,14 +19,19 @@ const GlobalFilterInput: React.FC<{
 	const isMediumScreen = useMediaQuery(PresetBreakPoints.MEDIUM)
 
 	return (
-		<Div className='flex h-9 items-center gap-x-1 rounded-md border px-3 shadow-sm'>
+		<ButtonGroup
+			className={buttonVariants({
+				variant: 'outline',
+				size: 'default',
+				className: 'bg-background font-normal hover:bg-background'
+			})}>
 			<Icon name='Search' />
 			<DebouncedInput
 				value={table.getState().globalFilter}
 				onChange={(value) => {
 					table.setGlobalFilter(String(value))
 				}}
-				className='h-full min-w-44 p-0 pl-2 shadow-none placeholder:text-sm xl:min-w-56'
+				className='h-full min-w-44 p-0 shadow-none placeholder:text-sm xl:min-w-56'
 				placeholder={
 					isMediumScreen
 						? t('ns_common:actions.search') + ' ...'
@@ -34,7 +39,7 @@ const GlobalFilterInput: React.FC<{
 				}
 				type='search'
 			/>
-		</Div>
+		</ButtonGroup>
 	)
 }
 
