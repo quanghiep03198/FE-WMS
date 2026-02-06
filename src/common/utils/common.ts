fd99c426e4306281abe68374101ca2b7d8500801
quanghiep03198 <quanghiep03198@gmail.com>
2026-02-06 09:52:19 +0700

    fix(common-utils): add coalesce function for handling null and undefined values

--- src/common/utils/common.ts
+++ src/common/utils/common.ts
@@ -1,3 +1,7 @@
 export function isPrimitive(value: any) {
 	return (typeof value !== 'object' && typeof value !== 'function') || value === null
 }
+
+export function coalesce<T>(value: T | null, fallbackValue: T) {
+	return value === null || value === undefined ? fallbackValue : value
+}
