03d42832c1e0aaac168bec8175215236984f15a6
quanghiep03198 <quanghiep03198@gmail.com>
2026-02-09 16:14:14 +0700

    refactor(epc-table): update styles for DataTableScrollArea and DataTableEmpty components

--- src/app/(features)/_layout.(defective-goods)/defective-goods-inoutbound/-components/epc-table/table-empty.tsx
+++ src/app/(features)/_layout.(defective-goods)/defective-goods-inoutbound/-components/epc-table/table-empty.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 
 const DataTableEmpty: React.FC<{ colSpan: number }> = ({ colSpan }) => {
 	return (
-		<TableBody className='h-[calc(var(--outlet-wrapper-height)-var(--header-height)-var(--row-height)*2)]'>
+		<TableBody className='h-[calc(100cqh-var(--row-height)*2-1px)]'>
 			<TableRow>
 				<TableCell colSpan={colSpan} className='p-0'>
-					<Div className='sticky left-0 top-0 flex items-center justify-center gap-x-2 text-muted-foreground'>
+					<Div className='sticky left-0 top-0 flex w-[100cqw] items-center justify-center gap-x-2 text-muted-foreground'>
 						<Icon name='Database' strokeWidth={1} size={32} /> No data
 					</Div>
 				</TableCell>
