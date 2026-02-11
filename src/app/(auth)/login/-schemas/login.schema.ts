634c0c610ca5b765512c013572ff560255b5c435
quanghiep03198 <quanghiep03198@gmail.com>
2026-02-11 16:21:31 +0700

    refactor(login-schema): replace lodash isEmpty with zod nonempty validation

--- src/app/(auth)/login/-schemas/login.schema.ts
+++ src/app/(auth)/login/-schemas/login.schema.ts
@@ -1,9 +1,12 @@
-import { isEmpty } from 'lodash-es'
 import { infer as Infer, object, string } from 'zod'
 
 export const loginSchema = object({
-	username: string().refine((value) => !isEmpty(value), { message: 'ns_auth:validation.require_account' }),
-	password: string().refine((value) => !isEmpty(value), { message: 'ns_auth:validation.require_password' })
+	username: string({ error: 'ns_auth:validation.require_account' }).nonempty({
+		error: 'ns_auth:validation.require_account'
+	}),
+	password: string({ error: 'ns_auth:validation.require_password' }).nonempty({
+		error: 'ns_auth:validation.require_password'
+	})
 })
 
 export type LoginFormValues = Infer<typeof loginSchema>
