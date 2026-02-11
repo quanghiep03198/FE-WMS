782626838e8ab73f6fa5726bffc157bdbee3e6ef
quanghiep03198 <quanghiep03198@gmail.com>
2026-02-11 18:26:58 +0700

    fix(update-password): check empty currentPassword field

--- src/app/(features)/preferences/_layout.account/-schemas/update-password.schema.ts
+++ src/app/(features)/preferences/_layout.account/-schemas/update-password.schema.ts
@@ -1,15 +1,7 @@
-import { useAuthStore } from '@/stores/auth.store'
-import { compareSync } from 'bcryptjs-react'
-import { debounce } from 'lodash-es'
 import { object, string, type infer as Infer } from 'zod'
 
-const { user } = useAuthStore.getState()
-
 export const updatePasswordFormValues = object({
-	currentPassword: string({ error: 'ns_validation:required' }).refine(
-		debounce((value) => compareSync(value, user?.password), 200),
-		{ error: 'ns_auth:' }
-	),
+	currentPassword: string({ error: 'ns_validation:required' }).nonempty({ error: 'ns_validation:required' }),
 	password: string({ error: 'ns_validation:required' }).min(3, {
 		error: JSON.stringify({ key: 'ns_validation:min_length', bindings: { min: 3 } })
 	})
