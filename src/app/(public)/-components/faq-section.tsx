85466c606eb3ac05e84daf42f420ace77ed66767
quanghiep03198 <quanghiep03198@gmail.com>
2026-02-19 18:50:01 +0700

    fix(faq-section): adjust styles for improved layout and responsiveness

--- src/app/(public)/-components/faq-section.tsx
+++ src/app/(public)/-components/faq-section.tsx
@@ -87,9 +87,9 @@ const FAQsSection: React.FunctionComponent = () => {
 			</Div>
 			<Div
 				style={{
-					transform: 'perspective(999px) rotateX(35deg)'
+					transform: 'perspective(1920px) rotateX(30deg)'
 				}}
-				className='flex max-h-[32rem] w-full flex-grow basis-1/3 flex-col items-stretch rounded-lg border bg-background drop-shadow-[4px_4px_16px_hsl(var(--accent))]'>
+				className='flex max-h-[36rem] w-full max-w-xl flex-grow basis-1/3 flex-col items-stretch rounded-lg border bg-background antialiased drop-shadow-[4px_4px_16px_hsl(var(--accent))] *:antialiased sm:max-w-[18rem]'>
 				<Div className='flex items-center gap-x-2 border-b bg-accent/50 p-2'>
 					<Div className='size-3 rounded-full bg-destructive' />
 					<Div className='size-3 rounded-full bg-warning' />
@@ -123,13 +123,13 @@ const FAQsSection: React.FunctionComponent = () => {
 						</Fragment>
 					))}
 				</ScrollShadow>
-				<Div className='flex min-h-12 -translate-y-3 scale-[1.05] items-center gap-x-3 overflow-hidden rounded-md border bg-accent/50 px-4 py-2 text-sm text-accent-foreground backdrop-blur-sm sm:flex-col sm:items-stretch'>
+				<Div className='flex min-h-12 w-[110%] -translate-y-3 items-center gap-x-3 self-center overflow-hidden rounded-md border bg-background/50 px-4 py-2 text-sm backdrop-blur-sm sm:flex-col sm:items-stretch'>
 					<Typewriter
 						playState={containerInViewPort ? 'running' : 'paused'}
 						className='max-h-10 flex-1 overflow-y-auto text-foreground !scrollbar-none'
-						text={`I have some question, can you help me?`}
+						text='I have some question, can you help me?'
 					/>
-					<Div className='inline-flex items-center gap-x-3 bg-background sm:self-end'>
+					<Div className='inline-flex items-center gap-x-3 sm:self-end'>
 						<Icon name='SmilePlus' />
 						<Icon name='Paperclip' />
 						<Icon name='Send' />
