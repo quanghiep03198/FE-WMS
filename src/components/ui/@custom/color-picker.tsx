ebd28a40ac17596e7111f2ae028a3c97b2a315cf
quanghiep03198 <quanghiep03198@gmail.com>
2026-02-21 14:35:05 +0700

    refactor(color-picker): add className prop to SelectTrigger

--- src/components/ui/@custom/color-picker.tsx
+++ src/components/ui/@custom/color-picker.tsx
@@ -282,7 +282,7 @@ export const ColorPickerOutput = ({ className, ...props }: ColorPickerOutputProp
 
 	return (
 		<Select onValueChange={setMode} value={mode}>
-			<SelectTrigger className='h-8 w-20 shrink-0 text-xs' {...(props as any)}>
+			<SelectTrigger className={cn('h-8 w-20 shrink-0 text-xs', className)} {...(props as any)}>
 				<SelectValue placeholder='Mode' />
 			</SelectTrigger>
 			<SelectContent>
