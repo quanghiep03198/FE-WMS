773b9ffde97837a97a6ae4c190aa5e282b4a7aeb
quanghiep03198 <quanghiep03198@gmail.com>
2026-03-02 16:31:01 +0700

    fix(inventory-audit-schema): update quantity validation to use positive numbers

--- src/app/(features)/_layout.inventory-audit/-schemas/inventory-audit.schema.ts
+++ src/app/(features)/_layout.inventory-audit/-schemas/inventory-audit.schema.ts
@@ -4,8 +4,8 @@ export const reportDataSchema = object({
 	data: array(
 		object({
 			size_numcode: string(),
-			mn_ist_qty: number().min(0, { message: 'Invalid value' }),
-			mn_ost_qty: number().min(0, { message: 'Invalid value' })
+			mn_ist_qty: number().positive({ error: 'ns_validation:invalid_value' }),
+			mn_ost_qty: number().positive({ error: 'ns_validation:invalid_value' })
 		})
 	)
 })
