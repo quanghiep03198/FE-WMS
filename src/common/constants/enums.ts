6c5d00a418f2f3b55fe30842059559b288333497
quanghiep03198 <quanghiep03198@gmail.com>
2026-03-10 00:26:38 +0700

    fix(enums): update medium and large breakpoint values for improved responsiveness

--- src/common/constants/enums.ts
+++ src/common/constants/enums.ts
@@ -61,8 +61,8 @@ export enum Languages {
 
 export enum PresetBreakPoints {
 	SMALL = '(min-width: 360px) and (max-width: 599px)',
-	MEDIUM = '(min-width: 600px) and (max-width: 1023px)',
-	LARGE = '(min-width: 1024px) and (max-width: 1365px)',
+	MEDIUM = '(min-width: 600px) and (max-width: 919px)',
+	LARGE = '(min-width: 920px) and (max-width: 1365px)',
 	EXTRA_LARGE = '(min-width: 1366px)',
 	ULTIMATE_LARGE = '(min-width: 1920px)'
 }
