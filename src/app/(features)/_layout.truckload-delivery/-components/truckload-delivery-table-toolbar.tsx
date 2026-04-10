5f44644184c1d3cdda2e082950dad85c3cc66844
quanghiep03198 <quanghiep03198@gmail.com>
2026-04-10 15:49:41 +0700

    fix: refactor reload button to use queryClient for refetching queries

--- src/app/(features)/_layout.truckload-delivery/-components/truckload-delivery-table-toolbar.tsx
+++ src/app/(features)/_layout.truckload-delivery/-components/truckload-delivery-table-toolbar.tsx
@@ -2,22 +2,36 @@
 
 import { Button, Div, Icon } from '@/components/ui'
 import { type ITruckloadDelivery } from '@/services/truckload-delivery.service'
+import { useQueryClient } from '@tanstack/react-query'
 import { type Table } from '@tanstack/react-table'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import { useGetTruckloadDeliveryQuery } from '../-hooks/use-truckload-delivery-asm'
+import { TruckloadDeliveryQueryKeys } from '../-hooks/use-truckload-delivery-asm'
 import DownloadExcelButton from './download-excel-button'
 import GlobalFilter from './global-filter'
 import { TableViewOptions } from './table-view-options'
 
 const TruckloadDeliveryTableToolbar: React.FC<{ table: Table<ITruckloadDelivery> }> = ({ table }) => {
 	const { t } = useTranslation()
-	const { refetch } = useGetTruckloadDeliveryQuery()
+	const queryClient = useQueryClient()
 
 	return (
 		<Div className='flex items-center justify-end gap-x-2'>
 			<GlobalFilter />
-			<Button variant='outline' size='default' className='ml-auto' onClick={() => refetch()}>
+			<Button
+				variant='outline'
+				size='default'
+				className='ml-auto'
+				onClick={() =>
+					queryClient.refetchQueries({
+						predicate: (query) =>
+							query.queryKey.some(
+								(key) =>
+									key === TruckloadDeliveryQueryKeys.TRUCKLOAD_DELIVERY ||
+									key === TruckloadDeliveryQueryKeys.TRUCKLOAD_DELIVERY_DETAIL
+							)
+					})
+				}>
 				<Icon name='RefreshCcw' /> {t('ns_common:actions.reload')}
 			</Button>
 			<TableViewOptions table={table} />
